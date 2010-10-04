710e9d974c6c16abbb6ba112a0a1c194fe1e5d26
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2010-10-04 23:31:49 +0000

    Don't inline the fuse functions
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@53620 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-arch/bdk-fuse.h
+++ libbdk-arch/bdk-fuse.h
@@ -1,34 +1,9 @@
 /**
- * Read a byte of fuse data
- * @param byte_addr   address to read
- *
- * @return fuse value: 0 or 1
- */
-static inline uint8_t bdk_fuse_read_byte(int byte_addr)
-{
-    bdk_mio_fus_rcmd_t read_cmd;
-
-    read_cmd.u64 = 0;
-    read_cmd.s.addr = byte_addr;
-    read_cmd.s.pend = 1;
-    BDK_CSR_WRITE(BDK_MIO_FUS_RCMD, read_cmd.u64);
-    do
-    {
-        read_cmd.u64 = BDK_CSR_READ(BDK_MIO_FUS_RCMD);
-    } while (read_cmd.s.pend);
-    return read_cmd.s.dat;
-}
-
-
-/**
  * Read a single fuse bit
  *
  * @param fuse   Fuse number (0-1024)
  *
  * @return fuse value: 0 or 1
  */
-static inline int bdk_fuse_read(int fuse)
-{
-    return((bdk_fuse_read_byte(fuse >> 3) >> (fuse & 0x7)) & 1);
-}
+extern int bdk_fuse_read(int fuse);
 
