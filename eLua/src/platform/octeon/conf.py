a5041b1dcf6e4a8cfd331ee97347d19c1d4f8fe9
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2010-10-04 18:32:22 +0000

    Cleanup octeon platform directory
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@53603 c83883e8-9f28-0410-baf1-f2025811e16a

--- eLua/src/platform/octeon/conf.py
+++ eLua/src/platform/octeon/conf.py
@@ -1,21 +1,15 @@
 # Configuration file for the linux backend
 
-specific_files = "boot.s platform.c host.c"
 specific_files = "boot.s platform.c"
-ldscript = "octeon.ld"
 
 # Prepend with path
 specific_files = " ".join( [ "src/platform/%s/%s" % ( platform, f ) for f in specific_files.split() ] )
-ldscript = "src/platform/%s/%s" % ( platform, ldscript )
 
 
 # Standard GCC Flags
 comp.Append(CCFLAGS = ['-W', '-Wall', '-Wno-unused-parameter', '-g', '-ffunction-sections','-fdata-sections'])
-#comp.Append(LINKFLAGS = ['-nostartfiles','-nostdlib','-T',ldscript,'-Wl,--gc-sections','-Wl,--allow-multiple-definition'])
-#comp.Append(LINKFLAGS = ['-nostartfiles' ,'-T', ldscript, '-Wl,--allow-multiple-definition', '-Wl,--gc-sections'])
 comp.Append(LINKFLAGS = ['-nostartfiles' , '-Wl,--allow-multiple-definition', '-Wl,--gc-sections'])
 comp.Append(ASFLAGS = ['-g', '-c', '-x','assembler-with-cpp','-Wall','$_CPPDEFFLAGS'])
-#comp.Append(LIBS = ['c','gcc','m'])
 if comp[ 'target' ] == 'lua':
     comp.Append(LIBS = ['m'])
 
@@ -24,7 +18,6 @@ TARGET_FLAGS = ['-g', '-march=octeon']
 comp.Prepend(CCFLAGS = [TARGET_FLAGS,'-fno-builtin'])
 comp.Prepend(LINKFLAGS = [TARGET_FLAGS,'-Wl,-e,__start'])
 comp['AS'] = toolset[ 'asm' ]  # Need to force toolset
-#comp.Prepend(ASFLAGS = ['-felf'])
 
 # Toolset data
 tools[ 'octeon' ] = {}
