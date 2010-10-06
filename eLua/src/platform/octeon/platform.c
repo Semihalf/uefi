a1abce42c0b88c0e8e1357bb4b4ea64b21eab698
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2010-10-06 23:56:09 +0000

    Fill in core platform functions
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@53716 c83883e8-9f28-0410-baf1-f2025811e16a

--- eLua/src/platform/octeon/platform.c
+++ eLua/src/platform/octeon/platform.c
@@ -19,28 +19,59 @@ int platform_init()
   return PLATFORM_OK;
 }
 
+/**
+ * Set global interrupt state
+ *
+ * @param status
+ *
+ * @return
+ */
 int platform_cpu_set_global_interrupts( int status )
 {
-    return 0;
+    int old_status;
+
+    if (status == PLATFORM_CPU_ENABLE)
+        BDK_EI(old_status);
+    else
+        BDK_DI(old_status);
+
+    return (old_status) ? PLATFORM_CPU_ENABLE : PLATFORM_CPU_DISABLE;
 }
 
+/**
+ * Get global interrupt state
+ *
+ * @return
+ */
 int platform_cpu_get_global_interrupts()
 {
-    return 0;
+    int status;
+    BDK_MF_COP0(status, COP0_STATUS);
+    return (status & 1) ? PLATFORM_CPU_ENABLE : PLATFORM_CPU_DISABLE;
 }
 
+/**
+ * Enable / disable an interrupt
+ *
+ * @param id
+ * @param resnum
+ * @param status
+ *
+ * @return
+ */
 int platform_cpu_set_interrupt( elua_int_id id, elua_int_resnum resnum, int status )
 {
+    // FIXME
     return 0;
 }
 
-int platform_cpu_get_interrupt( elua_int_id id, elua_int_resnum resnum )
-{
-    return 0;
-}
-
+/**
+ * Get the CPU frequency
+ *
+ * @return
+ */
 u32 platform_cpu_get_frequency()
 {
-    return 0;
+    return bdk_clock_get_rate(BDK_CLOCK_CORE);
 }
 
