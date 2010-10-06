99692f2f45d53c018685eb2c76ff76c3b538a624
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2010-10-06 23:56:09 +0000

    Cleanup uart platform functions
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@53717 c83883e8-9f28-0410-baf1-f2025811e16a

--- eLua/src/platform/octeon/platform-uart.c
+++ eLua/src/platform/octeon/platform-uart.c
@@ -1,11 +1,29 @@
 #include <bdk.h>
 #include "platform.h"
 
+/**
+ * Setup the UART
+ *
+ * @param id
+ * @param baud
+ * @param databits
+ * @param parity
+ * @param stopbits
+ *
+ * @return
+ */
 u32 platform_uart_setup( unsigned id, u32 baud, int databits, int parity, int stopbits )
 {
-  return 0;
+    // FIXME
+    return 0;
 }
 
+/**
+ * Send a byte to the uart
+ *
+ * @param id
+ * @param data
+ */
 void platform_uart_send( unsigned id, u8 data )
 {
     u64 lsrval;
@@ -19,6 +37,14 @@ void platform_uart_send( unsigned id, u8 data )
     BDK_CSR_WRITE(BDK_MIO_UARTX_THR(id), data);
 }
 
+/**
+ * Receive a uart byte
+ *
+ * @param id
+ * @param timeout
+ *
+ * @return
+ */
 int platform_s_uart_recv( unsigned id, s32 timeout )
 {
     if (timeout == PLATFORM_UART_INFINITE_TIMEOUT)
@@ -28,11 +54,12 @@ int platform_s_uart_recv( unsigned id, s32 timeout )
         {
             lsrval = BDK_CSR_READ(BDK_MIO_UARTX_LSR(id));
         } while ((lsrval & 1) == 0);
+        /* Read the byte */
         return BDK_CSR_READ(BDK_MIO_UARTX_RBR(id));
     }
-    else if (BDK_CSR_READ(BDK_MIO_UARTX_LSR(id)) & 1)
-        return BDK_CSR_READ(BDK_MIO_UARTX_RBR(id));
-    else
+    else if (BDK_CSR_WAIT_FOR_FIELD(BDK_MIO_UARTX_LSR(id), dr, ==, 1, timeout))
         return -1;
+    else
+        return BDK_CSR_READ(BDK_MIO_UARTX_RBR(id));
 }
 
