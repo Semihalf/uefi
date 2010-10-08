5b23359e3a025d9fa58024e7e1f8c7e1281adc65
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2010-10-08 18:55:36 +0000

    Add bdk_dprintf
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@53763 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-arch/bdk-warn.c
+++ libbdk-arch/bdk-warn.c
@@ -9,3 +9,11 @@ void bdk_warn(const char *format, ...)
     va_end(args);
 }
 
+void bdk_dprintf(const char *format, ...)
+{
+    va_list args;
+    va_start(args, format);
+    vprintf(format, args);
+    va_end(args);
+}
+
