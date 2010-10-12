17e8a7b22ece85e7a059878267fb2fa1074c3ab5
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2010-10-12 21:43:02 +0000

    Implement UART setup
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@53901 c83883e8-9f28-0410-baf1-f2025811e16a

--- eLua/src/platform/octeon/platform-uart.c
+++ eLua/src/platform/octeon/platform-uart.c
@@ -14,7 +14,24 @@
  */
 u32 platform_uart_setup( unsigned id, u32 baud, int databits, int parity, int stopbits )
 {
-    // FIXME
+    BDK_CSR_MODIFY(u, BDK_MIO_UARTX_LCR(id),
+        u.s.dlab = 1; /* Divisor Latch Address bit */
+        u.s.eps = 0; /* Even Parity Select bit */
+        u.s.pen = 0; /* Parity Enable bit */
+        u.s.stop = 0; /* Stop Control bit */
+        u.s.cls = 3); /* Character Length Select */
+
+    BDK_CSR_MODIFY(u, BDK_MIO_UARTX_FCR(id),
+        u.s.en = 1);
+
+    int divisor = bdk_clock_get_rate(BDK_CLOCK_SCLK) / baud / 16;
+    BDK_CSR_WRITE(BDK_MIO_UARTX_DLH(id), divisor>>8);
+    BDK_CSR_WRITE(BDK_MIO_UARTX_DLL(id), divisor & 0xff);
+
+    BDK_CSR_MODIFY(u, BDK_MIO_UARTX_LCR(id),
+        u.s.dlab = 0); /* Divisor Latch Address bit */
+    BDK_CSR_READ(BDK_MIO_UARTX_LCR(id));
+    bdk_wait_usec(1);
     return 0;
 }
 
