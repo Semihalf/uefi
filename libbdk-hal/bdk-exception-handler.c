1f3792eface121a5dc3819aa81110484290901f2
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2010-10-12 21:43:15 +0000

    Make eth force interrupt weak. Needs rewrite
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@53908 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-hal/bdk-exception-handler.c
+++ libbdk-hal/bdk-exception-handler.c
@@ -5,7 +5,7 @@ char bdk_exception_stack[1024];
 void bdk_exception_handler(uint64_t saved_regs[34])
 {
     // FIXME
-    extern void platform_eth_force_interrupt();
+    extern void platform_eth_force_interrupt() __attribute__ ((weak));
 
     int cause;
     BDK_MF_COP0(cause, COP0_CAUSE);
@@ -15,6 +15,7 @@ void bdk_exception_handler(uint64_t saved_regs[34])
         BDK_MF_COP0(compare, COP0_COMPARE);
         compare += bdk_clock_get_rate(BDK_CLOCK_CORE) / 10;
         BDK_MT_COP0(compare, COP0_COMPARE);
-        platform_eth_force_interrupt();
+        if (platform_eth_force_interrupt)
+            platform_eth_force_interrupt();
     }
 }
