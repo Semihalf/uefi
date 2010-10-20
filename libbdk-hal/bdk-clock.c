0394097fe9da3b7fbaf602d7a194aec48de2fc43
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2010-10-20 00:28:26 +0000

    Fix bdk_clock failure if globals are in flash
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@54103 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-hal/bdk-clock.c
+++ libbdk-hal/bdk-clock.c
@@ -2,7 +2,6 @@
 
 static uint64_t rate_eclk = 0;
 static uint64_t rate_sclk = 0;
-static uint64_t rate_dclk = 0;
 
 /**
  * Get clock rate based on the clock type.
@@ -13,21 +12,27 @@ static uint64_t rate_dclk = 0;
 uint64_t bdk_clock_get_rate(bdk_clock_t clock)
 {
     const uint64_t REF_CLOCK = 50000000;
+    uint64_t eclk = rate_eclk;
+    uint64_t sclk = rate_sclk;
 
-    if (bdk_unlikely(!rate_eclk))
+    /* This is written such that you get right answer even if globals
+        are in flash */
+    if (bdk_unlikely(!eclk || !sclk))
     {
         if (octeon_has_feature(OCTEON_FEATURE_NPEI))
         {
             BDK_CSR_INIT(npei_dbg_data, BDK_NPEI_DBG_DATA);
-            rate_eclk =  REF_CLOCK * npei_dbg_data.s.c_mul;
-            rate_sclk = rate_eclk;
+            eclk =  REF_CLOCK * npei_dbg_data.s.c_mul;
+            sclk = eclk;
         }
         else
         {
             BDK_CSR_INIT(mio_rst_boot, BDK_MIO_RST_BOOT);
-            rate_eclk =  REF_CLOCK * mio_rst_boot.s.c_mul;
-            rate_sclk = REF_CLOCK * mio_rst_boot.s.pnr_mul;
+            eclk =  REF_CLOCK * mio_rst_boot.s.c_mul;
+            sclk = REF_CLOCK * mio_rst_boot.s.pnr_mul;
         }
+        rate_eclk = eclk;
+        rate_sclk = sclk;
     }
 
     switch (clock)
@@ -35,14 +40,14 @@ uint64_t bdk_clock_get_rate(bdk_clock_t clock)
         case BDK_CLOCK_SCLK:
         case BDK_CLOCK_TIM:
         case BDK_CLOCK_IPD:
-            return rate_sclk;
+            return sclk;
 
         case BDK_CLOCK_RCLK:
         case BDK_CLOCK_CORE:
-            return rate_eclk;
+            return eclk;
 
         case BDK_CLOCK_DDR:
-            return rate_dclk;
+            return 0;
     }
 
     return 0;
