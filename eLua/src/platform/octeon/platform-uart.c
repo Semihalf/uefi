2a15ddf599ab934478d5dc52bfcf566779710e2b
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2010-10-22 19:17:25 +0000

    Make eLua serial IO do thread yields
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@54237 c83883e8-9f28-0410-baf1-f2025811e16a

--- eLua/src/platform/octeon/platform-uart.c
+++ eLua/src/platform/octeon/platform-uart.c
@@ -28,10 +28,12 @@ void platform_uart_send( unsigned id, u8 data )
     u64 lsrval;
 
     /* Spin until there is room */
-    do
+    lsrval = BDK_CSR_READ(BDK_MIO_UARTX_LSR(id));
+    while ((lsrval & (1<<5)) == 0)
     {
+        bdk_thread_yield();
         lsrval = BDK_CSR_READ(BDK_MIO_UARTX_LSR(id));
-    } while ((lsrval & (1<<5)) == 0);
+    }
     /* Write the byte */
     BDK_CSR_WRITE(BDK_MIO_UARTX_THR(id), data);
 }
@@ -46,13 +48,16 @@ void platform_uart_send( unsigned id, u8 data )
  */
 int platform_s_uart_recv( unsigned id, s32 timeout )
 {
+    bdk_thread_yield();
     if (timeout == PLATFORM_UART_INFINITE_TIMEOUT)
     {
         u64 lsrval;
-        do
+        lsrval = BDK_CSR_READ(BDK_MIO_UARTX_LSR(id));
+        while ((lsrval & 1) == 0)
         {
+            bdk_thread_yield();
             lsrval = BDK_CSR_READ(BDK_MIO_UARTX_LSR(id));
-        } while ((lsrval & 1) == 0);
+        }
         /* Read the byte */
         return BDK_CSR_READ(BDK_MIO_UARTX_RBR(id));
     }
