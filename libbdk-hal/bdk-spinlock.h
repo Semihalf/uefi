b97a70fd70f3d3d0b98f3d0a23f255caa26029e6
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2010-11-09 17:32:14 +0000

    Implement fair spinlock
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@54623 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-hal/bdk-spinlock.h
+++ libbdk-hal/bdk-spinlock.h
@@ -13,7 +13,8 @@
  * Spinlocks for Octeon
  */
 typedef struct {
-    volatile uint32_t value;
+    int32_t ticket;
+    int32_t serving;
 } bdk_spinlock_t;
 
 /**
@@ -23,21 +24,9 @@ typedef struct {
  */
 static inline void bdk_spinlock_init(bdk_spinlock_t *lock)
 {
-    lock->value = 0;
+    lock->ticket = 0;
+    lock->serving = 0;
 }
-
-
-/**
- * Return non-zero if the spinlock is currently locked
- *
- * @param lock   Lock to check
- * @return Non-zero if locked
- */
-static inline int bdk_spinlock_locked(bdk_spinlock_t *lock)
-{
-    return (lock->value != 0);
-}
-
 
 /**
  * Releases lock
@@ -46,43 +35,7 @@ static inline int bdk_spinlock_locked(bdk_spinlock_t *lock)
  */
 static inline void bdk_spinlock_unlock(bdk_spinlock_t *lock)
 {
-    BDK_SYNCW;
-    lock->value = 0;
-    BDK_SYNCW;
-}
-
-
-/**
- * Attempts to take the lock, but does not spin if lock is not available.
- * May take some time to acquire the lock even if it is available
- * due to the ll/sc not succeeding.
- *
- * @param lock   pointer to lock structure
- *
- * @return 0: lock successfully taken
- *         1: lock not taken, held by someone else
- * These return values match the Linux semantics.
- */
-
-static inline unsigned int bdk_spinlock_trylock(bdk_spinlock_t *lock)
-{
-    unsigned int tmp;
-
-    __asm__ __volatile__(
-    ".set noreorder         \n"
-    "1: ll   %[tmp], %[val] \n"
-    "   bnez %[tmp], 2f     \n"  // if lock held, fail immediately
-    "   li   %[tmp], 1      \n"
-    "   sc   %[tmp], %[val] \n"
-    "   beqz %[tmp], 1b     \n"
-    "   li   %[tmp], 0      \n"
-    "2:                     \n"
-    ".set reorder           \n"
-    : [val] "+m" (lock->value), [tmp] "=&r" (tmp)
-    :
-    : "memory");
-
-    return (!!tmp);  /* normalize to 0 or 1 */
+    bdk_atomic_add32(&lock->serving, 1);
 }
 
 /**
@@ -92,21 +45,12 @@ static inline unsigned int bdk_spinlock_trylock(bdk_spinlock_t *lock)
  */
 static inline void bdk_spinlock_lock(bdk_spinlock_t *lock)
 {
-    unsigned int tmp;
-
-    __asm__ __volatile__(
-    ".set noreorder         \n"
-    "1: ll   %[tmp], %[val]  \n"
-    "   bnez %[tmp], 1b     \n"
-    "   li   %[tmp], 1      \n"
-    "   sc   %[tmp], %[val] \n"
-    "   beqz %[tmp], 1b     \n"
-    "   nop                \n"
-    ".set reorder           \n"
-    : [val] "+m" (lock->value), [tmp] "=&r" (tmp)
-    :
-    : "memory");
-
+    int ticket = bdk_atomic_fetch_and_add32_nosync(&lock->ticket, 1);
+    BDK_SYNCW;
+    while ((volatile int32_t)lock->serving != ticket)
+    {
+        BDK_ASM_PAUSE;
+    }
 }
 
 /** @} */
