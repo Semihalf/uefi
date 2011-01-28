c85c45db3ac3d3f59dd16233c8969209d496d1eb
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2011-01-28 21:00:31 +0000

    Disable unwind stacks to save space
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@56766 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk/bdk.mk
+++ libbdk/bdk.mk
@@ -17,7 +17,7 @@ STRIP=$(CROSS)strip
 # Setup the compile flags
 #
 CPPFLAGS = -I $(BDK_ROOT)/libbdk -I $(BDK_ROOT)/liblua -I $(BDK_ROOT)/libc/mipsisa64-octeon-elf/include
-CFLAGS = -Wall -Wextra -Wno-unused-parameter -O3 -g -march=octeon2 -std=gnu99
+CFLAGS = -Wall -Wextra -Wno-unused-parameter -O3 -g -march=octeon2 -std=gnu99 -fno-asynchronous-unwind-tables
 ASFLAGS = $(CFLAGS)
 
 LDFLAGS  = -nostdlib -nostartfiles
