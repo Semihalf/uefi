73b54b34dc56cd33bfa18c0d83e57e2fb3f51a04
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2011-01-28 21:00:32 +0000

    Lock L2
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@56768 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-os/bdk-init.c
+++ libbdk-os/bdk-init.c
@@ -65,6 +65,7 @@ static void bdk_init_stage2(void)
         __bdk_init_exception();
 
         write(1, BANNER_2, sizeof(BANNER_2)-1);
+        bdk_l2c_lock_mem_region(0, bdk_l2c_get_cache_size_bytes());
 
         /* Zero BSS */
         write(1, BANNER_3, sizeof(BANNER_3)-1);
