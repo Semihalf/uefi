1fa61b86e7c00a787dca0e84495fa75566f993d7
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2011-03-03 23:57:32 +0000

    Improve Lua error reporting when something crashes the menus
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@57721 c83883e8-9f28-0410-baf1-f2025811e16a

--- rom/init.lua
+++ rom/init.lua
@@ -5,10 +5,12 @@ print("Octeon Bringup and Diagnostic Kit")
 print("Copyright (C) 2010-2011 Cavium Networks")
 print("")
 
+local debug = require("debug")
+
 -- Start the main menu.
 while true do
-    status, result = pcall(dofile, "/rom/main.lua")
+    status, result = xpcall(dofile, debug.traceback, "/rom/main.lua")
     if not status then
-        printf("ERROR: %s\n", result)
+        print("ERROR:", result)
     end
 end
