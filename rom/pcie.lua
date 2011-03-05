b43f441157e829dcd5039e72705c62f954daa454
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2011-03-05 01:02:17 +0000

    Add bus enumeration to PCIe module
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@57777 c83883e8-9f28-0410-baf1-f2025811e16a

--- rom/pcie.lua
+++ rom/pcie.lua
@@ -62,50 +62,57 @@ local PCICONFIG_PREF_LIMIT_UPPER    = 0x2c
 local PCICONFIG_IO_BASE_UPPER       = 0x30
 local PCICONFIG_IO_LIMIT_UPPER      = 0x32
 
+
+local configr8 = c_func.bdk_pcie_config_read8
+local configr16 = c_func.bdk_pcie_config_read16
+local configr32 = c_func.bdk_pcie_config_read32
+local configw8 = c_func.bdk_pcie_config_write8
+local configw16 = c_func.bdk_pcie_config_write16
+local configw32 = c_func.bdk_pcie_config_write32
+
 --
 -- This function creates a table representing a PCIe device. It reads
 -- the VENDOR and DEVICE ID. If these are undefined it returns nil as the
 -- device doesn't exist.
 --
 local function create_device(root, bus, deviceid, func)
-    local device = {}
-    device.config = {}
-    device.root = root
-    device.bus = bus
-    device.deviceid = deviceid
-    device.func = func
+    local newdev = {}
+    newdev.root = root
+    newdev.bus = bus
+    newdev.deviceid = deviceid
+    newdev.func = func
 
     --
     -- PCIe config space reads
     --
-    function device.config.read8(reg)
-        return bit32.AND(c_func.bdk_pcie_config_read8(device.root.port, device.bus, device.deviceid, device.func, reg), 0xff)
+    function newdev:read8(reg)
+        return bit32.AND(configr8(self.root.port, self.bus, self.deviceid, self.func, reg), 0xff)
     end
-    function device.config.read16(reg)
-        return bit32.AND(c_func.bdk_pcie_config_read16(device.root.port, device.bus, device.deviceid, device.func, reg), 0xffff)
+    function newdev:read16(reg)
+        return bit32.AND(configr16(self.root.port, self.bus, self.deviceid, self.func, reg), 0xffff)
     end
-    function device.config.read32(reg)
-        return bit32.AND(c_func.bdk_pcie_config_read32(device.root.port, device.bus, device.deviceid, device.func, reg), 0xffffffff)
+    function newdev:read32(reg)
+        return bit32.AND(configr32(self.root.port, self.bus, self.deviceid, self.func, reg), 0xffffffff)
     end
 
     --
     -- PCIe config space writes
     --
-    function device.config.write8(reg, val)
-        c_func.bdk_pcie_config_write8(device.root.port, device.bus, device.deviceid, device.func, reg, val)
+    function newdev:write8(reg, val)
+        configw8(self.root.port, self.bus, self.deviceid, self.func, reg, val)
     end
-    function device.config.write16(reg, val)
-        c_func.bdk_pcie_config_write16(device.root.port, device.bus, device.deviceid, device.func, reg, val)
+    function newdev:write16(reg, val)
+        configw16(self.root.port, self.bus, self.deviceid, self.func, reg, val)
     end
-    function device.config.write32(reg, val)
-        c_func.bdk_pcie_config_write32(device.root.port, device.bus, device.deviceid, device.func, reg, val)
+    function newdev:write32(reg, val)
+        configw32(self.root.port, self.bus, self.deviceid, self.func, reg, val)
     end
 
     --
     -- Scan for devices behind this one on a subordinate bus. The scan is
     -- recursive.
     --
-    function device:scan()
+    function newdev:scan()
         -- This can only be called if the device is a brdige/switch
         assert(self.isbridge)
         -- Create a table that will contain our sub devices
@@ -137,58 +144,238 @@ local function create_device(root, bus, deviceid, func)
         end
     end
 
+    local function  bar_size_compare(a, b)
+        return a[3] > b[3]
+    end
+
+    function newdev:getresources()
+        local io_bars = {}
+        local mem_bars = {}
+        local pmem_bars = {}
+        if self.isbridge then
+            for _,device in ipairs(self.devices) do
+                local io, mem, pmem = device:getresources()
+                for inp,outp in pairs({[io]=io_bars, [mem]=mem_bars, [pmem]=pmem_bars}) do
+                    local size = 0
+                    for i = 1,#inp do
+                        size = size + inp[i][3]
+                    end
+                    if size > 0 then
+                        table.insert(outp, {nil, 0, size, 0})
+                    end
+                end
+            end
+        end
+        local max_bar = PCICONFIG_CARDBUS_CIS
+        if self.isbridge then
+            max_bar = PCICONFIG_PRIMARY_BUS
+        end
+        local bar = PCICONFIG_BAR0
+        while bar < max_bar do
+            self:write32(bar, 0xffffffff)
+            local v = self:read32(bar)
+            local ismem = not bit32.TEST(v, 1)
+            local is64 = ismem and bit32.TEST(v, 4)
+            local ispref = ismem and bit32.TEST(v, 8)
+            if is64 then
+                self:write32(bar+4, 0xffffffff)
+                v = self:read32(bar+4) * 2^32 + v
+            end
+            if v ~= 0 then
+                if ispref then
+                    local size = bit32.NOT(bit32.AND(v, -16)) + 1
+                    table.insert(pmem_bars, {self, bar, size, v})
+                elseif ismem then
+                    local size = bit32.NOT(bit32.AND(v, -16)) + 1
+                    table.insert(mem_bars, {self, bar, size, v})
+                else
+                    local size = bit32.NOT(bit32.AND(v, -4)) + 1
+                    table.insert(io_bars, {self, bar, size, v})
+                end
+            end
+            if is64 then
+                bar = bar + 8
+            else
+                bar = bar + 4
+            end
+        end
+        table.sort(io_bars, bar_size_compare)
+        table.sort(mem_bars, bar_size_compare)
+        table.sort(pmem_bars, bar_size_compare)
+        return io_bars, mem_bars, pmem_bars
+    end
+
     --
     -- Assign resources to the device
     --
-    function device:enumerate(start_address)
+    function newdev:enumerate(io_address, mem_address, pmem_address)
+        local io_bars, mem_bars, pmem_bars = self:getresources()
+        for _,bar_table in ipairs({io_bars, mem_bars, pmem_bars}) do
+            for _,bar in ipairs(bar_table) do
+                local device, bar_addr, size, barv = table.unpack(bar)
+                if device then
+                    if bar_table == io_bars then
+                        if bit32.AND(io_address, size - 1) then
+                            io_address = io_address + size - 1
+                            io_address = bit32.AND(io_address, -size)
+                        end
+                        -- printf("%d:%d.%d [%x] = %x (%x bytes) [IO]\n", device.bus, device.deviceid, device.func, bar_addr, io_address, size)
+                        device:write32(bar_addr, io_address)
+                        io_address = io_address + size
+                    elseif bar_table == mem_bars then
+                        local is64 = bit32.TEST(barv, 4)
+                        if bit32.AND(mem_address, size - 1) then
+                            mem_address = mem_address + size - 1
+                            mem_address = bit32.AND(mem_address, -size)
+                        end
+                        -- printf("%d:%d.%d [%x] = %x (%x bytes) [Mem]\n", device.bus, device.deviceid, device.func, bar_addr, mem_address, size)
+                        device:write32(bar_addr, mem_address)
+                        if is64 then
+                            device:write32(bar_addr+4, mem_address / 2^32)
+                        end
+                        mem_address = mem_address + size
+                    else
+                        local is64 = bit32.TEST(barv, 4)
+                        if bit32.AND(pmem_address, size - 1) then
+                            pmem_address = pmem_address + size - 1
+                            pmem_address = bit32.AND(pmem_address, -size)
+                        end
+                        -- printf("%d:%d.%d [%x] = %x (%x bytes) [PMem]\n", device.bus, device.deviceid, device.func, bar_addr, pmem_address, size)
+                        device:write32(bar_addr, pmem_address)
+                        if is64 then
+                            device:write32(bar_addr+4, pmem_address / 2^32)
+                        end
+                        pmem_address = pmem_address + size
+                    end
+                end
+            end
+        end
+        if self.isbridge then
+            -- Align IO on a 4k boundary
+            if bit32.AND(io_address, 4095) then
+                io_address = io_address + 4095
+                io_address = bit32.AND(io_address, -4096)
+            end
+            -- Align Mem on a 1M boundary
+            if bit32.AND(mem_address, 0xfffff) then
+                mem_address = mem_address + 0xfffff
+                mem_address = bit32.AND(mem_address, -0x100000)
+            end
+            -- Align Pmem on a 1M boundary
+            if bit32.AND(pmem_address, 0xfffff) then
+                pmem_address = pmem_address + 0xfffff
+                pmem_address = bit32.AND(pmem_address, -0x100000)
+            end
+            -- printf("%d:%d.%d Bridge base IO = %x, Mem = %x, PMem = %x\n", self.bus, self.deviceid, self.func, io_address, mem_address, pmem_address)
+            -- Program IO base
+            local v = self:read8(PCICONFIG_IO_BASE)
+            self:write8(PCICONFIG_IO_BASE, bit32.SHR(io_address, 8))
+            if bit32.TEST(v, 1) then
+                self:write16(PCICONFIG_IO_BASE_UPPER, bit32.SHR(io_address, 16))
+            end
+            -- Program Mem base
+            self:write16(PCICONFIG_MEMORY_BASE, bit32.SHR(mem_address, 16))
+            -- Program Pmem base
+            local v = self:read16(PCICONFIG_PREF_BASE)
+            self:write16(PCICONFIG_PREF_BASE, bit32.SHR(pmem_address, 16))
+            if bit32.TEST(v, 1) then
+                self:write32(PCICONFIG_PREF_BASE_UPPER, bit32.SHR(pmem_address, 32))
+            end
+            for _,device in ipairs(self.devices) do
+                io_address, mem_address, pmem_address = device:enumerate(io_address, mem_address, pmem_address)
+            end
+            -- printf("%d:%d.%d Bridge limit IO = %x, Mem = %x, PMem = %x\n", self.bus, self.deviceid, self.func, io_address, mem_address, pmem_address)
+            -- Program IO limit
+            local v = self:read8(PCICONFIG_IO_LIMIT)
+            self:write8(PCICONFIG_IO_LIMIT, bit32.SHR(io_address-1, 8))
+            if bit32.TEST(v, 1) then
+                self:write8(PCICONFIG_IO_LIMIT_UPPER, bit32.SHR(io_address-1, 16))
+            end
+            -- Program Mem limit
+            self:write16(PCICONFIG_MEMORY_LIMIT, bit32.SHR(mem_address-1, 16))
+            -- Program Pmem limit
+            local v = self:read16(PCICONFIG_PREF_LIMIT)
+            self:write16(PCICONFIG_PREF_LIMIT, bit32.SHR(pmem_address-1, 16))
+            if bit32.TEST(v, 1) then
+                self:write32(PCICONFIG_PREF_LIMIT_UPPER, bit32.SHR(pmem_address-1, 32))
+            end
+        end
+        return io_address, mem_address, pmem_address
     end
 
     --
     -- Display a device and all children
     --
-    function device:display(indent)
+    function newdev:display(indent)
         if not indent then
             indent = ""
         end
-        local t = device.config.read32(PCICONFIG_CLASS_CODE_REV_ID)
+        local t = self:read32(PCICONFIG_CLASS_CODE_REV_ID)
         printf("%s%d:%d.%d Class:%06x Vendor:%04x Device:%04x (Rev %02x)\n",
             indent,
             self.bus,
             self.deviceid,
             self.func,
-            bit32.SAR(t, 8),
-            device.config.read16(PCICONFIG_VENDOR_ID),
-            device.config.read16(PCICONFIG_DEVICE_ID),
+            bit32.SHR(t, 8),
+            self:read16(PCICONFIG_VENDOR_ID),
+            self:read16(PCICONFIG_DEVICE_ID),
             bit32.AND(t, 0xff))
         printf("%s    Command:%04x Status:%04x\n",
             indent,
-            device.config.read16(PCICONFIG_COMMAND),
-            device.config.read16(PCICONFIG_STATUS))
+            self:read16(PCICONFIG_COMMAND),
+            self:read16(PCICONFIG_STATUS))
         printf("%s    Cache Line Size:%02x Master Latency Timer:%02x Header Type:%02x BIST:%02x\n",
             indent,
-            device.config.read8(PCICONFIG_CACHE_LINE_SIZE),
-            device.config.read8(PCICONFIG_MASTER_LT),
-            device.config.read8(PCICONFIG_HEADER_TYPE),
-            device.config.read8(PCICONFIG_BIST))
+            self:read8(PCICONFIG_CACHE_LINE_SIZE),
+            self:read8(PCICONFIG_MASTER_LT),
+            self:read8(PCICONFIG_HEADER_TYPE),
+            self:read8(PCICONFIG_BIST))
         printf("%s    Cardbus CIS Pointer:%08x\n",
             indent,
-            device.config.read32(PCICONFIG_CARDBUS_CIS))
+            self:read32(PCICONFIG_CARDBUS_CIS))
         printf("%s    Subsystem Vendor:%04x Device:%04x\n",
             indent,
-            device.config.read16(PCICONFIG_SUBVENDOR_ID),
-            device.config.read16(PCICONFIG_SUBDEVICE_ID))
+            self:read16(PCICONFIG_SUBVENDOR_ID),
+            self:read16(PCICONFIG_SUBDEVICE_ID))
         printf("%s    Expansion ROM:%08x\n",
             indent,
-            device.config.read32(PCICONFIG_ROM))
+            self:read32(PCICONFIG_ROM))
         printf("%s    Capabilities Pointer:%02x\n",
             indent,
-            device.config.read8(PCICONFIG_CAP_PTR))
+            self:read8(PCICONFIG_CAP_PTR))
         printf("%s    Interrupt Line:%02x Interrupt Pin:%02x Min Grant:%02x Max Latency:%02x\n",
             indent,
-            device.config.read8(PCICONFIG_INT_LINE),
-            device.config.read8(PCICONFIG_INT_PIN),
-            device.config.read8(PCICONFIG_MIN_GNT),
-            device.config.read8(PCICONFIG_MAX_LAT))
+            self:read8(PCICONFIG_INT_LINE),
+            self:read8(PCICONFIG_INT_PIN),
+            self:read8(PCICONFIG_MIN_GNT),
+            self:read8(PCICONFIG_MAX_LAT))
+        if self.isbridge then
+            printf("%s    Primary Bus:%02x Secondary Bus:%02x Subordinate Bus:%02x Secondary Latency Timer:%02x\n",
+                indent,
+                self:read8(PCICONFIG_PRIMARY_BUS),
+                self:read8(PCICONFIG_SECONDARY_BUS),
+                self:read8(PCICONFIG_SUBORIDINATE_BUS),
+                self:read8(PCICONFIG_SECONDARY_LAT))
+            printf("%s    Secondary Status:%04x\n",
+                indent,
+                self:read16(PCICONFIG_SECONDARY_STATUS))
+            printf("%s    IO Base:%04x%02x00 Limit:%04x%02xff\n",
+                indent,
+                self:read16(PCICONFIG_IO_BASE_UPPER),
+                bit32.AND(self:read8(PCICONFIG_IO_BASE), -16),
+                self:read16(PCICONFIG_IO_LIMIT_UPPER),
+                bit32.OR(self:read8(PCICONFIG_IO_LIMIT), 0xf))
+            printf("%s    Memory Base:%02x0000 Limit:%02xffff\n",
+                indent,
+                bit32.AND(self:read16(PCICONFIG_MEMORY_BASE), -16),
+                bit32.OR(self:read16(PCICONFIG_MEMORY_LIMIT), 0xf))
+            printf("%s    Prefetchable Memory Base:%04x%02x0000 Limit:%04x%02xffff\n",
+                indent,
+                self:read32(PCICONFIG_PREF_BASE_UPPER),
+                bit32.AND(self:read16(PCICONFIG_PREF_BASE), -16),
+                self:read32(PCICONFIG_PREF_LIMIT_UPPER),
+                bit32.OR(self:read16(PCICONFIG_PREF_LIMIT), 0xf))
+        end
         local max_bar = PCICONFIG_CARDBUS_CIS
         if self.isbridge then
             max_bar = PCICONFIG_PRIMARY_BUS
@@ -196,15 +383,27 @@ local function create_device(root, bus, deviceid, func)
         local bar = PCICONFIG_BAR0
         local barno = 0
         while bar < max_bar do
-            local v = device.config.read32(bar)
+            local v = self:read32(bar)
             local ismem = not bit32.TEST(v, 1)
             local is64 = ismem and bit32.TEST(v, 4)
+            local ispref = ismem and bit32.TEST(v, 8)
+            local display_type = "IO"
+            if ismem then
+                display_type = "Memory"
+                if ispref then
+                    display_type = "Prefetchable Memory"
+                end
+            end
             if is64 then
-                v = device.config.read32(bar+4) * 2^32 + v
-                printf("%s    BAR%d: %016x\n", indent, barno, v)
+                v = self:read32(bar+4) * 2^32 + v
+                if v ~= 0 then
+                    printf("%s    BAR%d: %016x [%s]\n", indent, barno, v, display_type)
+                end
                 bar = bar + 8
             else
-                printf("%s    BAR%d: %08x\n", indent, barno, v)
+                if v ~= 0 then
+                    printf("%s    BAR%d: %08x [%s]\n", indent, barno, v, display_type)
+                end
                 bar = bar + 4
             end
             barno = barno + 1
@@ -218,35 +417,35 @@ local function create_device(root, bus, deviceid, func)
     end
 
     -- Read the VENDOR and DEVICE ID
-    device.did = device.config.read32(PCICONFIG_VENDOR_ID)
+    newdev.did = newdev:read32(PCICONFIG_VENDOR_ID)
     -- Fail create if device didn't respond
-    if device.did == 0xffffffff then
+    if newdev.did == 0xffffffff then
         return nil
     end
-    -- printf("PCICONFIG_VENDOR_ID(port=%d, bus=%d, deviceid=%d, func=%d) 0x%x\n", device.root.port, device.bus, device.deviceid, device.func, device.did)
+    --printf("%d:%d.%d Found %08x\n", newdev.bus, newdev.deviceid, newdev.func, newdev.did)
 
     -- Figure out if the device supports multiple functions and/or
     -- if it is a switch
-    local header_type = device.config.read8(PCICONFIG_HEADER_TYPE)
-    device.ismultifunction = bit32.TEST(header_type, 0x80)
-    device.isbridge = bit32.AND(header_type, 0x7f) == 1
-    if device.isbridge then
+    local header_type = newdev:read8(PCICONFIG_HEADER_TYPE)
+    newdev.ismultifunction = bit32.TEST(header_type, 0x80)
+    newdev.isbridge = bit32.AND(header_type, 0x7f) == 1
+    if newdev.isbridge then
         -- Device is a bridge/switch. Assign bus numbers so we can
         -- scan for subordinate devices
-        device.config.write8(PCICONFIG_PRIMARY_BUS, device.bus)
+        newdev:write8(PCICONFIG_PRIMARY_BUS, newdev.bus)
         root.last_bus = root.last_bus + 1
-        device.busnum = root.last_bus
-        device.config.write8(PCICONFIG_SECONDARY_BUS, device.busnum)
+        newdev.busnum = root.last_bus
+        newdev:write8(PCICONFIG_SECONDARY_BUS, newdev.busnum)
         -- Set to max value as we don't know how many busses are there
-        device.config.write8(PCICONFIG_SUBORIDINATE_BUS, 0xff)
+        newdev:write8(PCICONFIG_SUBORIDINATE_BUS, 0xff)
         -- Scan for children
-        device:scan()
+        newdev:scan()
         -- Update the last bus number now that we know how many busses
         -- were there
-        device.config.write8(PCICONFIG_SUBORIDINATE_BUS, root.last_bus)
+        newdev:write8(PCICONFIG_SUBORIDINATE_BUS, root.last_bus)
     end
     -- Return the device
-    return device
+    return newdev
 end
 
 --
@@ -302,10 +501,12 @@ function pcie.initialize(pcie_port)
         if #self.devices == 0 then
             self:scan()
         end
-        local start_address = 0xf0000000
+        local io_address = 0x1000
+        local mem_address = 0xe0000000
+        local pmem_address = 0xf0000000
         -- Assign resources to all top level devices
         for _,device in ipairs(self.devices) do
-            start_address = device:enumerate(start_address)
+            io_address, mem_address = device:enumerate(io_address, mem_address, pmem_address)
         end
     end
 
