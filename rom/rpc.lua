dab47d2307e319ff2496125c1a4e8116ebb77466
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2011-03-22 23:18:41 +0000

    Set the baud rate and such for RPC if the filename starts with a slash.
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@58337 c83883e8-9f28-0410-baf1-f2025811e16a

--- rom/rpc.lua
+++ rom/rpc.lua
@@ -47,6 +47,11 @@ local function getconnection(stream, is_input)
                 error("socket:connect(): " .. message)
             end
         else
+            -- Set the baud rate for filenames that start with "/". This
+            -- is a best guess of what should work for people
+            if stream:sub(1,1) == "/" then
+                os.execute("stty -F " .. stream .. " 115200 crtscts -cstopb -parenb")
+            end
             -- Open for RW in case we are running over Pipes. Linux pipes
             -- block until someone connects to the other side unless the
             -- pipe is openned RW.
