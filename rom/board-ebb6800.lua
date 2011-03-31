f68424d39953ac6cf13a2f4dbf0e4120d1e67f46
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2011-03-31 18:54:47 +0000

    Configure the MDIO switch so we can access all SGMII ports on all QLMs.
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@58642 c83883e8-9f28-0410-baf1-f2025811e16a

--- rom/board-ebb6800.lua
+++ rom/board-ebb6800.lua
@@ -1,10 +1,23 @@
 
 print("Configuring for the EBB6800")
 
+-- Configure MDIO mux to connect MGMT port _and_ QLM4 to MDIO 0.
+-- Connect just QLM0 to MDIO 1, as only QLM0 has GMX interfaces.
+octeon.c.bdk_twsix_write_ia(0, 0x71, 0, 1, 1, 0xf3)
+octeon.c.bdk_wait_usec(11000)
+
 local set_config = octeon.c.bdk_config_set
 
 -- Mgmt port PHY address
-set_config(octeon.CONFIG_PHY_MGMT_PORT0, 6)
+set_config(octeon.CONFIG_PHY_MGMT_PORT0, 0x000 + 6)
+
+-- This is SGMII on QLM0
+set_config(octeon.CONFIG_PHY_IF0_PORT0, 0x100 + 1)
+set_config(octeon.CONFIG_PHY_IF0_PORT1, 0x100 + 2)
+set_config(octeon.CONFIG_PHY_IF0_PORT2, 0x100 + 3)
+set_config(octeon.CONFIG_PHY_IF0_PORT3, 0x100 + 4)
+
+-- QLM1 doesn't do SGMII, so no PHY addresses
 
 -- This is SGMII on QLM2
 set_config(octeon.CONFIG_PHY_IF2_PORT0, 0x200 + 1)
@@ -17,4 +30,10 @@ set_config(octeon.CONFIG_PHY_IF3_PORT0, 0x300 + 1)
 set_config(octeon.CONFIG_PHY_IF3_PORT1, 0x300 + 2)
 set_config(octeon.CONFIG_PHY_IF3_PORT2, 0x300 + 3)
 set_config(octeon.CONFIG_PHY_IF3_PORT3, 0x300 + 4)
+
+-- This is SGMII on QLM4
+set_config(octeon.CONFIG_PHY_IF4_PORT0, 0x000 + 1)
+set_config(octeon.CONFIG_PHY_IF4_PORT1, 0x000 + 2)
+set_config(octeon.CONFIG_PHY_IF4_PORT2, 0x000 + 3)
+set_config(octeon.CONFIG_PHY_IF4_PORT3, 0x000 + 4)
 
