c1ceee423b384dcbac74e5c7bb0977b8003a5bc6
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2011-04-22 22:35:06 +0000

    Update register names to match the n32/n64 calling convention instead of o32.
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@59466 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-os/bdk-exception-handler.c
+++ libbdk-os/bdk-exception-handler.c
@@ -4,7 +4,7 @@
 char bdk_exception_stack[1024];
 
 static const char reg_names[][3] = {  "r0","at","v0","v1","a0","a1","a2","a3",
-                                      "t0","t1","t2","t3","t4","t5","t6","t7",
+                                      "a4","a5","a6","a7","t0","t1","t2","t3",
                                       "s0","s1","s2","s3","s4","s5","s6","s7",
                                       "t8","t9","k0","k1","gp","sp","s8","ra" };
 /* Textual descriptions of cause codes */
