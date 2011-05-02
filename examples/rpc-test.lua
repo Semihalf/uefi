3de75519adf20e97c757f1f0c825ea4d38f2502c
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2011-05-02 22:25:36 +0000

    Cleanup the RPC test to be prettier and use the new host octoen module
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@59765 c83883e8-9f28-0410-baf1-f2025811e16a

--- examples/rpc-test.lua
+++ examples/rpc-test.lua
@@ -1,31 +1,19 @@
-if #arg < 1 then
-    print("You must specify the file to do RPC over. Example /dev/ttyS0")
-    return
-end
+require("octeon")
 
-rpc = require("rpc")
-r = rpc.connect(arg[1], arg[2])
-
-for k in r.octeon.csr() do
+-- Print out all the CSR names
+for k in octeon.csr() do
     print(k)
 end
 
-count = 0
-while true do
-    count = count + 1
+-- Loop the following a hundred times as a simple test
+for count=1,100 do
     print("Loop:", count)
-    print (r._VERSION)
-    r.foo = {1, 2, 3 ,4}
-    print("Length of new table:", #r.foo)
-    r.print("Hello World")
-    --r.print(1, 2, nil, "foo", {foo=1}, true, false)
-
-    print("Octeon RCLK:", r.octeon.c.bdk_clock_get_rate(0))
-    print("Octeon SCLK:", r.octeon.c.bdk_clock_get_rate(1))
-    print("Octeon CIU_FUSE:", r.octeon.csr.ciu_fuse.read())
-    print("Is CN63XX?", r.octeon.is_model(r.octeon.CN63XX))
-    print("Is CN68XX?", r.octeon.is_model(r.octeon.CN68XX))
-    print("Lua mem:", r.collectgarbage("count"))
-    print("C mem:", r.octeon.get_sbrk() / 1024)
+    print("Octeon RCLK:", octeon.c.bdk_clock_get_rate(0))
+    print("Octeon SCLK:", octeon.c.bdk_clock_get_rate(1))
+    print("Octeon CIU_FUSE:", octeon.csr.ciu_fuse.read())
+    print("Is CN63XX?", octeon.is_model(octeon.CN63XX))
+    print("Is CN68XX?", octeon.is_model(octeon.CN68XX))
+    print("Lua mem:", octeon.global.collectgarbage("count"))
+    print("C mem:", octeon.get_sbrk() / 1024)
     collectgarbage()
 end
