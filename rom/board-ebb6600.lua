b398c2ba8d08eb34dc67ae7ccc04fe743aa3a24f
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2011-07-12 22:09:20 +0000

    Minor comment updates for SGMII PHY addresses. Need to add support for MDIO switch.
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@62235 c83883e8-9f28-0410-baf1-f2025811e16a

--- rom/board-ebb6600.lua
+++ rom/board-ebb6600.lua
@@ -5,12 +5,15 @@ local set_config = octeon.c.bdk_config_set
 
 set_config(octeon.CONFIG_PHY_MGMT_PORT0, 4) -- Mgmt port 0 PHY address
 set_config(octeon.CONFIG_PHY_MGMT_PORT1, 5) -- Mgmt port 1 PHY address
-set_config(octeon.CONFIG_PHY_IF0_PORT0, 256 + 1) -- QLM1 SGMII port 0 PHY address
-set_config(octeon.CONFIG_PHY_IF0_PORT1, 256 + 2) -- QLM1 SGMII port 1 PHY address
-set_config(octeon.CONFIG_PHY_IF0_PORT2, 256 + 3) -- QLM1 SGMII port 2 PHY address
-set_config(octeon.CONFIG_PHY_IF0_PORT3, 256 + 4) -- QLM1 SGMII port 3 PHY address
-set_config(octeon.CONFIG_PHY_IF1_PORT0, 1) -- QLM2 SGMII port 0 PHY address
-set_config(octeon.CONFIG_PHY_IF1_PORT1, 2) -- QLM2 SGMII port 1 PHY address
-set_config(octeon.CONFIG_PHY_IF1_PORT2, 3) -- QLM2 SGMII port 2 PHY address
-set_config(octeon.CONFIG_PHY_IF1_PORT3, 4) -- QLM2 SGMII port 3 PHY address
+-- These are behind a MDIO switch that must choose which QLM to access
+-- Option 1
+set_config(octeon.CONFIG_PHY_IF0_PORT0, 256 + 1) -- QLM2 SGMII port 0 PHY address
+set_config(octeon.CONFIG_PHY_IF0_PORT1, 256 + 2) -- QLM2 SGMII port 1 PHY address
+set_config(octeon.CONFIG_PHY_IF0_PORT2, 256 + 3) -- QLM2 SGMII port 2 PHY address
+set_config(octeon.CONFIG_PHY_IF0_PORT3, 256 + 4) -- QLM2 SGMII port 3 PHY address
+-- Option 2
+set_config(octeon.CONFIG_PHY_IF1_PORT0, 256 + 1) -- QLM1 SGMII port 0 PHY address
+set_config(octeon.CONFIG_PHY_IF1_PORT1, 256 + 2) -- QLM1 SGMII port 1 PHY address
+set_config(octeon.CONFIG_PHY_IF1_PORT2, 256 + 3) -- QLM1 SGMII port 2 PHY address
+set_config(octeon.CONFIG_PHY_IF1_PORT3, 256 + 4) -- QLM1 SGMII port 3 PHY address
 
