d1dbc902eca48736f288ecb8360fb76d7daf145e
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2011-08-15 23:13:08 +0000

    Delete shallow_copy as RPC now does this by default
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@63491 c83883e8-9f28-0410-baf1-f2025811e16a

--- lua-modules/octeon.lua
+++ lua-modules/octeon.lua
@@ -31,25 +31,7 @@ The special value of "remote" uses the OCTEON_REMOTE_PROTOCOL to
 access the remote console.
 ]])
 local remote = rpc.connect(cnx)
-local remote_octeon = remote.octeon
-
--- When we are iterating tables we really want to iterate on the remote table,
--- so we need a pointer to the remote pairs() function so we can call it.
-local remote_pairs = remote.pairs
-
--- Copy a remote table into a local table by making each local field
--- Point to the remote field
-local function shallow_copy(remote_table)
-    local result = {}
-    for name, value in remote_pairs(remote_table) do
-        result[name] = value
-    end
-    return result
-end
-
--- Perform a shallow copy so the local octeon module
--- points to all the members of the remote octeon module.
-octeon = shallow_copy(remote_octeon)
+octeon = remote.octeon
 
 -- Create a local fake readline as calling the remote one would cause issues
 function octeon.readline(prompt, complete, timeout_us)
@@ -60,16 +42,6 @@ end
 -- Add a global member that has access to the global context on the remote
 -- system
 octeon.global = remote
-
--- C functions are used often, so copy their table so we avoid an extra
--- RPC call to lookup the function names later
-octeon.c = shallow_copy(remote_octeon.c)
-
---- Make a local copy of the trafficgen member
-octeon.trafficgen = shallow_copy(remote_octeon.trafficgen)
-
---- Make a local copy of the perf member
-octeon.perf = shallow_copy(remote_octeon.perf)
 
 return octeon
 
