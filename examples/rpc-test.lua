886ae69ca922dccab70aeaea996df46e84a4f5d8
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2011-08-15 16:14:37 +0000

    Fix case of CSR CIU_FUSE
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@63473 c83883e8-9f28-0410-baf1-f2025811e16a

--- examples/rpc-test.lua
+++ examples/rpc-test.lua
@@ -10,7 +10,7 @@ for count=1,100 do
     print("Loop:", count)
     print("Octeon RCLK:", octeon.c.bdk_clock_get_rate(0))
     print("Octeon SCLK:", octeon.c.bdk_clock_get_rate(1))
-    print("Octeon CIU_FUSE:", octeon.csr.ciu_fuse.read())
+    print("Octeon CIU_FUSE:", octeon.csr.CIU_FUSE.read())
     print("Is CN63XX?", octeon.is_model(octeon.CN63XX))
     print("Is CN68XX?", octeon.is_model(octeon.CN68XX))
     print("Lua mem:", octeon.global.collectgarbage("count"))
