d23a16dcb8137f516e8a7c9231c4aca8bae37183
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2011-08-25 19:20:11 +0000

    Create binary images for minimal and full that contain different components
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@63964 c83883e8-9f28-0410-baf1-f2025811e16a

--- bdk-boot/bdk-common.c
+++ bdk-boot/bdk-common.c
@@ -58,31 +58,3 @@ int main(void)
     return 0;
 }
 
-/**
- * This function is not defined by the BDK libraries. It must be
- * defined by all BDK applications. It should be empty except for
- * containing BDK_REQUIRE() lines. The bdk-init code has a strong
- * reference to bdk_requires_depends() which then contains strong
- * references to all needed components.
- */
-void __bdk_require_depends(void)
-{
-    BDK_REQUIRE(PCIE);
-    BDK_REQUIRE(FS_PCIE);
-    BDK_REQUIRE(SRIO);
-    BDK_REQUIRE(FS_SRIO);
-    BDK_REQUIRE(GPIO);
-    BDK_REQUIRE(RNG);
-    BDK_REQUIRE(TRA);
-    BDK_REQUIRE(KEY_MEMORY);
-    BDK_REQUIRE(DRAM_TEST);
-    BDK_REQUIRE(ENVIRONMENT);
-    BDK_REQUIRE(LWIP);
-    BDK_REQUIRE(FS_TCP);
-    BDK_REQUIRE(TELNET);
-    BDK_REQUIRE(TFTP);
-    BDK_REQUIRE(FS_XMODEM);
-    BDK_REQUIRE(FS_RAM);
-    BDK_REQUIRE(CSR_DB);
-}
-
