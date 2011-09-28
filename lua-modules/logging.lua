269c229d9d5e31cc220c4cb96e63d07ad00070f4
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2011-09-28 23:59:43 +0000

    Add Ldoc documentation markup to logging module. This is my first experiment with Ldoc.
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@65513 c83883e8-9f28-0410-baf1-f2025811e16a

--- lua-modules/logging.lua
+++ lua-modules/logging.lua
@@ -1,11 +1,19 @@
+--- Module for logging data in a uniform way with debug levels.
+-- 1. Create a new logger object by calling logging.new().
+-- 2. Then use logger methods to send output.
+-- 3. Only messages below the log level are displayed.
+-- @alias logging
 require("utils")
 
 local logging = {}
-logging.DEBUG = 3   -- Logs all information including debug
-logging.INFO = 2    -- Logs informational, warnings, errors, and fatal
-logging.WARN = 1    -- Logs warnings, errors, and fatal. This is the default.
+--- Logs all information including debug
+logging.DEBUG = 3
+--- Logs informational, warnings, errors, and fatal
+logging.INFO = 2
+--- Logs warnings, errors, and fatal. This is the default.
+logging.WARN = 1
 
---
+---
 -- Internal function that actually writes the message
 --
 local function log_write(logger, level, arg1, ...)
@@ -17,8 +25,14 @@ local function log_write(logger, level, arg1, ...)
     end
 end
 
---
+--- @class logger
+-- Object returned by new()
+
+---
 -- Called to log DEBUG messages
+-- @function logger:debug
+-- @param format printf style format string
+-- @param ... Arguments to format string
 --
 local function log_debug(logger, arg1, ...)
     if logger.level >= logging.DEBUG then
@@ -26,8 +40,11 @@ local function log_debug(logger, arg1, ...)
     end
 end
 
---
+---
 -- Called to log INFO messages
+-- @function logger:info
+-- @param format printf style format string
+-- @param ... Arguments to format string
 --
 local function log_info(logger, arg1, ...)
     if logger.level >= logging.INFO then
@@ -35,31 +52,43 @@ local function log_info(logger, arg1, ...)
     end
 end
 
---
+---
 -- Called to log WARN messages
+-- @function logger:warn
+-- @param format printf style format string
+-- @param ... Arguments to format string
 --
 local function log_warn(logger, arg1, ...)
     log_write(logger, "WARN", arg1, ...)
 end
 
---
+---
 -- Called to log ERROR messages
+-- @function logger:error
+-- @param format printf style format string
+-- @param ... Arguments to format string
 --
 local function log_error(logger, arg1, ...)
     log_write(logger, "ERROR", arg1, ...)
 end
 
---
+---
 -- Called to log FATAL messages
+-- @function logger:fatal
+-- @param format printf style format string
+-- @param ... Arguments to format string
 --
 local function log_fatal(logger, arg1, ...)
     log_write(logger, "FATAL", arg1, ...)
     error("FATAL message logged")
 end
 
---
+---
 -- Create a new logger for a unit. If level isn't specified it defaults
 -- to logging.WARN.
+-- @param unit String name for object/unit that messages are for.
+-- @param level Level of debug output to log
+-- @return New "logger" object
 --
 function logging.new(unit, level)
     local logger = {}
