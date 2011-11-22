391d6c3343e2f0c4c6571d1493089553df018063
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2011-11-22 19:35:12 +0000

    Improve configuration of CN61XX QLMs based on better information. There msut be a better wat to do this as this will be quite error prone as more chips are added.
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@67288 c83883e8-9f28-0410-baf1-f2025811e16a

--- lua-modules/qlm_menu.lua
+++ lua-modules/qlm_menu.lua
@@ -13,38 +13,217 @@ local function show_config(qlm_num)
         config.speed % 1000);
 end
 
-local function set_config(qlm_num)
-    assert(octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD == 15, "QLM is already configured")
-    local function sgmii_config(qlm_num)
+local function set_config_cn61xx()
+    -- Check if measured is within 10% of the reference
+    local function is_ref_clock(measured, reference_mhz)
+        local mhz = measured / 1000000
+        local range = reference_mhz / 10
+        return (mhz >= reference_mhz - mhz) and (mhz <= reference_mhz + mhz)
+    end
+
+    -- Set the speed of either QLM0 or QLM2. Doesn't support QLM1
+    local function set_qlm02_speed(qlm_num, speed)
+        local ref_clock = qlm.measure_clock(qlm_num)
+        if is_ref_clock(ref_clock, 100) then
+            -- Using 100Mhz reference
+            if speed == "1250" then
+                octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 3
+            elseif speed == "2500" then
+                octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 2
+            elseif speed == "3125" then
+                error("3.125 GBaud not supported with a 100Mhz reference clock")
+            elseif speed == "3750" then
+                error("3.750 GBaud not supported with a 100Mhz reference clock")
+            elseif speed == "5000" then
+                octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 0
+            elseif speed == "6250" then
+                error("6.250 GBaud not supported with a 100Mhz reference clock")
+            else
+                error("Unexpected speed selection")
+            end
+
+        elseif is_ref_clock(ref_clock, 125) then
+            -- Using 125Mhz reference
+            if speed == "1250" then
+                octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 10
+            elseif speed == "2500" then
+                octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 9
+            elseif speed == "3125" then
+                octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 8
+            elseif speed == "3750" then
+                error("3.750 GBaud not supported with a 125Mhz reference clock")
+            elseif speed == "5000" then
+                octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 6
+            elseif speed == "6250" then
+                octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 5
+            else
+                error("Unexpected speed selection")
+            end
+        elseif is_ref_clock(ref_clock, 156) then
+            -- Using 156.25Mhz reference
+            if speed == "1250" then
+                octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 4
+            elseif speed == "2500" then
+                octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 7
+            elseif speed == "3125" then
+                octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 14
+            elseif speed == "3750" then
+                octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 13
+            elseif speed == "5000" then
+                octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 11
+            elseif speed == "6250" then
+                octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 12
+            else
+                error("Unexpected speed selection")
+            end
+        else
+            error("QLM0 reference clock is not one of the expected values. Measured: " .. tostring(ref_clock) .. " Hz")
+        end
+    end
+
+    --
+    -- Configure QLM0
+    --
+    local m = menu.new("QLM0 Mode")
+    m:item("pcie",  "PCIe")
+    m:item("sgmii",  "SGMII")
+    m:item("xaui",  "XAUI or DXAUI")
+    m:item("disabled",  "Disable QLM")
+    local qlm0_mode = m:show()
+    local qlm_num = 0
+
+    if qlm0_mode == "pcie" then
+        -- Ask the user if they want gen1 or gen2 so we can figure out
+        -- the clock speed
+        local gen2 = menu.prompt_yes_no("Configure PCIe for gen2")
+        set_qlm02_speed(qlm_num, gen2 and "5000" or "2500")
+        octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_CFG = 0
+
+    elseif qlm0_mode == "sgmii" then
+        -- Only support the three common speeds for running SGMII
+        local m = menu.new("QLM0 Speed")
+        m:item("1250",  "1.250 GBaud")
+        m:item("2500",  "2.500 GBaud")
+        m:item("3125",  "3.125 GBaud")
+        local speed = m:show()
+        set_qlm02_speed(qlm_num, speed)
         octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_CFG = 2
-        octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 3
-    end
 
-    local function xaui_config(qlm_num)
+    elseif qlm0_mode == "xaui" then
+        -- Support all speeds for XAUI
+        local m = menu.new("QLM0 Speed")
+        m:item("1250",  "1.250 GBaud")
+        m:item("2500",  "2.500 GBaud")
+        m:item("3125",  "3.125 GBaud")
+        m:item("3750",  "3.750 GBaud")
+        m:item("5000",  "5.000 GBaud")
+        m:item("6250",  "6.250 GBaud")
+        local speed = m:show()
+        set_qlm02_speed(qlm_num, speed)
         octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_CFG = 3
-        octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 8
+    else
+        -- Force mode of SGMII when disabling just to have a default
+        octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_CFG = 2
+        octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 15
     end
 
-    local function dxaui_config(qlm_num)
+    --
+    -- Configure QLM1
+    --
+    local m = menu.new("QLM1 Mode")
+    m:item("1x2",  "Single PCIe with two lanes")
+    if qlm0_mode ~= "pcie" then
+        m:item("2x1",  "Dual PCIe with one lane each")
+    end
+    m:item("disabled",  "Disable QLM")
+    local qlm1_mode = m:show()
+    local qlm_num = 1
+
+    if qlm1_mode == "1x2" then
+        -- Ask the user if they want gen1 or gen2 so we can figure out
+        -- the clock speed
+        local gen2 = menu.prompt_yes_no("Configure PCIe for gen2")
+        local ref_clock = qlm.measure_clock(qlm_num)
+        if is_ref_clock(ref_clock, 100) then
+            octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = gen2 and 1 or 2
+        elseif is_ref_clock(ref_clock, 125) then
+            octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = gen2 and 4 or 6
+        else
+            error("QLM0 reference clock is not one of the expected values. Measured: " .. tostring(ref_clock) .. " Hz")
+        end
+        octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_CFG = 0
+    elseif qlm1_mode == "2x1" then
+        -- Ask the user if they want gen1 or gen2 so we can figure out
+        -- the clock speed
+        local pem0_gen2 = menu.prompt_yes_no("Configure PCIe port 0 for gen2")
+        local pem1_gen2 = menu.prompt_yes_no("Configure PCIe port 1 for gen2")
+        local ref_clock = qlm.measure_clock(qlm_num)
+        if is_ref_clock(ref_clock, 100) then
+            if pem0_gen2 and pem1_gen2 then
+                octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 0
+            elseif pem0_gen2 then
+                octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 2
+            elseif pem1_gen2 then
+                octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 1
+            else
+                octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 3
+            end
+        elseif is_ref_clock(ref_clock, 125) then
+            if pem0_gen2 and pem1_gen2 then
+                octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 4
+            elseif pem0_gen2 then
+                octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 6
+            elseif pem1_gen2 then
+                octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 9
+            else
+                octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 7
+            end
+        else
+            error("QLM0 reference clock is not one of the expected values. Measured: " .. tostring(ref_clock) .. " Hz")
+        end
+        octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_CFG = 1
+    else
+        -- Force mode of 1x2 when disabling just to have a default
+        octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_CFG = 0
+        octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 15
+    end
+
+    --
+    -- Configure QLM2
+    --
+    local m = menu.new("QLM2 Mode")
+    m:item("sgmii",  "SGMII")
+    m:item("xaui",  "XAUI or DXAUI")
+    m:item("disabled",  "Disable QLM")
+    local qlm2_mode = m:show()
+    local qlm_num = 2
+    if qlm0_mode == "sgmii" then
+        -- Only support the three common speeds for running SGMII
+        local m = menu.new("QLM0 Speed")
+        m:item("1250",  "1.250 GBaud")
+        m:item("2500",  "2.500 GBaud")
+        m:item("3125",  "3.125 GBaud")
+        local speed = m:show()
+        set_qlm02_speed(qlm_num, speed)
+        octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_CFG = 2
+
+    elseif qlm0_mode == "xaui" then
+        -- Support all speeds for XAUI
+        local m = menu.new("QLM0 Speed")
+        m:item("1250",  "1.250 GBaud")
+        m:item("2500",  "2.500 GBaud")
+        m:item("3125",  "3.125 GBaud")
+        m:item("3750",  "3.750 GBaud")
+        m:item("5000",  "5.000 GBaud")
+        m:item("6250",  "6.250 GBaud")
+        local speed = m:show()
+        set_qlm02_speed(qlm_num, speed)
         octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_CFG = 3
-        octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 5
+    else
+        -- Force mode of SGMII when disabling just to have a default
+        octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_CFG = 2
+        octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = 15
     end
-
-    local function custom_config(qlm_num)
-        local mode = menu.prompt_number("QLM%d Mode" % qlm_num, nil, 0, 3)
-        local speed = menu.prompt_number("QLM%d Speed" % qlm_num, nil, 0, 15)
-        octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_CFG = mode
-        octeon.csr.MIO_QLMX_CFG(qlm_num).QLM_SPD = speed
-    end
-
-    local prefix = "QLM" .. qlm_num
-    local m = menu.new(prefix .. "Configuration")
-    m:item("sgmii",  prefix .. ": SGMII 1.25Ghz", sgmii_config, qlm_num)
-    m:item("xaui",   prefix .. ": XAUI  3.125Ghz", xaui_config, qlm_num)
-    m:item("dxaui",  prefix .. ": DXAUI 6.25Ghz", dxaui_config, qlm_num)
-    m:item("custom", prefix .. ": Custom", custom_config, qlm_num)
-    m:item("quit", "Main menu")
-    m:show()
 end
 
 local function measure_clock(qlm_num)
@@ -122,9 +301,6 @@ end
 local function qlm_submenu(qlm_num)
     local prefix = "QLM" .. qlm_num
     local m = menu.new(prefix .. " Menu")
-    if octeon.is_model(octeon.CN61XX) then
-        m:item("set", prefix .. ": Set configuration", set_config, qlm_num)
-    end
     m:item("show", prefix .. ": Show configuration", show_config, qlm_num)
     m:item("clock", prefix .. ": Measure clock", measure_clock, qlm_num)
     m:item("amp", prefix .. ": Change TX amplitude", change_tx_amplitude, qlm_num)
@@ -158,6 +334,9 @@ local m = menu.new("QLM Menu")
 
 -- Build a list of QLMs
 local num_qlms = octeon.c.bdk_qlm_get_num()
+    if octeon.is_model(octeon.CN61XX) then
+        m:item("set", "Change QLM configuration", set_config_cn61xx)
+    end
 for qlm_num = 0, num_qlms-1 do
     local config = qlm.get_config(qlm_num)
     local option = "QLM %d - %s @%2d.%03d GBaud" % {qlm_num, config.mode, config.speed / 1000, config.speed % 1000}
