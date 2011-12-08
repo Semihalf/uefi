3e4e7e9cef5303419d73181f6afc3740f8dcbafe
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2011-12-08 19:16:31 +0000

    Update ebb6100 setup to program GPIO3 and update the PHY comments
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@67880 c83883e8-9f28-0410-baf1-f2025811e16a

--- lua-modules/board-ebb6100.lua
+++ lua-modules/board-ebb6100.lua
@@ -5,13 +5,18 @@ local set_config = octeon.c.bdk_config_set
 
 set_config(octeon.CONFIG_PHY_MGMT_PORT0, 4) -- Mgmt port 0 PHY address
 set_config(octeon.CONFIG_PHY_MGMT_PORT1, 5) -- Mgmt port 1 PHY address
--- These are behind a MDIO switch that must choose which QLM to access
--- Option 1
+
+-- SGMII PHYs are behind a MDIO switch controlled by GPIO 3
+-- Configure GPIO 3 as an output high, using QLM2
+octeon.c.bdk_gpio_initialize(3, true, 1)
+
+-- GPIO 3 high selects the PHYs on QLM2, which is GMX0
 set_config(octeon.CONFIG_PHY_IF0_PORT0, 256 + 1) -- QLM2 SGMII port 0 PHY address
 set_config(octeon.CONFIG_PHY_IF0_PORT1, 256 + 2) -- QLM2 SGMII port 1 PHY address
 set_config(octeon.CONFIG_PHY_IF0_PORT2, 256 + 3) -- QLM2 SGMII port 2 PHY address
 set_config(octeon.CONFIG_PHY_IF0_PORT3, 256 + 4) -- QLM2 SGMII port 3 PHY address
--- Option 2
+
+-- GPIO 3 low selects the PHYs on QLM0, which is GMX1
 set_config(octeon.CONFIG_PHY_IF1_PORT0, 256 + 1) -- QLM0 SGMII port 0 PHY address
 set_config(octeon.CONFIG_PHY_IF1_PORT1, 256 + 2) -- QLM0 SGMII port 1 PHY address
 set_config(octeon.CONFIG_PHY_IF1_PORT2, 256 + 3) -- QLM0 SGMII port 2 PHY address
