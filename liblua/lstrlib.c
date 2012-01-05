3dd1a327294c0c4b97eed09b6f11f8d4a2a688a7
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2012-01-05 17:40:24 +0000

    Remove some range checks that break when using 64bit numbers
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@68700 c83883e8-9f28-0410-baf1-f2025811e16a

--- liblua/lstrlib.c
+++ liblua/lstrlib.c
@@ -870,16 +870,12 @@ static int str_format (lua_State *L) {
         }
         case 'd':  case 'i': {
           lua_Number n = luaL_checknumber(L, arg);
-          luaL_argcheck(L, (MIN_INTFRM - 1) < n && n < (MAX_INTFRM + 1), arg,
-                        "not a number in proper range");
           addlenmod(form, LUA_INTFRMLEN);
           nb = sprintf(buff, form, (LUA_INTFRM_T)n);
           break;
         }
         case 'o':  case 'u':  case 'x':  case 'X': {
           lua_Number n = luaL_checknumber(L, arg);
-          luaL_argcheck(L, 0 <= n && n < (MAX_UINTFRM + 1), arg,
-                        "not a non-negative number in proper range");
           addlenmod(form, LUA_INTFRMLEN);
           nb = sprintf(buff, form, (unsigned LUA_INTFRM_T)n);
           break;
