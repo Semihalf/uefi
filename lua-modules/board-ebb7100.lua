211a9bc693f5e2f833dca40ed8fd6b850f59c1e5
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2012-03-05 13:57:24 +0000

    Fix the SGMII addresses for the CNF71XX board
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@70613 c83883e8-9f28-0410-baf1-f2025811e16a

--- lua-modules/board-ebb7100.lua
+++ lua-modules/board-ebb7100.lua
@@ -3,6 +3,6 @@ print("Configuring for the EBB7100")
 
 local set_config = octeon.c.bdk_config_set
 
-set_config(octeon.CONFIG_PHY_IF0_PORT0, 256 + 1) -- QLM0 SGMII port 0 PHY address
-set_config(octeon.CONFIG_PHY_IF0_PORT1, 256 + 2) -- QLM0 SGMII port 1 PHY address
+set_config(octeon.CONFIG_PHY_IF0_PORT0, 1) -- QLM0 SGMII port 0 PHY address
+set_config(octeon.CONFIG_PHY_IF0_PORT1, 2) -- QLM0 SGMII port 1 PHY address
 
