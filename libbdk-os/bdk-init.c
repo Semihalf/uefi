5d2a5c3f2e5da8c457290239a685126068980a6c
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2012-04-10 16:29:09 +0000

    Supress L2C_TADX_INT[rddislmc] caused by the L2 locking
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@72127 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-os/bdk-init.c
+++ libbdk-os/bdk-init.c
@@ -120,6 +120,18 @@ void __bdk_init(long base_address)
         {
             write(1, BANNER_2, sizeof(BANNER_2)-1);
             bdk_l2c_lock_mem_region(0, bdk_l2c_get_num_sets() * (bdk_l2c_get_num_assoc()-1) * BDK_CACHE_LINE_SIZE);
+            /* The above locking will cause L2 to load zeros without DRAM setup.
+                This will cause L2C_TADX_INT[rddislmc], which we supress below */
+            BDK_CSR_DEFINE(l2c_tadx_int, BDK_L2C_TADX_INT(0));
+            l2c_tadx_int.u64 = 0;
+            l2c_tadx_int.s.rddislmc = 1;
+            BDK_CSR_WRITE(BDK_L2C_TADX_INT(0), l2c_tadx_int.u64);
+            if (OCTEON_IS_MODEL(OCTEON_CN68XX))
+            {
+                BDK_CSR_WRITE(BDK_L2C_TADX_INT(1), l2c_tadx_int.u64);
+                BDK_CSR_WRITE(BDK_L2C_TADX_INT(2), l2c_tadx_int.u64);
+                BDK_CSR_WRITE(BDK_L2C_TADX_INT(3), l2c_tadx_int.u64);
+            }
         }
 
         write(1, BANNER_3, sizeof(BANNER_3)-1);
