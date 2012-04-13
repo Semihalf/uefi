fc50ccd22457a5bb97faf2f14191cad088e3859b
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2012-04-13 22:00:55 +0000

    Don't lock the L2 cache if we're in simulation. The simulator asserts if we lock all ways
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@72326 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-os/bdk-init.c
+++ libbdk-os/bdk-init.c
@@ -119,7 +119,7 @@ void __bdk_init(long base_address)
 
         /* Only lock L2 if DDR3 isn't initialized */
         BDK_CSR_INIT(lmcx_ddr_pll_ctl, BDK_LMCX_DDR_PLL_CTL(0));
-        if (lmcx_ddr_pll_ctl.s.reset_n == 0)
+        if ((lmcx_ddr_pll_ctl.s.reset_n == 0) && !bdk_is_simulation())
         {
             write(1, BANNER_2, sizeof(BANNER_2)-1);
             bdk_l2c_lock_mem_region(0, bdk_l2c_get_num_sets() * bdk_l2c_get_num_assoc() * BDK_CACHE_LINE_SIZE);
