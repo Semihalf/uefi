62d12fa2ce19e7e05252c366160267d6cac127a4
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2013-03-01 19:46:52 +0000

    Add PCIe IDs for o70 and o78
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@81427 c83883e8-9f28-0410-baf1-f2025811e16a

--- utils/bdk-lua/octeon-remote-pci.c
+++ utils/bdk-lua/octeon-remote-pci.c
@@ -67,6 +67,8 @@ static const uint32_t  OCTEON_PCI_IDS[] = { 0x177d0090, /* CN63XX */
                                             0x177d0092, /* CN66XX */
                                             0x177d0093, /* CN61XX */
                                             0x177d0094, /* CNF71XX */
+                                            0x177d0095, /* CN78XX */
+                                            0x177d0096, /* CN70XX */
                                             0 };
 
 static uint32_t octeon_pci_bar0_size    = 0;    /* Size of the BAR0 memory mapped region */
@@ -240,6 +242,12 @@ static int pci_get_device(int device)
             break;
         case 0x94: /* CNF71XX */
             octeon_pci_model = OCTEON_CNF71XX_PASS1_0;
+            break;
+        case 0x95: /* CN78XX */
+            octeon_pci_model = OCTEON_CN78XX_PASS1_0;
+            break;
+        case 0x96: /* CN70XX */
+            octeon_pci_model = OCTEON_CN70XX_PASS1_0;
             break;
         default:
             octeon_remote_debug(-1, "Octeon model not recognized\n");
