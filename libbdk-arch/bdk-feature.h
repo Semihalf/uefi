9daeaea5cb8b6307f09f1f3a4c3e736b2b3f1725
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2013-03-18 23:33:54 +0000

    Remove the unused octeon_has_feature() function
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@81969 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-arch/bdk-feature.h
+++ libbdk-arch/bdk-feature.h
@@ -10,48 +10,6 @@
  */
 
 
-typedef enum
-{
-    OCTEON_FEATURE_ZIP,         /**<  Does this Octeon support the ZIP offload engine? */
-    OCTEON_FEATURE_CRYPTO,      /**<  Does this Octeon support crypto acceleration using COP2? */
-} octeon_feature_t;
-
-/**
- * Determine if the current Octeon supports a specific feature. These
- * checks have been optimized to be fairly quick, but they should still
- * be kept out of fast path code.
- *
- * @param feature Feature to check for. This should always be a constant so the
- *                compiler can remove the switch statement through optimization.
- *
- * @return Non zero if the feature exists. Zero if the feature does not
- *         exist.
- */
-static inline int octeon_has_feature(octeon_feature_t feature)
-{
-    switch (feature)
-    {
-        case OCTEON_FEATURE_ZIP:
-            return !bdk_fuse_read(121);
-
-        case OCTEON_FEATURE_CRYPTO:
-        {
-            bdk_mio_fus_dat2_t fus_2;
-            fus_2.u64 = BDK_CSR_READ(BDK_MIO_FUS_DAT2);
-            if (fus_2.s.nocrypto || fus_2.s.nomul) {
-                return 0;
-            } else if (!fus_2.s.dorm_crypto) {
-                return 1;
-            } else {
-                bdk_rnm_ctl_status_t st;
-                st.u64 = BDK_CSR_READ(BDK_RNM_CTL_STATUS);
-                return st.s.eer_val;
-            }
-        }
-    }
-    return 0;
-}
-
 /**
  * Return true if the software is currently running on the
  * Octeon software simulator.
