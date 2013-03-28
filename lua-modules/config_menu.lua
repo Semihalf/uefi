7cd926f12955ed287589a40b8c90a9f688e151c2
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2013-03-28 00:24:27 +0000

    Add missing node param in config menu. Hard coded as 0
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@82351 c83883e8-9f28-0410-baf1-f2025811e16a

--- lua-modules/config_menu.lua
+++ lua-modules/config_menu.lua
@@ -78,30 +78,31 @@ addMenu("MAC address", octeon.CONFIG_MAC_ADDRESS)
 addMenuKey("coremask", "Good Coremask", octeon.CONFIG_COREMASK)
 
 -- Add an item for each MGMT port
+local node = 0
 local if_mgmt = 5
-for port = 0,octeon.c.bdk_if_num_ports(if_mgmt, 0)-1 do
+for port = 0,octeon.c.bdk_if_num_ports(node, if_mgmt, 0)-1 do
     addPhyMenu("RGMII/MII port " .. port, octeon.CONFIG_PHY_MGMT_PORT0 + port)
 end
 
 -- Add an item for each SGMII port
 local if_sgmii = 0
-for interface = 0,octeon.c.bdk_if_num_interfaces(if_sgmii)-1 do
-    for port = 0, octeon.c.bdk_if_num_ports(if_sgmii, interface)-1 do
+for interface = 0,octeon.c.bdk_if_num_interfaces(node, if_sgmii)-1 do
+    for port = 0, octeon.c.bdk_if_num_ports(node, if_sgmii, interface)-1 do
         addPhyMenu("SGMII interface " .. interface .. ", port " .. port, octeon.CONFIG_PHY_IF0_PORT0 + interface*4 + port)
     end
 end
 
 -- Add an item for each XAUI port
 local if_xaui = 1
-for interface = 0,octeon.c.bdk_if_num_interfaces(if_xaui)-1 do
-    if octeon.c.bdk_if_num_ports(if_xaui, interface) > 0 then
+for interface = 0,octeon.c.bdk_if_num_interfaces(node, if_xaui)-1 do
+    if octeon.c.bdk_if_num_ports(node, if_xaui, interface) > 0 then
         addMenu("XAUI interface " .. interface .. " Higig mode, 0=XAUI,1=Higig(+),2=Higig2", octeon.CONFIG_HIGIG_MODE_IF0 + interface, 0, 2)
     end
 end
 
 -- Add an item for each ILK port
 local if_ilk = 6
-for interface = 0,octeon.c.bdk_if_num_interfaces(if_ilk)-1 do
+for interface = 0,octeon.c.bdk_if_num_interfaces(node, if_ilk)-1 do
     addMenu("Interlaken port " .. interface .. " lanes", octeon.CONFIG_ILK0_LANES + interface, 0, 8)
     addMenu("Interlaken port " .. interface .. " channels", octeon.CONFIG_ILK0_PORTS + interface, 1, 64)
 end
