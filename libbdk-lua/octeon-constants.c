8b1d46224e3e13acb4e8dabb487e7280df989c22
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2013-04-05 19:08:04 +0000

    Add the MPI constants to Lua
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@82827 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-lua/octeon-constants.c
+++ libbdk-lua/octeon-constants.c
@@ -10,17 +10,29 @@
  */
 void register_octeon_constants(lua_State* L)
 {
-    lua_pushnumber(L, BDK_CSR_TYPE_RSL);
-    lua_setfield(L, -2, "CSR_TYPE_RSL");
-    lua_pushnumber(L, BDK_CSR_TYPE_NCB);
-    lua_setfield(L, -2, "CSR_TYPE_NCB");
-    lua_pushnumber(L, BDK_CSR_TYPE_PEXP);
-    lua_setfield(L, -2, "CSR_TYPE_PEXP");
-    lua_pushnumber(L, BDK_CSR_TYPE_PEXP_NCB);
-    lua_setfield(L, -2, "CSR_TYPE_PEXP_NCB");
-    lua_pushnumber(L, BDK_CSR_TYPE_PCICONFIGEP);
-    lua_setfield(L, -2, "CSR_TYPE_PCICONFIGEP");
-    lua_pushnumber(L, BDK_CSR_TYPE_PCICONFIGRC);
-    lua_setfield(L, -2, "CSR_TYPE_PCICONFIGRC");
+    #define push_bdk_enum(enum)             \
+        do                                  \
+        {                                   \
+            lua_pushnumber(L, enum);        \
+            lua_setfield(L, -2, #enum + 4); \
+        } while (0)
+
+    push_bdk_enum(BDK_CSR_TYPE_RSL);
+    push_bdk_enum(BDK_CSR_TYPE_NCB);
+    push_bdk_enum(BDK_CSR_TYPE_PEXP);
+    push_bdk_enum(BDK_CSR_TYPE_PEXP_NCB);
+    push_bdk_enum(BDK_CSR_TYPE_PCICONFIGEP);
+    push_bdk_enum(BDK_CSR_TYPE_PCICONFIGRC);
+#ifndef BDK_BUILD_HOST
+    push_bdk_enum(BDK_MPI_FLAGS_ENABLE_CS0);
+    push_bdk_enum(BDK_MPI_FLAGS_ENABLE_CS1);
+    push_bdk_enum(BDK_MPI_FLAGS_ENABLE_CS2);
+    push_bdk_enum(BDK_MPI_FLAGS_ENABLE_CS3);
+    push_bdk_enum(BDK_MPI_FLAGS_CS_ACTIVE_HI);
+    push_bdk_enum(BDK_MPI_FLAGS_ONE_WIRE);
+    push_bdk_enum(BDK_MPI_FLAGS_IDLE_CLOCKS);
+    push_bdk_enum(BDK_MPI_FLAGS_IDLE_LOW);
+    push_bdk_enum(BDK_MPI_FLAGS_LSB_FIRST);
+#endif
 }
 
