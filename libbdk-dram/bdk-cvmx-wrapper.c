d41f54eb57508ab80fbcbc26918ee1c61e6af08c
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2013-08-05 22:59:09 +0000

    Add cvmx_clock_get_rate_node(), required by dram init code
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@86674 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-dram/bdk-cvmx-wrapper.c
+++ libbdk-dram/bdk-cvmx-wrapper.c
@@ -9,24 +9,29 @@ int cvmx_twsix_read_ia(int twsi_id, uint8_t dev_addr, uint16_t internal_addr, in
     return num_bytes;
 }
 
-uint64_t cvmx_clock_get_rate(int clock)
+uint64_t cvmx_clock_get_rate_node(int node, int clock)
 {
     switch (clock)
     {
         case 0: // CVMX_CLOCK_RCLK
-            return bdk_clock_get_rate(bdk_numa_local(), BDK_CLOCK_RCLK);
+            return bdk_clock_get_rate(node, BDK_CLOCK_RCLK);
         case 1: // CVMX_CLOCK_SCLK
-            return bdk_clock_get_rate(bdk_numa_local(), BDK_CLOCK_SCLK);
+            return bdk_clock_get_rate(node, BDK_CLOCK_SCLK);
         case 2: // CVMX_CLOCK_DDR
             return 0;
         case 3: // CVMX_CLOCK_CORE
-            return bdk_clock_get_rate(bdk_numa_local(), BDK_CLOCK_CORE);
+            return bdk_clock_get_rate(node, BDK_CLOCK_CORE);
         case 4: // CVMX_CLOCK_TIM
-            return bdk_clock_get_rate(bdk_numa_local(), BDK_CLOCK_TIM);
+            return bdk_clock_get_rate(node, BDK_CLOCK_TIM);
         case 5: // CVMX_CLOCK_IPD
-            return bdk_clock_get_rate(bdk_numa_local(), BDK_CLOCK_IPD);
+            return bdk_clock_get_rate(node, BDK_CLOCK_IPD);
     }
     return 0;
+}
+
+uint64_t cvmx_clock_get_rate(int clock)
+{
+    return cvmx_clock_get_rate_node(bdk_numa_local(), BDK_CLOCK_RCLK);
 }
 
 void octeon_delay_cycles(uint64_t cycles)
