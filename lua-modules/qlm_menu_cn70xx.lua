862d824a36eac1a4e99ab6bb1296288a54f6ab76
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2013-08-12 18:28:12 +0000

    Add prompts to setup SATA pins that use GPIO signals. There are a total of 9.
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@86885 c83883e8-9f28-0410-baf1-f2025811e16a

--- lua-modules/qlm_menu_cn70xx.lua
+++ lua-modules/qlm_menu_cn70xx.lua
@@ -27,6 +27,16 @@ local function setup_dlm0(mode, speed, flags)
     assert(0 == octeon.c.bdk_qlm_set_mode(node, 0, mode, speed, flags), "Setting DLM0 mode failed")
 end
 
+local function setup_gpio_output(gpio, mode)
+    if gpio == -1 then
+        return
+    elseif gpio >= 16 then
+        octeon.csr.GPIO_XBIT_CFGX(gpio).encode({OUTPUT_SEL = mode, TX_OE = 1})
+    else
+        octeon.csr.GPIO_BIT_CFGX(gpio).encode({OUTPUT_SEL = mode, TX_OE = 1})
+    end
+end
+
 -- Chip specific configuration prompting for CN70XX
 function qlm_setup_cn70xx()
     if ((octeon.c.bdk_qlm_get_mode(node, 0) ~= octeon.QLM_MODE_DISABLED) or
@@ -69,6 +79,45 @@ function qlm_setup_cn70xx()
         pcie1_root = menu.prompt_yes_no("Configure PCIe port 1 as a root complex", "y")
         flags = pcie_gen2 and octeon.QLM_MODE_FLAG_GEN2 or octeon.QLM_MODE_FLAG_GEN1
         flags = flags + (pcie1_root and 0 or octeon.QLM_MODE_FLAG_ENDPOINT)
+    end
+
+    -- SATA can use a number of GPIOs for various signals
+    -- Prompt the user for which GPIOs are needed
+    local have_sata = (pcie_mode == "1x2") or (pcie_mode == "2x1")
+    if have_sata then
+        -- PORT0
+        local sata_lab_lb_pin = menu.prompt_number("GPIO for SATA compliance lab loopback testing (lab_lb_pin)", -1, -1, 19)
+        sata_lab_lb_pin = (sata_lab_lb_pin == -1) and 31 or sata_lab_lb_pin
+        octeon.csr.GPIO_SATA_CTL.sel0 = sata_lab_lb_pin
+
+        local p0_mp_switch = menu.prompt_number("GPIO for SATA0 mechanical presence detect (p0_mp_switch)", -1, -1, 19)
+        p0_mp_switch = (p0_mp_switch == -1) and 31 or p0_mp_switch
+        octeon.csr.GPIO_SATA_CTL.sel1 = p0_mp_switch
+
+        local p0_cp_det = menu.prompt_number("GPIO for SATA0 cold presence detect (p0_cp_det)", -1, -1, 19)
+        p0_cp_det = (p0_cp_det == -1) and 31 or p0_cp_det
+        octeon.csr.GPIO_SATA_CTL.sel3 = p0_cp_det
+
+        local p0_cp_pod = menu.prompt_number("GPIO for SATA0 cold presence power-on device (p0_cp_pod)", -1, -1, 19)
+        setup_gpio_output(p0_cp_pod, 0x15)
+
+        local p0_act_led = menu.prompt_number("GPIO for SATA0 activity LED(p0_act_led)", -1, -1, 19)
+        setup_gpio_output(p0_act_led, 0x17)
+
+        -- PORT1
+        local p1_mp_switch = menu.prompt_number("GPIO for SATA1 mechanical presence detect (p1_mp_switch)", -1, -1, 19)
+        p1_mp_switch = (p1_mp_switch == -1) and 31 or p1_mp_switch
+        octeon.csr.GPIO_SATA_CTL.sel2 = p1_mp_switch
+
+        local p1_cp_det = menu.prompt_number("GPIO for SATA1 cold presence detect (p1_cp_det)", -1, -1, 19)
+        p1_cp_det = (p1_cp_det == -1) and 31 or p1_cp_det
+        octeon.csr.GPIO_SATA_CTL.sel4 = p1_cp_det
+
+        local p1_cp_pod = menu.prompt_number("GPIO for SATA1 cold presence power-on device (p1_cp_pod)", -1, -1, 19)
+        setup_gpio_output(p1_cp_pod, 0x16)
+
+        local p1_act_led = menu.prompt_number("GPIO for SATA1 activity LED (p1_act_led)", -1, -1, 19)
+        setup_gpio_output(p1_act_led, 0x18)
     end
 
     if pcie_mode == "1x4" then
