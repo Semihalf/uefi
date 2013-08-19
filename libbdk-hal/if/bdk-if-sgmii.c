309da13538b8ac10d2c2a100a474650d872dfa4d
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2013-08-19 17:48:26 +0000

    Set PKO_REG_GMX_PORT_MODE correctly for SGMII on CN70XX
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@87082 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-hal/if/bdk-if-sgmii.c
+++ libbdk-hal/if/bdk-if-sgmii.c
@@ -314,7 +314,7 @@ static int if_init(bdk_if_handle_t handle)
         BDK_CSR_MODIFY(gmx_rx_prts, handle->node, BDK_GMXX_RX_PRTS(gmx_block),
             gmx_rx_prts.s.prts = if_num_ports(handle->node, gmx_block));
 
-        if (OCTEON_IS_MODEL(OCTEON_CN61XX))
+        if (OCTEON_IS_MODEL(OCTEON_CN61XX) || OCTEON_IS_MODEL(OCTEON_CN70XX))
         {
             /* Tell PKO the number of ports on this interface */
             BDK_CSR_MODIFY(pko_mode, handle->node, BDK_PKO_REG_GMX_PORT_MODE,
