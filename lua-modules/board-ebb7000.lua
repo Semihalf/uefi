becca807e1e8d5e2c334e24f40f8f4403e35cb71
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2013-08-21 00:20:24 +0000

    Update MDIO addresses for the EBB7000
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@87143 c83883e8-9f28-0410-baf1-f2025811e16a

--- lua-modules/board-ebb7000.lua
+++ lua-modules/board-ebb7000.lua
@@ -3,21 +3,31 @@ print("Configuring for the EBB7000")
 
 local set_config = octeon.c.bdk_config_set
 
-set_config(octeon.CONFIG_PHY_MGMT_PORT0, 4) -- Mgmt port 0 PHY address
+-- RGMII PHY address is 2, on SMI0
+set_config(octeon.CONFIG_PHY_MGMT_PORT0, 2)
 
--- SGMII PHYs are behind a MDIO switch controlled by GPIO 3
--- Configure GPIO 3 as an output high
-octeon.c.bdk_gpio_initialize(0, 3, true, 1)
+-- (Q)SGMII PHY address for interface 0 are 4-7, on SMI1
+set_config(octeon.CONFIG_PHY_IF0_PORT0, 256 + 4)
+set_config(octeon.CONFIG_PHY_IF0_PORT1, 256 + 5)
+set_config(octeon.CONFIG_PHY_IF0_PORT2, 256 + 6)
+set_config(octeon.CONFIG_PHY_IF0_PORT3, 256 + 7)
 
--- GPIO 3 high selects the PHYs on QLM2, which is GMX0
-set_config(octeon.CONFIG_PHY_IF0_PORT0, 256 + 1) -- DLM0.0 SGMII port 0 PHY address
-set_config(octeon.CONFIG_PHY_IF0_PORT1, 256 + 2) -- DLM0.0 SGMII port 1 PHY address
-set_config(octeon.CONFIG_PHY_IF0_PORT2, 256 + 3) -- DLM0.0 SGMII port 2 PHY address
-set_config(octeon.CONFIG_PHY_IF0_PORT3, 256 + 4) -- DLM0.0 SGMII port 3 PHY address
+-- (Q)SGMII PHY address for interface 1 are 8-11, on SMI1
+set_config(octeon.CONFIG_PHY_IF1_PORT0, 256 + 8)
+set_config(octeon.CONFIG_PHY_IF1_PORT1, 256 + 9)
+set_config(octeon.CONFIG_PHY_IF1_PORT2, 256 + 10)
+set_config(octeon.CONFIG_PHY_IF1_PORT3, 256 + 11)
 
--- GPIO 3 low selects the PHYs on QLM0, which is GMX1
-set_config(octeon.CONFIG_PHY_IF1_PORT0, 256 + 1) -- DLM0.1 SGMII port 0 PHY address
-set_config(octeon.CONFIG_PHY_IF1_PORT1, 256 + 2) -- DLM0.1 SGMII port 1 PHY address
-set_config(octeon.CONFIG_PHY_IF1_PORT2, 256 + 3) -- DLM0.1 SGMII port 2 PHY address
-set_config(octeon.CONFIG_PHY_IF1_PORT3, 256 + 4) -- DLM0.1 SGMII port 3 PHY address
+-- RXAUI PHY address is 18, hex 0x12, on SMI1
+-- set_config(octeon.CONFIG_PHY_IF0_PORT0, 256 + 18)
+
+-- TWSI info
+-- DDR0 converter: Address 0x31
+-- Core converter: Address 0x40
+-- DDR0 Rom: Address 0x50
+-- EEPROM: Address 0x56
+-- QLM Rom: Address 0x57
+-- DDR clock control: Address 0x58
+-- PNR clock control: Address 0x5b
+-- PLL clock control: Address 0x5a
 
