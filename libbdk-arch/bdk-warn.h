c73031d5773d1fc859d2d8ff9b99110adebbfc0f
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2013-09-21 19:09:45 +0000

    Create new macro BDK_TRACE meant to control very low level logging. Its used to display exceeding detail during chip bringup when everything is suspect.
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@88496 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-arch/bdk-warn.h
+++ libbdk-arch/bdk-warn.h
@@ -16,4 +16,12 @@ extern void bdk_warn(const char *format, ...) __attribute__ ((format(printf, 1, 
 extern void bdk_dprintf(const char *format, ...) __attribute__ ((format(printf, 1, 2)));
 #define bdk_warn_if(expression, format, ...) if (bdk_unlikely(expression)) bdk_warn(format, ##__VA_ARGS__)
 
+#ifndef BDK_DEBUG_TRACE
+#define BDK_DEBUG_TRACE 1
+#endif
+#if BDK_DEBUG_TRACE
+#define BDK_TRACE(format, ...) bdk_dprintf("[%s:%d] " format, __FILE__, __LINE__, ##__VA_ARGS__)
+#else
+#define BDK_TRACE(format, ...) do {} while (0)
+#endif
 /** @} */
