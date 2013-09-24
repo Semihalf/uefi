8c096aafeeda80245d39caf0135b46062c560626
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2013-09-24 22:11:15 +0000

    Disable debug tracing. We seem to be past needing the low level details
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@88642 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-arch/bdk-warn.h
+++ libbdk-arch/bdk-warn.h
@@ -17,7 +17,7 @@ extern void bdk_dprintf(const char *format, ...) __attribute__ ((format(printf, 
 #define bdk_warn_if(expression, format, ...) if (bdk_unlikely(expression)) bdk_warn(format, ##__VA_ARGS__)
 
 #ifndef BDK_DEBUG_TRACE
-#define BDK_DEBUG_TRACE 1
+#define BDK_DEBUG_TRACE 0
 #endif
 #if BDK_DEBUG_TRACE
 #define BDK_TRACE(format, ...) bdk_dprintf("[%s:%d] " format, __FILE__, __LINE__, ##__VA_ARGS__)
