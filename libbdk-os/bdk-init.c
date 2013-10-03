c64d0cfae16e1705f25aa022a7f694bc8f99bc6e
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2013-10-03 20:18:08 +0000

    Fix a non-obvious bug with L2 setup in the BDK. When we initially lock L2 before DRAM, the lines involved are not considered dirty by L2. Later when we unlock this region (DRAM initialized) these lines are discarded instead of writing to DDR. The lines that aren't written then get ECC errors on load since DDR was never initialized. Simply fix is to always write to all cache lines.
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@89132 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-os/bdk-init.c
+++ libbdk-os/bdk-init.c
@@ -166,6 +166,15 @@ void __bdk_init(long base_address)
                 BDK_CSR_WRITE(node, BDK_L2C_TADX_INT(6), l2c_tadx_int.u64);
                 BDK_CSR_WRITE(node, BDK_L2C_TADX_INT(7), l2c_tadx_int.u64);
             }
+            /* The locked region isn't considered dirty by L2. Do read
+               read/write of each cache line to force each to be dirty */
+            volatile uint8_t *ptr = bdk_phys_to_ptr(1);
+            uint8_t *end = bdk_phys_to_ptr(bdk_l2c_get_cache_size_bytes(node));
+            while (ptr < end)
+            {
+                *ptr = *ptr;
+                ptr += 128;
+            }
         }
 
         if (BDK_SHOW_BOOT_BANNERS)
