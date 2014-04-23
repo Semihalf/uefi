c05ea02e31b06ccd79d93580bba57421b3589109
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2014-04-23 17:31:35 +0000

    Update MDIO assignments
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@96978 c83883e8-9f28-0410-baf1-f2025811e16a

--- lua-modules/board-ebb7800.lua
+++ lua-modules/board-ebb7800.lua
@@ -3,36 +3,36 @@ print("Configuring for the EBB7800")
 
 local set_config = octeon.c.bdk_config_set
 
--- QLM0
+-- BGX0 (QLM0 or QLM2)
 set_config(octeon.CONFIG_PHY_IF0_PORT0, 0x000 + 1)
 set_config(octeon.CONFIG_PHY_IF0_PORT1, 0x000 + 2)
 set_config(octeon.CONFIG_PHY_IF0_PORT2, 0x000 + 3)
 set_config(octeon.CONFIG_PHY_IF0_PORT3, 0x000 + 4)
--- QLM1
-set_config(octeon.CONFIG_PHY_IF0_PORT0, 0x100 + 1)
-set_config(octeon.CONFIG_PHY_IF0_PORT1, 0x100 + 2)
-set_config(octeon.CONFIG_PHY_IF0_PORT2, 0x100 + 3)
-set_config(octeon.CONFIG_PHY_IF0_PORT3, 0x100 + 4)
--- QLM2
-set_config(octeon.CONFIG_PHY_IF0_PORT0, 0x200 + 1)
-set_config(octeon.CONFIG_PHY_IF0_PORT1, 0x200 + 2)
-set_config(octeon.CONFIG_PHY_IF0_PORT2, 0x200 + 3)
-set_config(octeon.CONFIG_PHY_IF0_PORT3, 0x200 + 4)
--- QLM3
-set_config(octeon.CONFIG_PHY_IF0_PORT0, 0x300 + 1)
-set_config(octeon.CONFIG_PHY_IF0_PORT1, 0x300 + 2)
-set_config(octeon.CONFIG_PHY_IF0_PORT2, 0x300 + 3)
-set_config(octeon.CONFIG_PHY_IF0_PORT3, 0x300 + 4)
--- QLM4
-set_config(octeon.CONFIG_PHY_IF0_PORT0, 0x000 + 5)
-set_config(octeon.CONFIG_PHY_IF0_PORT1, 0x000 + 6)
-set_config(octeon.CONFIG_PHY_IF0_PORT2, 0x000 + 7)
-set_config(octeon.CONFIG_PHY_IF0_PORT3, 0x000 + 8)
--- QLM5
-set_config(octeon.CONFIG_PHY_IF0_PORT0, 0x100 + 5)
-set_config(octeon.CONFIG_PHY_IF0_PORT1, 0x100 + 6)
-set_config(octeon.CONFIG_PHY_IF0_PORT2, 0x100 + 7)
-set_config(octeon.CONFIG_PHY_IF0_PORT3, 0x100 + 8)
+-- BGX1 (QLM1 or QLM3)
+set_config(octeon.CONFIG_PHY_IF1_PORT0, 0x100 + 1)
+set_config(octeon.CONFIG_PHY_IF1_PORT1, 0x100 + 2)
+set_config(octeon.CONFIG_PHY_IF1_PORT2, 0x100 + 3)
+set_config(octeon.CONFIG_PHY_IF1_PORT3, 0x100 + 4)
+-- BGX2 (QLM4)
+set_config(octeon.CONFIG_PHY_IF2_PORT0, 0x200 + 1)
+set_config(octeon.CONFIG_PHY_IF2_PORT1, 0x200 + 2)
+set_config(octeon.CONFIG_PHY_IF2_PORT2, 0x200 + 3)
+set_config(octeon.CONFIG_PHY_IF2_PORT3, 0x200 + 4)
+-- BGX3 (QLM5)
+set_config(octeon.CONFIG_PHY_IF3_PORT0, 0x300 + 1)
+set_config(octeon.CONFIG_PHY_IF3_PORT1, 0x300 + 2)
+set_config(octeon.CONFIG_PHY_IF3_PORT2, 0x300 + 3)
+set_config(octeon.CONFIG_PHY_IF3_PORT3, 0x300 + 4)
+-- BGX4 (QLM6)
+set_config(octeon.CONFIG_PHY_IF4_PORT0, 0x000 + 5)
+set_config(octeon.CONFIG_PHY_IF4_PORT1, 0x000 + 6)
+set_config(octeon.CONFIG_PHY_IF4_PORT2, 0x000 + 7)
+set_config(octeon.CONFIG_PHY_IF4_PORT3, 0x000 + 8)
+-- BGX5 (QLM7)
+set_config(octeon.CONFIG_PHY_IF5_PORT0, 0x100 + 5)
+set_config(octeon.CONFIG_PHY_IF5_PORT1, 0x100 + 6)
+set_config(octeon.CONFIG_PHY_IF5_PORT2, 0x100 + 7)
+set_config(octeon.CONFIG_PHY_IF5_PORT3, 0x100 + 8)
 
 -- printf("Configuring QLMs for a sample setup\n");
 -- local node = octeon.MASTER_NODE
