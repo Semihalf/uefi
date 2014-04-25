d96ffdc3b71a146ac69071999932006d1a938ecb
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2014-04-25 14:42:32 +0000

    Remove programming the PKO PICK registers. Per the designer, these should never be touched
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@97149 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-hal/if/bdk-if-cn78xx.c
+++ libbdk-hal/if/bdk-if-cn78xx.c
@@ -476,12 +476,6 @@ static int pko_port_init(bdk_if_handle_t handle)
     BDK_CSR_MODIFY(c, handle->node, BDK_PKO_L1_SQX_LINK(pq),
         c.s.link = lmac;
         c.s.cc_enable = 0);
-    if (!bdk_is_simulation())
-        BDK_CSR_MODIFY(c, handle->node, BDK_PKO_L1_SQX_PICK(pq),
-            c.s.child = sq_l2;
-            c.s.dq = dq;
-            c.s.p_con = 1;
-            c.s.c_con = 1);
     /* Program L2 = schedule queue */
     BDK_CSR_MODIFY(c, handle->node, BDK_PKO_L2_SQX_SCHEDULE(sq_l2),
         c.s.prio = 0;
@@ -490,12 +484,6 @@ static int pko_port_init(bdk_if_handle_t handle)
         c.s.prio_anchor = sq_l3;
         c.s.parent = pq;
         c.s.rr_prio = 0);
-    if (!bdk_is_simulation())
-        BDK_CSR_MODIFY(c, handle->node, BDK_PKO_L2_SQX_PICK(sq_l2),
-            c.s.child = sq_l3;
-            c.s.dq = dq;
-            c.s.p_con = 1;
-            c.s.c_con = 1);
     /* Program L3 = schedule queue */
     BDK_CSR_MODIFY(c, handle->node, BDK_PKO_L3_L2_SQX_CHANNEL(sq_l3),
         c.s.cc_channel = handle->ipd_port);
@@ -506,12 +494,6 @@ static int pko_port_init(bdk_if_handle_t handle)
         c.s.prio_anchor = sq_l4;
         c.s.parent = sq_l2;
         c.s.rr_prio = 0);
-    if (!bdk_is_simulation())
-        BDK_CSR_MODIFY(c, handle->node, BDK_PKO_L3_SQX_PICK(sq_l3),
-            c.s.child = sq_l4;
-            c.s.dq = dq;
-            c.s.p_con = 1;
-            c.s.c_con = 1);
     /* Program L4 = schedule queue */
     BDK_CSR_MODIFY(c, handle->node, BDK_PKO_L4_SQX_SCHEDULE(sq_l4),
         c.s.prio = 0;
@@ -520,12 +502,6 @@ static int pko_port_init(bdk_if_handle_t handle)
         c.s.prio_anchor = sq_l5;
         c.s.parent = sq_l3;
         c.s.rr_prio = 0);
-    if (!bdk_is_simulation())
-        BDK_CSR_MODIFY(c, handle->node, BDK_PKO_L4_SQX_PICK(sq_l4),
-            c.s.child = sq_l5;
-            c.s.dq = dq;
-            c.s.p_con = 1;
-            c.s.c_con = 1);
     /* Program L5 = schedule queue */
     BDK_CSR_MODIFY(c, handle->node, BDK_PKO_L5_SQX_SCHEDULE(sq_l5),
         c.s.prio = 0;
@@ -534,12 +510,6 @@ static int pko_port_init(bdk_if_handle_t handle)
         c.s.prio_anchor = dq;
         c.s.parent = sq_l4;
         c.s.rr_prio = 0);
-    if (!bdk_is_simulation())
-        BDK_CSR_MODIFY(c, handle->node, BDK_PKO_L5_SQX_PICK(sq_l5),
-            c.s.child = dq;
-            c.s.dq = dq;
-            c.s.p_con = 1;
-            c.s.c_con = 1);
     /* Program L6 = descriptor queue */
     for (int q=0; q<PKO_QUEUES_PER_CHANNEL; q++)
     {
@@ -548,11 +518,6 @@ static int pko_port_init(bdk_if_handle_t handle)
             c.s.rr_quantum = 0);
         BDK_CSR_MODIFY(c, handle->node, BDK_PKO_DQX_TOPOLOGY(dq+q),
             c.s.parent = sq_l5);
-        if (!bdk_is_simulation())
-            BDK_CSR_MODIFY(c, handle->node, BDK_PKO_DQX_PICK(dq+q),
-                c.s.dq = dq;
-                c.s.p_con = 1;
-                c.s.c_con = 0);
     }
     BDK_CSR_MODIFY(c, handle->node, BDK_PKO_LUTX(compressed_channel_id),
         c.s.valid = 1;
