5891c97742883a9dc4aa688dc79a54a820348935
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2014-04-30 20:42:31 +0000

    Add a display of RST_BOOT on every startup per request for debug
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@97497 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-os/bdk-init-main.c
+++ libbdk-os/bdk-init-main.c
@@ -133,6 +133,13 @@ void __bdk_init_main(int arg, void *arg1)
                 BDK_CSR_WRITE(node, BDK_SMIX_EN(i), smix_en.u64);
             }
         }
+
+        /* FIXME: Dump RST_BOOT for debug purposes */
+        bdk_csr_decode("RST_BOOT", BDK_CSR_READ(node, BDK_RST_BOOT));
+        printf("Clearing RST_BOOT[LBOOT,RBOOT]\n");
+        BDK_CSR_MODIFY(c, node, BDK_RST_BOOT,
+            c.s.lboot = 0;
+            c.s.rboot = 0);
     }
 
     /* Core 0 start main as another thread. We create a new thread so that
