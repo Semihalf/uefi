47c8fd9f8a0d79d7b0aef32b25fb61fb9788d3f5
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2014-05-03 21:56:52 +0000

    Update DRAM init to run on multiple nodes
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@97822 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-dram/bdk-dram-config.c
+++ libbdk-dram/bdk-dram-config.c
@@ -4,6 +4,10 @@
 #include "cvmx.h"
 #include "lib_octeon_shared.h"
 extern int global_ddr_clock_initialized;
+static volatile int thread_mbyte_result;
+
+//#define BDK_TRACE(format, ...) printf("[%s:%d] " format, __FILE__, __LINE__, ##__VA_ARGS__)
+#define BDK_TRACE(format, ...) do {} while (0)
 
 /**
  * Given a predefined board name, lookup the DRAM configuration
@@ -63,12 +67,17 @@ ddr_config_table_t *bdk_dram_lookup_board(const char *board_name)
  * @return Amount of DRAM in MB, or negative on failure
  */
 
-int bdk_dram_config_raw(const ddr_config_table_t *ddr_config_table, int ddr_clock_hertz)
+static int thread_dram_config_raw(int arg1, void *arg2)
 {
-    uint32_t cpu_id = cvmx_get_proc_id();
+    const int ddr_clock_hertz = arg1;
+    const ddr_config_table_t *ddr_config_table = arg2;
+
+    const uint32_t cpu_id = cvmx_get_proc_id();
+    const int node = cvmx_get_node_num();
     uint32_t interface_mask = 0;
     uint32_t measured_ddr_hertz;
     int interface_index;
+    BDK_TRACE("N%d: DRAM init thread started (hertz=%d, config=%p)\n", node, ddr_clock_hertz, ddr_config_table);
 
     const ddr_configuration_t *ddr_config = ddr_config_table->ddr_config;
 
@@ -86,6 +95,7 @@ int bdk_dram_config_raw(const ddr_config_table_t *ddr_config_table, int ddr_cloc
 
     /* We need to clear this global if we want this to work more than once.... */
     global_ddr_clock_initialized = 0;
+    BDK_TRACE("N%d: Calling DRAM init\n", node);
     int mbytes = octeon_ddr_initialize(cpu_id,
                               cvmx_clock_get_rate(CVMX_CLOCK_CORE),
                               ddr_clock_hertz,
@@ -96,24 +106,27 @@ int bdk_dram_config_raw(const ddr_config_table_t *ddr_config_table, int ddr_cloc
                               0,
                               0,
                               0);
+    BDK_TRACE("N%d: DRAM init returned %d\n", node, mbytes);
     if (mbytes <= 0)
     {
         printf("ERROR: DDR initialization failed\n");
+        thread_mbyte_result = -1;
         return -1;
     }
 
     /* Clear any DRAM errors set during init */
+    BDK_TRACE("N%d: Clearing L2 errors\n", node);
     if (OCTEON_IS_MODEL(OCTEON_CN70XX))
     {
-        cvmx_write_csr(CVMX_CIU_CIB_LMCX_RAWX(0, 0), cvmx_read_csr(CVMX_CIU_CIB_LMCX_RAWX(0, 0)));
-        cvmx_write_csr(CVMX_LMCX_INT(0), cvmx_read_csr(CVMX_LMCX_INT(0)));
+        cvmx_write_csr_node(node, CVMX_CIU_CIB_LMCX_RAWX(0, 0), cvmx_read_csr_node(node, CVMX_CIU_CIB_LMCX_RAWX(0, 0)));
+        cvmx_write_csr_node(node, CVMX_LMCX_INT(0), cvmx_read_csr_node(node, CVMX_LMCX_INT(0)));
     }
     else if (OCTEON_IS_MODEL(OCTEON_CN78XX))
     {
-        cvmx_write_csr(CVMX_LMCX_INT(0), cvmx_read_csr(CVMX_LMCX_INT(0)));
-        cvmx_write_csr(CVMX_LMCX_INT(1), cvmx_read_csr(CVMX_LMCX_INT(1)));
-        cvmx_write_csr(CVMX_LMCX_INT(2), cvmx_read_csr(CVMX_LMCX_INT(2)));
-        cvmx_write_csr(CVMX_LMCX_INT(3), cvmx_read_csr(CVMX_LMCX_INT(3)));
+        cvmx_write_csr_node(node, CVMX_LMCX_INT(0), cvmx_read_csr_node(node, CVMX_LMCX_INT(0)));
+        cvmx_write_csr_node(node, CVMX_LMCX_INT(1), cvmx_read_csr_node(node, CVMX_LMCX_INT(1)));
+        cvmx_write_csr_node(node, CVMX_LMCX_INT(2), cvmx_read_csr_node(node, CVMX_LMCX_INT(2)));
+        cvmx_write_csr_node(node, CVMX_LMCX_INT(3), cvmx_read_csr_node(node, CVMX_LMCX_INT(3)));
     }
 
     /* Store the amount of memory in the environment */
@@ -123,23 +136,64 @@ int bdk_dram_config_raw(const ddr_config_table_t *ddr_config_table, int ddr_cloc
     setenv("dram_size_mbytes", buffer, 1);
 
     printf("DRAM: %d MB, %d MHz\n", mbytes, measured_ddr_hertz/1000000);
+    thread_mbyte_result = mbytes;
     return mbytes;
+}
+
+/**
+ * Given a DRAM configuration, configure the hardware to match.
+ * Note that a copy of the configuration is made, so the caller
+ * may dispose of the structure after the call.
+ *
+ * @param board  DRAM configuration
+ *
+ * @return Amount of DRAM in MB, or negative on failure
+ */
+
+int bdk_dram_config_raw(int node, const ddr_config_table_t *ddr_config_table, int ddr_clock_hertz)
+{
+    /* The BDK and CVMX headers are incompatable. Use externals to define
+       the types for functions I need */
+    extern void bdk_thread_yield(void);
+    extern int bdk_thread_create(int node, uint64_t coremask, void *func, int arg0, const void *arg1, int stack_size);
+
+    /* Start the DRAM code on the node we need DRAM setup. It needs a bigger
+       stack than normal */
+    BDK_TRACE("Starting thread for DRAM init\n");
+    thread_mbyte_result = 0;
+    int status = bdk_thread_create(node, 0, thread_dram_config_raw,
+        ddr_clock_hertz, ddr_config_table, 16384);
+    if (status)
+    {
+        printf("ERROR: Failed to create thread for DRAM init\n");
+        return -1;
+    }
+
+    BDK_TRACE("Waiting for DRAM init to complete\n");
+    /* Wait for DRAM setup to be complete */
+    while (thread_mbyte_result == 0)
+    {
+        bdk_thread_yield();
+    }
+    BDK_TRACE("DRAM init returned %d\n", thread_mbyte_result);
+    return thread_mbyte_result;
 }
 
 /**
  * Configure DRAM for a particular board
  *
+ * @param node       Node ID to configure DRAM on
  * @param board_name Board name to configure
  * @param ddr_clock_hertz
  *                   DDR clock in hertz
  *
  * @return Amount of DRAM in MB, or negative on failure
  */
-int bdk_dram_config(const char *board_name, int ddr_clock_hertz)
+int bdk_dram_config(int node, const char *board_name, int ddr_clock_hertz)
 {
     ddr_config_table_t *ddr_config = bdk_dram_lookup_board(board_name);
     if (!ddr_config)
         return -1;
-    return bdk_dram_config_raw(ddr_config, ddr_clock_hertz);
+    return bdk_dram_config_raw(node, ddr_config, ddr_clock_hertz);
 }
 
--- libbdk-dram/bdk-dram-config.h
+++ libbdk-dram/bdk-dram-config.h
@@ -12,12 +12,13 @@
 /**
  * Configure DRAM for a particular board
  *
+ * @param node       Node ID to configure DRAM on
  * @param board_name Board name to configure
  * @param ddr_clock_hertz
  *                   DDR clock in hertz
  *
  * @return Amount of DRAM in MB, or negative on failure
  */
-extern int bdk_dram_config(const char *board_name, int ddr_clock_hertz);
+extern int bdk_dram_config(int node, const char *board_name, int ddr_clock_hertz);
 
 /** @} */
