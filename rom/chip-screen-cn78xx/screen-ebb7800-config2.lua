c184c8a49d3c56725b57c8f53ddf82ab0dc5790d
amusthafa <amusthafa@c83883e8-9f28-0410-baf1-f2025811e16a>
2014-05-09 18:39:33 +0000

    Changing mode as per BDK traffic module to 10G-KR
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@98237 c83883e8-9f28-0410-baf1-f2025811e16a

--- rom/chip-screen-cn78xx/screen-ebb7800-config2.lua
+++ rom/chip-screen-cn78xx/screen-ebb7800-config2.lua
@@ -20,12 +20,12 @@ octeon.c.bdk_qlm_set_mode(node, 3, octeon.QLM_MODE_XAUI_1X4, 6250, 0)
 octeon.csr.GSERX_REFCLK_SEL(5).USE_COM1 = 0
 octeon.csr.GSERX_REFCLK_SEL(5).COM_CLK_SEL = 0
 octeon.c.bdk_qlm_reset(node, 5)
-octeon.c.bdk_qlm_set_mode(node, 5, octeon.QLM_MODE_10GR_4X1, 10312, 0)
+octeon.c.bdk_qlm_set_mode(node, 5, octeon.QLM_MODE_10G_KR_4X1, 10312, 0)
 
 --QLM 7
 octeon.csr.GSERX_REFCLK_SEL(7).USE_COM1 = 0
 octeon.csr.GSERX_REFCLK_SEL(7).COM_CLK_SEL = 0
 octeon.c.bdk_qlm_reset(node, 7)
-octeon.c.bdk_qlm_set_mode(node, 7, octeon.QLM_MODE_10GR_4X1, 10312, 0)
+octeon.c.bdk_qlm_set_mode(node, 7, octeon.QLM_MODE_10G_KR_4X1, 10312, 0)
 
 
--- rom/chip-screen-cn78xx/screen-ebb7800.lua
+++ rom/chip-screen-cn78xx/screen-ebb7800.lua
@@ -20,12 +20,12 @@ octeon.c.bdk_qlm_set_mode(node, 1, octeon.QLM_MODE_RXAUI_2X2, 6250, 0)
 octeon.csr.GSERX_REFCLK_SEL(4).USE_COM1 = 0
 octeon.csr.GSERX_REFCLK_SEL(4).COM_CLK_SEL = 0
 octeon.c.bdk_qlm_reset(node, 4)
-octeon.c.bdk_qlm_set_mode(node, 4, octeon.QLM_MODE_10GR_4X1, 10312, 0)
+octeon.c.bdk_qlm_set_mode(node, 4, octeon.QLM_MODE_10G_KR_4X1, 10312, 0)
 
 --QLM 6
 octeon.csr.GSERX_REFCLK_SEL(6).USE_COM1 = 0
 octeon.csr.GSERX_REFCLK_SEL(6).COM_CLK_SEL = 0
 octeon.c.bdk_qlm_reset(node, 6)
-octeon.c.bdk_qlm_set_mode(node, 6, octeon.QLM_MODE_10GR_4X1, 10312, 0)
+octeon.c.bdk_qlm_set_mode(node, 6, octeon.QLM_MODE_10G_KR_4X1, 10312, 0)
 
 
