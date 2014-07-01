9600e24ac2de61328c8f81f8ef151f3af3c4aa5c
amusthafa <amusthafa@c83883e8-9f28-0410-baf1-f2025811e16a>
2014-07-01 16:58:47 +0000

    Adding XAUI tests to config3
    git-svn-id: http://casvn/svn/octeon/bdk/trunk@100840 c83883e8-9f28-0410-baf1-f2025811e16a

--- rom/chip-screen-cn78xx/screen-ebb7800-config3.lua
+++ rom/chip-screen-cn78xx/screen-ebb7800-config3.lua
@@ -13,16 +13,16 @@ octeon.c.bdk_qlm_reset(node, 0)
 octeon.c.bdk_qlm_set_mode(node, 0, octeon.QLM_MODE_SGMII, 1250, 0)
 
 --QLM 1
---octeon.csr.GSERX_REFCLK_SEL(1).USE_COM1 = 0
---octeon.csr.GSERX_REFCLK_SEL(1).COM_CLK_SEL = 0
---octeon.c.bdk_qlm_reset(node, 1)
---octeon.c.bdk_qlm_set_mode(node, 1, octeon.QLM_MODE_XAUI_1X4, 3125, 0)
+octeon.csr.GSERX_REFCLK_SEL(1).USE_COM1 = 0
+octeon.csr.GSERX_REFCLK_SEL(1).COM_CLK_SEL = 0
+octeon.c.bdk_qlm_reset(node, 1)
+octeon.c.bdk_qlm_set_mode(node, 1, octeon.QLM_MODE_XAUI_1X4, 3125, 0)
 
 --QLM 5
---octeon.csr.GSERX_REFCLK_SEL(5).USE_COM1 = 0
---octeon.csr.GSERX_REFCLK_SEL(5).COM_CLK_SEL = 0
---octeon.c.bdk_qlm_reset(node, 5)
---octeon.c.bdk_qlm_set_mode(node, 5, octeon.QLM_MODE_XAUI_1X4, 3125, 0)
+octeon.csr.GSERX_REFCLK_SEL(5).USE_COM1 = 0
+octeon.csr.GSERX_REFCLK_SEL(5).COM_CLK_SEL = 0
+octeon.c.bdk_qlm_reset(node, 5)
+octeon.c.bdk_qlm_set_mode(node, 5, octeon.QLM_MODE_XAUI_1X4, 3125, 0)
 
 
 
