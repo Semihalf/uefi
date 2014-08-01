a51378506ce37719c34a1fe04d45edfbd1d13db2
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2014-08-01 22:29:58 +0000

    Redo clock to work better with Thunder. Everything is based on RST_REF_CNTR
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@102454 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-hal/bdk-clock.h
+++ libbdk-hal/bdk-clock.h
@@ -13,8 +13,9 @@
  * Enumeration of different Clocks.
  */
 typedef enum{
-    BDK_CLOCK_CORE,        /**< Clock used by cores, coherent bus and L2 cache. */
-    BDK_CLOCK_SCLK,        /**< Clock used by IO blocks. */
+    BDK_CLOCK_MAIN_REF, /**< Main reference clock */
+    BDK_CLOCK_CORE,     /**< Clock used by cores, coherent bus and L2 cache. */
+    BDK_CLOCK_SCLK,     /**< Clock used by IO blocks. */
 } bdk_clock_t;
 
 /**
@@ -26,25 +27,43 @@ typedef enum{
 static inline uint64_t bdk_clock_get_count(bdk_clock_t clock) __attribute__ ((always_inline));
 static inline uint64_t bdk_clock_get_count(bdk_clock_t clock)
 {
+    bdk_node_t node = bdk_numa_local();
+    BDK_CSR_INIT(rst_boot, node, BDK_RST_BOOT);
+    uint64_t ref_cntr = BDK_CSR_READ(node, BDK_RST_REF_CNTR);
     switch(clock)
     {
+        case BDK_CLOCK_MAIN_REF:
+            return ref_cntr;
         case BDK_CLOCK_CORE:
-        {
-            uint64_t count;
-            BDK_MRS(CNTPCT_EL0, count);
-            return count;// FIXME: Read RCLK
-        }
-
+            return ref_cntr * rst_boot.s.c_mul;
         case BDK_CLOCK_SCLK:
-        {
-            uint64_t count;
-            BDK_MRS(CNTPCT_EL0, count);
-            return count;// FIXME: Read SCLK
-        }
+            return ref_cntr * rst_boot.s.pnr_mul;
     }
     return 0;
 }
 
-extern uint64_t bdk_clock_get_rate(bdk_node_t node, bdk_clock_t clock) __attribute__ ((pure));
+/**
+ * Get clock rate based on the clock type.
+ *
+ * @param node    Node to use in a Numa setup. Can be an exact ID or a special value.
+ * @param clock - Enumeration of the clock type.
+ * @return      - return the clock rate.
+ */
+static inline uint64_t bdk_clock_get_rate(bdk_node_t node, bdk_clock_t clock) __attribute__ ((pure));
+static inline uint64_t bdk_clock_get_rate(bdk_node_t node, bdk_clock_t clock)
+{
+    const uint64_t REF_CLOCK = 50000000; /* This is currently defined to be 50Mhz */
+    BDK_CSR_INIT(mio_rst_boot, node, BDK_RST_BOOT);
+    switch (clock)
+    {
+        case BDK_CLOCK_MAIN_REF:
+            return REF_CLOCK;
+        case BDK_CLOCK_CORE:
+            return REF_CLOCK * mio_rst_boot.s.c_mul;
+        case BDK_CLOCK_SCLK:
+            return REF_CLOCK * mio_rst_boot.s.pnr_mul;
+    }
+    return 0;
+}
 
 /** @} */
