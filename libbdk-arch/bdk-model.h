76f694132b81bc6a3fd22aa197dac33b77e6c50f
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2014-08-05 22:23:43 +0000

    Make a model function so gcc knows the model is a contant for optimization
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@102581 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-arch/bdk-model.h
+++ libbdk-arch/bdk-model.h
@@ -22,6 +22,14 @@
 #define CAVIUM_CN85XX           (CAVIUM_CN85XX_PASS1_0 | __OM_IGNORE_REVISION)
 #define CAVIUM_CN85XX_PASS1_X   (CAVIUM_CN85XX_PASS1_0 | __OM_IGNORE_MINOR_REVISION)
 
+static inline uint64_t cavium_get_model() __attribute__ ((pure, always_inline));
+static inline uint64_t cavium_get_model()
+{
+    uint64_t result;
+    asm ("mrs %[rd],MIDR_EL1" : [rd] "=r" (result));
+    return result;
+}
+
 static inline int CAVIUM_IS_MODEL(uint32_t arg_model) __attribute__ ((pure, always_inline));
 static inline int CAVIUM_IS_MODEL(uint32_t arg_model)
 {
@@ -29,7 +37,7 @@ static inline int CAVIUM_IS_MODEL(uint32_t arg_model)
     bdk_sys_midr_el1_t midr_el1;
 
     arg_midr_el1.u = arg_model;
-    asm ("mrs %[rd],MIDR_EL1" : [rd] "=r" (midr_el1.u));
+    midr_el1.u = cavium_get_model();
 
     if (arg_model & __OM_IGNORE_REVISION)
     {
