aaa9892af6465e3aabf85f95e723fa044d61b4d4
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2014-08-14 19:10:58 +0000

    Port random number generator to Thunder
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@103008 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-hal/bdk-rng.c
+++ libbdk-hal/bdk-rng.c
@@ -5,42 +5,13 @@
 BDK_REQUIRE_DEFINE(RNG);
 
 /**
- * Structure describing the data format used for IOBDMA stores to the RNG.
- */
-typedef union
-{
-    uint64_t        u;
-    struct {
-        uint64_t    scraddr : 8;    /**< the (64-bit word) location in scratchpad to write to (if len != 0) */
-        uint64_t    len     : 8;    /**< the number of words in the response (0 => no response) */
-        uint64_t    did     : 8;    /**< the ID of the device on the non-coherent bus */
-        uint64_t    reserved_38_39 : 2;
-        uint64_t    node    : 2;    /**< Numa node number */
-        uint64_t    addr    :40;    /**< the address that will appear in the first tick on the NCB bus */
-    } s;
-} bdk_rng_iobdma_data_t;
-
-
-/**
  * Enables the random number generator. Must be called before RNG is used
  */
 void bdk_rng_enable(bdk_node_t node)
 {
-    bdk_rnm_ctl_status_t rnm_ctl_status;
-    rnm_ctl_status.u = BDK_CSR_READ(node, BDK_RNM_CTL_STATUS);
-    if (!rnm_ctl_status.s.rng_en)
-    {
-        rnm_ctl_status.s.ent_en = 1;
-        rnm_ctl_status.s.rng_en = 1;
-        BDK_CSR_WRITE(node, BDK_RNM_CTL_STATUS, rnm_ctl_status.u);
-    }
-}
-
-static inline uint64_t bdk_rng_load_address()
-{
-    uint64_t result = 0x800140ull << 40;
-    result |= (uint64_t)bdk_numa_local() << 36;
-    return result;
+    BDK_CSR_MODIFY(c, node, BDK_RNM_CTL_STATUS,
+        c.s.ent_en = 1;
+        c.s.rng_en = 1);
 }
 
 /**
@@ -50,7 +21,7 @@ static inline uint64_t bdk_rng_load_address()
  */
 uint8_t bdk_rng_get_random8(void)
 {
-    return bdk_read64_uint8(bdk_rng_load_address());
+    return bdk_read64_uint8(BDK_RNM_RANDOM);
 }
 
 /**
@@ -60,7 +31,7 @@ uint8_t bdk_rng_get_random8(void)
  */
 uint16_t bdk_rng_get_random16(void)
 {
-    return bdk_read64_uint16(bdk_rng_load_address());
+    return bdk_read64_uint16(BDK_RNM_RANDOM);
 }
 
 /**
@@ -70,7 +41,7 @@ uint16_t bdk_rng_get_random16(void)
  */
 uint32_t bdk_rng_get_random32(void)
 {
-    return bdk_read64_uint32(bdk_rng_load_address());
+    return bdk_read64_uint32(BDK_RNM_RANDOM);
 }
 
 /**
@@ -80,6 +51,6 @@ uint32_t bdk_rng_get_random32(void)
  */
 uint64_t bdk_rng_get_random64(void)
 {
-    return bdk_read64_uint64(bdk_rng_load_address());
+    return bdk_read64_uint64(BDK_RNM_RANDOM);
 }
 
