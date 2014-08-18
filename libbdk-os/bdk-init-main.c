428733b480b5ca463147df6a7a06e481556cf9bf
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2014-08-18 18:29:01 +0000

    Reorganize init code to properly initialize multiple nodes
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@103110 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-os/bdk-init-main.c
+++ libbdk-os/bdk-init-main.c
@@ -2,17 +2,59 @@
 #include <stdio.h>
 
 
+/**
+ * Do per core initialization of system registers
+ */
 static void __bdk_init_sysreg(void)
 {
     // FIXME: Setup THUNDERX sysreg
 }
 
+/**
+ * Run on each node as a thread to perform error reporting
+ *
+ * @param arg
+ * @param arg1
+ */
 static void __bdk_error_poll(int arg, void *arg1)
 {
     while (bdk_error_check)
     {
         bdk_error_check(bdk_numa_local());
         bdk_wait_usec(100000);
+    }
+}
+
+/**
+ * Perfome one time initalization for a node. Called for each node.
+ */
+static void __bdk_init_local_node(void)
+{
+    bdk_node_t node = bdk_numa_local();
+
+    if (BDK_SHOW_BOOT_BANNERS)
+        printf("N%d: Performing node initialization\n", node);
+
+    bdk_l2c_initialize(node);
+
+    if (BDK_IS_REQUIRED(PCIE))
+        bdk_pcie_global_initialize(node);
+    if (BDK_IS_REQUIRED(QLM))
+        bdk_qlm_init(node);
+    if (BDK_IS_REQUIRED(TWSI) && !bdk_is_simulation())
+        bdk_twsix_initialize(node);
+    bdk_mdio_initialize(node);
+
+    if (BDK_IS_REQUIRED(ERROR_DECODE) && !bdk_is_simulation())
+    {
+        if (BDK_SHOW_BOOT_BANNERS)
+            printf("N%d: Enabling error reporting\n", node);
+        bdk_error_enable(node);
+        if (bdk_error_check)
+        {
+            if (bdk_thread_create(node, 0, __bdk_error_poll, 0, NULL, 0))
+                bdk_fatal("Create of error poll thread failed\n");
+        }
     }
 }
 
@@ -25,47 +67,32 @@ static void __bdk_error_poll(int arg, void *arg1)
  */
 void __bdk_init_main(int arg, void *arg1)
 {
-    /* Unlock the glbal stack now that we aren't using it anymore */
-    extern volatile uint64_t __bdk_initial_stack;
-    __bdk_initial_stack = 0;
-
-    bdk_node_t node = bdk_numa_local();
+    const bdk_node_t node = bdk_numa_local();
     /* All cores start running threads here. Only the setup required to get
         threading up is done. More init is needed. This code will be locked to
-        a singel core with threads being spawned for each core */
+        a single core with threads being spawned for each core */
 
+    /* Unlock the global stack now that we aren't using it anymore */
+    extern volatile uint64_t __bdk_initial_stack;
+    __bdk_initial_stack = 0;
+    BDK_WMB;
+
+    /* Init any system registers that need to be done on every core */
     __bdk_init_sysreg();
 
+    /* Perform one time init that must be done on the master node. This
+       will not be run on the other nodes */
     if (bdk_is_boot_core())
     {
         if (BDK_SHOW_BOOT_BANNERS)
             printf("Performing common initialization\n");
 
-        bdk_l2c_initialize(node);
         __bdk_config_init(); /* Some config setting are dynamically updated */
 
         extern char **environ;
         environ = calloc(sizeof(*environ), 1);
         if (!environ)
             bdk_error("Failed to allocate environment, setenv will crash\n");
-
-        if (BDK_IS_REQUIRED(QLM))
-            bdk_qlm_init(node);
-
-        if (!bdk_is_simulation()) // FIXME: Not modelled in asim
-        {
-            if (BDK_IS_REQUIRED(TWSI))
-                bdk_twsix_initialize(node);
-            if (bdk_error_enable)
-            {
-                if (BDK_SHOW_BOOT_BANNERS)
-                    printf("Enabling error reporting\n");
-                bdk_error_enable(node);
-            }
-        }
-
-        if (BDK_IS_REQUIRED(PCIE))
-            bdk_pcie_global_initialize(node);
 
         if (BDK_SHOW_BOOT_BANNERS)
         {
@@ -82,19 +109,11 @@ void __bdk_init_main(int arg, void *arg1)
                 }
             }
         }
-
-        /* Make sure SMI/MDIO is enabled so we can query PHYs */
-        int num_mdio = bdk_is_simulation() ? 0 : 4; // FIXME: Not modelled in asim
-        for (int i=0; i<num_mdio; i++)
-        {
-            BDK_CSR_INIT(smix_en, node, BDK_SMI_X_EN(i));
-            if (!smix_en.s.en)
-            {
-                smix_en.s.en = 1;
-                BDK_CSR_WRITE(node, BDK_SMI_X_EN(i), smix_en.u);
-            }
-        }
     }
+
+    /* Perform initialization that needs to be done once per node */
+    if (bdk_get_core_num() == 0)
+        __bdk_init_local_node();
 
     /* Core 0 start main as another thread. We create a new thread so that
         the coremask will allow all cores in case the application
@@ -106,9 +125,6 @@ void __bdk_init_main(int arg, void *arg1)
             printf("Switching to main\n");
         if (bdk_thread_create(node, 0, (bdk_thread_func_t)main, arg, arg1, BDK_THREAD_MAIN_STACK_SIZE))
             bdk_fatal("Create of main thread failed\n");
-        if (&bdk_error_check && bdk_error_check)
-            if (bdk_thread_create(node, 0, __bdk_error_poll, 0, NULL, 0))
-                bdk_fatal("Create of error poll thread failed\n");
 
         /* The following code doesn't do anything useful. It forces a link
             dependency on bdk_require_depends(), which forces link dependency
