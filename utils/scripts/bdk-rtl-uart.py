b40c677dd4a41925384375160e11fc20caaa23ae
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2014-08-26 22:51:28 +0000

    Delete two unused utility scripts
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@103540 c83883e8-9f28-0410-baf1-f2025811e16a

--- utils/scripts/bdk-rtl-uart.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import sys
-
-for line in open("logfile"):
-    if "UART0: offset: 0x0001180000000840 val: " in line:
-        val = long(line.split()[-1], 0)
-        sys.stdout.write(chr(val))
