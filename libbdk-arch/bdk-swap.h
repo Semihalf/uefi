2cc2cb06ee4c965f3d188490a0e7162470039e0e
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2014-09-15 23:10:27 +0000

    Don't use gcc's builtin swap on older compilers. The MA cluster uses soem ancient compiler that doesn't support these builtins.
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@104530 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-arch/bdk-swap.h
+++ libbdk-arch/bdk-swap.h
@@ -30,7 +30,13 @@ static inline uint16_t bdk_swap16(uint16_t x)
  */
 static inline uint32_t bdk_swap32(uint32_t x)
 {
+#if (__GNUC__ > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 3))
     return __builtin_bswap32(x);
+#else
+     x = ((x<<8)&0xFF00FF00) | ((x>>8)&0x00FF00FF);
+     x = (x>>16) | (x<<16);
+     return x;
+#endif
 }
 
 
@@ -42,7 +48,13 @@ static inline uint32_t bdk_swap32(uint32_t x)
  */
 static inline uint64_t bdk_swap64(uint64_t x)
 {
+#if (__GNUC__ > 4) || ((__GNUC__ == 4) && (__GNUC_MINOR__ >= 3))
     return __builtin_bswap64(x);
+#else
+     x = ((x<< 8)&0xFF00FF00FF00FF00ULL) | ((x>> 8)&0x00FF00FF00FF00FFULL);
+     x = ((x<<16)&0xFFFF0000FFFF0000ULL) | ((x>>16)&0x0000FFFF0000FFFFULL);
+     return (x>>32) | (x<<32);
+#endif
 }
 
 
