bd20495a18648137fad9ff02587fce4dd71714cb
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2014-09-25 18:47:06 +0000

    Only init TWSI ports in master mode
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@104986 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-hal/bdk-twsi.c
+++ libbdk-hal/bdk-twsi.c
@@ -39,8 +39,13 @@ int bdk_twsix_initialize(bdk_node_t node)
     sw_twsi.s.eop_ia = 3;  /* R=0 selects CLKCTL, R=1 selects STAT */
     sw_twsi.s.data = ((M_divider & 0xf) << 3) | ((N_divider & 0x7) << 0);
 
-    for (int bus=0; bus<6; bus++)
-        BDK_CSR_WRITE(node, BDK_MIO_TWSX_SW_TWSI(bus), sw_twsi.u);
+    for (int bus = 0; bus < 6; bus++)
+    {
+        /* Only init non-slave ports */
+        BDK_CSR_INIT(state, node, BDK_MIO_TWSX_SW_TWSI(bus));
+        if (!state.s.slonly)
+            BDK_CSR_WRITE(node, BDK_MIO_TWSX_SW_TWSI(bus), sw_twsi.u);
+    }
     return 0;
 }
 
