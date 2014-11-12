f312664e8f61905eb2adccfdd4e8ccd47fbdd438
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2014-11-12 19:55:10 +0000

    Have the normal boot stub select the common clocks used by the EVB8800
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@107784 c83883e8-9f28-0410-baf1-f2025811e16a

--- normal-boot/boot-stub.c
+++ normal-boot/boot-stub.c
@@ -366,6 +366,15 @@ int main(void)
     /* Send status to the BMC: Slave DRAM init complete */
     update_bmc_status(BMC_STATUS_BOOT_STUB_NODE1_DRAM_COMPLETE);
 
+    /* Setup reference clocks */
+    /* QLM0-1 use common clock 1 (156.25Mhz). QLM2-7 use common clock 0 (100Mhz) */
+    for (int qlm=0; qlm<8; qlm++)
+    {
+        BDK_CSR_MODIFY(c, node, BDK_GSERX_REFCLK_SEL(qlm),
+            c.s.com_clk_sel = 1;
+            c.s.use_com1 = (qlm < 2) ? 1 : 0);
+    }
+
     /* Initialize the QLMs */
     BDK_TRACE(BOOT_STUB, "Initializing QLMs\n");
     /* QLM0 = 40G-KR4 */
