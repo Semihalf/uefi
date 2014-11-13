3066b040f01d662d369c6e2b3f58b3740d490a66
rfranz <rfranz@c83883e8-9f28-0410-baf1-f2025811e16a>
2014-11-13 15:28:44 +0000

    Update screen prints for integration with scripts.
    Script is still just empty shell - no actual tests done yet.
    
    
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@107891 c83883e8-9f28-0410-baf1-f2025811e16a

--- screen/lua/init.lua
+++ screen/lua/init.lua
@@ -1,9 +1,10 @@
 
 -- Print out a banner
 print("")
+print("BDK version ".. require("bdk-version"))
 print("THUNDERX Chip Screen")
 print("Copyright (C) 2010-2014 Cavium Inc.")
-print("Version ".. require("bdk-version"))
 print("Nothing has been implemented yet. This is just a stub\n");
+print("BDK chip test complete.\n");
 
 
