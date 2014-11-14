0a396f1278326c4d8179cf8a5fe469eaa52af448
rfranz <rfranz@c83883e8-9f28-0410-baf1-f2025811e16a>
2014-11-14 16:00:10 +0000

    Implment 40GKR traffic test for QML0/1
    This uses a cable to loop QLMS 0/1.  Traffic rates are
    slow for now, as BDK is not performance tuned yet.
    
    
    
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@107977 c83883e8-9f28-0410-baf1-f2025811e16a

--- screen/lua/init.lua
+++ screen/lua/init.lua
@@ -1,10 +1,89 @@
+require("strict")
+require("menu")
+require("utils")
+require("cavium")
+require("trafficgen")
+local bit64 = require("bit64")
+
+local set_config = cavium.c.bdk_config_set
+local node = 0
+
+local function tg_run(tg, ports, size, count, rate, to_secs)
+    print("")
+    tg:command("default %s" % ports)
+    tg:command("clear all")
+    tg:command("size %d" % size)
+    tg:command("count %d" % count)
+    tg:command("tx_percent %d" % rate)
+    tg:command("start")
+    cavium.c.bdk_wait_usec(to_secs * 1000000)
+    tg:command("rx_octets_total")
+    local stats = tg:get_stats()
+    local ports_pass = true
+    for port,stat in pairs(stats) do
+        local pass = true
+        pass = pass and (stat.tx_packets_total == count)
+        pass = pass and (stat.rx_packets_total == count)
+        pass = pass and (stat.tx_octets_total == stat.rx_octets_total)
+        pass = pass and (stat.rx_errors == 0)
+        pass = pass and (stat.rx_validation_errors == 0)
+        if not pass then
+            pprint(port, stat)
+        end
+        ports_pass = ports_pass and pass
+    end
+    if ports_pass then
+        printf("Test %s, size %d, count %d: PASS\n", ports, size, count)
+    else
+        printf("Test %s, size %d, count %d: FAIL\n", ports, size, count)
+    end
+    return ports_pass
+end
 
 -- Print out a banner
 print("")
 print("BDK version ".. require("bdk-version"))
 print("THUNDERX Chip Screen")
 print("Copyright (C) 2010-2014 Cavium Inc.")
-print("Nothing has been implemented yet. This is just a stub\n");
-print("BDK chip test complete.\n");
 
+local coremask = menu.prompt_number("Coremask: ", 0xffffffffffff)
+-- Go multicore, based on coremask provided by script.
+printf("Using coremask: 0x%x\n", coremask)
+cavium.c.bdk_init_cores(0, coremask)
 
+cavium.csr.GSERX_REFCLK_SEL(0).COM_CLK_SEL = 1
+cavium.csr.GSERX_REFCLK_SEL(0).USE_COM1 = 1
+cavium.c.bdk_qlm_reset(node, 0)
+cavium.c.bdk_qlm_set_mode(node, 0, cavium.QLM_MODE_40G_KR4_1X4, 10312, 0)
+
+cavium.csr.GSERX_REFCLK_SEL(1).COM_CLK_SEL = 1
+cavium.csr.GSERX_REFCLK_SEL(1).USE_COM1 = 1
+cavium.c.bdk_qlm_reset(node, 1)
+cavium.c.bdk_qlm_set_mode(node, 1, cavium.QLM_MODE_40G_KR4_1X4, 10312, 0)
+
+local all_pass = true
+
+--
+-- Network Traffic Tests
+--
+print("test start: traffic")
+local tg_pass = true
+local trafficgen = require("trafficgen")
+local tg = trafficgen.new()
+cavium.c.bdk_wait_usec(3 * 1000000) -- wait for links to come up.
+tg_pass = tg_run(tg, "40GKR0-40GKR1", 60, 100000, 50, 5)
+tg_pass = tg_run(tg, "40GKR0-40GKR1", 1499, 100000, 50, 7) and tg_pass
+tg_pass = tg_run(tg, "40GKR0-40GKR1", 9111, 10000, 10, 12) and tg_pass
+all_pass = all_pass and tg_pass
+
+--
+-- Summary
+--
+print("")
+if all_pass then
+    print("All tested summary: PASS")
+else
+    print("All tested summary: FAIL")
+end
+print("BDK Chip Screen complete.")
+print("")
