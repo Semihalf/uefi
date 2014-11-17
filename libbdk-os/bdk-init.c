85c67a6850f12acccd371f619410cb1fb91f385a
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2014-11-17 21:37:28 +0000

    Enable core power gating as it seems to work on hardware
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@108131 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-os/bdk-init.c
+++ libbdk-os/bdk-init.c
@@ -151,8 +151,9 @@ void __bdk_init(uint32_t image_crc)
     {
         /* Shut off cores in reset to save power. It is optional, but probably
             good practice */
-        // FIXME: Core power control disabled as it causes issues in the emulator
-        //BDK_CSR_WRITE(node, BDK_RST_PP_POWER, -1);
+#ifndef HW_EMULATOR // FIXME: Core power control disabled as it causes issues in the emulator
+        BDK_CSR_WRITE(node, BDK_RST_PP_POWER, -1);
+#endif
 
         /* Initialize the is_simulation flag */
         BDK_CSR_INIT(c, node, BDK_OCLAX_CONST(0));
