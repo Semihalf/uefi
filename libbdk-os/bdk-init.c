17ebc85969783261bae5e524868213940bf11d28
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2014-11-20 19:30:06 +0000

    Change order of L2 operations in early boot to supress bogus L2 error bits
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@108416 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-os/bdk-init.c
+++ libbdk-os/bdk-init.c
@@ -185,6 +185,15 @@ void __bdk_init(uint32_t image_crc)
             if (BDK_TRACE_ENABLE_INIT)
                 write(1, BANNER_2, sizeof(BANNER_2)-1);
             bdk_l2c_lock_mem_region(node, bdk_numa_get_address(node, 0), bdk_l2c_get_cache_size_bytes(node) * 3 / 4);
+            /* The locked region isn't considered dirty by L2. Do read
+               read/write of each cache line to force each to be dirty */
+            volatile uint8_t *ptr = bdk_phys_to_ptr(bdk_numa_get_address(node, 1));
+            uint8_t *end = bdk_phys_to_ptr(bdk_numa_get_address(node, bdk_l2c_get_cache_size_bytes(node)));
+            while (ptr < end)
+            {
+                *ptr = *ptr;
+                ptr += 128;
+            }
             /* The above locking will cause L2 to load zeros without DRAM setup.
                 This will cause L2C_TADX_INT[rddislmc], which we suppress below */
             BDK_CSR_DEFINE(l2c_tadx_int, BDK_L2C_TADX_INT_W1C(0));
@@ -198,16 +207,6 @@ void __bdk_init(uint32_t image_crc)
             BDK_CSR_WRITE(node, BDK_L2C_TADX_INT_W1C(5), l2c_tadx_int.u);
             BDK_CSR_WRITE(node, BDK_L2C_TADX_INT_W1C(6), l2c_tadx_int.u);
             BDK_CSR_WRITE(node, BDK_L2C_TADX_INT_W1C(7), l2c_tadx_int.u);
-
-            /* The locked region isn't considered dirty by L2. Do read
-               read/write of each cache line to force each to be dirty */
-            volatile uint8_t *ptr = bdk_phys_to_ptr(bdk_numa_get_address(node, 1));
-            uint8_t *end = bdk_phys_to_ptr(bdk_numa_get_address(node, bdk_l2c_get_cache_size_bytes(node)));
-            while (ptr < end)
-            {
-                *ptr = *ptr;
-                ptr += 128;
-            }
         }
 
         /* Validate the image CRC */
