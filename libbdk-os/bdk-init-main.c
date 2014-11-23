45dc8ca9d5617c6d39aa2a81dca8ef564cd9d477
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2014-11-23 15:21:45 +0000

    Add workaround for Errara (AP-22934) CIM module has incorrect conditional clock
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@108555 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-os/bdk-init-main.c
+++ libbdk-os/bdk-init-main.c
@@ -7,16 +7,20 @@
  */
 static void __bdk_init_sysreg(void)
 {
-    /* Errata (AP-22500) GlobalSync request during a multi-cycle ATOMIC
-       stalls forever */
     if (CAVIUM_IS_MODEL(CAVIUM_CN88XX_PASS1_X))
     {
-        /* Disable compare and swap on CN88XX pass 1.x */
         bdk_sys_cvmctl_el1_t cvmctl_el1;
-        /* Sadly, our own silly compiler doesn't unstand our registers */
         BDK_MRS(s3_0_c11_c0_0, cvmctl_el1.u);
+
+        /* Errara (AP-22934) CIM module has incorrect conditional clock */
+        cvmctl_el1.s.force_issue_clock = 1;
+
+        /* Errata (AP-22500) GlobalSync request during a multi-cycle ATOMIC
+           stalls forever */
+        /* Disable compare and swap on CN88XX pass 1.x */
         cvmctl_el1.s.disable_casp = 1;
         cvmctl_el1.s.disable_cas = 1;
+
         BDK_MSR(s3_0_c11_c0_0, cvmctl_el1.u);
     }
 }
