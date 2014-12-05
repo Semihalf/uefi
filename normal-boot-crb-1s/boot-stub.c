ca5c9a1c2532b38ac89dd4a0f05856bdb041fdf4
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2014-12-05 22:05:52 +0000

    Configure IO expander on CRB to enable SFP TX. XFI now works for me
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@109659 c83883e8-9f28-0410-baf1-f2025811e16a

--- normal-boot-crb-1s/boot-stub.c
+++ normal-boot-crb-1s/boot-stub.c
@@ -432,6 +432,25 @@ int main(void)
         }
     }
 
+    /* Initialize IO expander U29 to set SFP_TX_DISABLE_0=0 and SFP_TX_DISABLE_1=0 */
+    /* Set all pins as inputs except:
+        Pin 0.4: QSFP_RST_N
+        Pin 0.5: QSFP_MODSEL_N
+        Pin 0.6: QSFP_LPMODE
+        Pin 1.3: SFP_TX_DISABLE_0
+        Pin 1.7: SFP_TX_DISABLE_1
+        Data high means pin is input */
+    bdk_twsix_write_ia(bdk_numa_master(), 0, 0x20, 0x6, 1, 1, 0x8f);
+    bdk_twsix_write_ia(bdk_numa_master(), 0, 0x20, 0x7, 1, 1, 0x77);
+    /* Drive output pins:
+        Pin 0.4: QSFP_RST_N=1
+        Pin 0.5: QSFP_MODSEL_N=1
+        Pin 0.6: QSFP_LPMODE=1
+        Pin 1.3: SFP_TX_DISABLE_0=0
+        Pin 1.7: SFP_TX_DISABLE_1=0 */
+    bdk_twsix_write_ia(bdk_numa_master(), 0, 0x20, 0x2, 1, 1, 0x70);
+    bdk_twsix_write_ia(bdk_numa_master(), 0, 0x20, 0x3, 1, 1, 0x00);
+
     /* Select ATF or diagnostics image */
     int use_atf = 1;
     if (DIAGS_TIMEOUT > 0)
