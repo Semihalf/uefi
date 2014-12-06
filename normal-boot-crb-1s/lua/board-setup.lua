a5872fda854793fe6ec93b83b181f1ab3529ef37
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2014-12-06 18:02:52 +0000

    Remove the QLM setup from Lua on the CRB-1S as it is already done in the boot stub
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@109671 c83883e8-9f28-0410-baf1-f2025811e16a

--- normal-boot-crb-1s/lua/board-setup.lua
+++ normal-boot-crb-1s/lua/board-setup.lua
@@ -20,18 +20,3 @@ set_config(cavium.CONFIG_PHY_IF1_PORT1, 5)
 set_config(cavium.CONFIG_PHY_IF1_PORT2, 6)
 set_config(cavium.CONFIG_PHY_IF1_PORT3, 7)
 
-local node = cavium.MASTER_NODE
-
-printf("QLM0-1: Using common clock 1 (156.25Mhz)\n")
-printf("QLM2-7: Using common clock 0 (100.00Mhz)\n")
-for qlm=0,7 do
-    cavium.csr[node].GSERX_REFCLK_SEL(qlm).COM_CLK_SEL = 1
-    cavium.csr[node].GSERX_REFCLK_SEL(qlm).USE_COM1 = (qlm < 2) and 1 or 0
-end
-cavium.c.bdk_qlm_set_mode(node, 0, cavium.QLM_MODE_XLAUI_1X4, 10321, 0)
-cavium.c.bdk_qlm_set_mode(node, 1, cavium.QLM_MODE_XFI_4X1, 10321, 0)
-cavium.c.bdk_qlm_set_mode(node, 2, cavium.QLM_MODE_PCIE_1X4, 8000, cavium.QLM_MODE_FLAG_GEN3)
-cavium.c.bdk_qlm_set_mode(node, 3, cavium.QLM_MODE_SATA_4X1, 6000, 0)
-cavium.c.bdk_qlm_set_mode(node, 4, cavium.QLM_MODE_PCIE_1X8, 8000, cavium.QLM_MODE_FLAG_GEN3)
-cavium.c.bdk_qlm_set_mode(node, 6, cavium.QLM_MODE_PCIE_1X8, 8000, cavium.QLM_MODE_FLAG_GEN3)
-
