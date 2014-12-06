2471c22f700bdcb982248f2dfba19458e7662eb2
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2014-12-06 21:17:49 +0000

    Update CRB addresses for MDIO
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@109676 c83883e8-9f28-0410-baf1-f2025811e16a

--- normal-boot-crb-1s/lua/board-setup.lua
+++ normal-boot-crb-1s/lua/board-setup.lua
@@ -9,14 +9,9 @@ local set_config = cavium.c.bdk_config_set
 -- This code sets the default MDIO addresses for SGMII PHYs
 -- The number is "(0x100 * bus) + phy".
 
--- BGX0 (QLM0)
-set_config(cavium.CONFIG_PHY_IF0_PORT0, 0)
-set_config(cavium.CONFIG_PHY_IF0_PORT1, 1)
-set_config(cavium.CONFIG_PHY_IF0_PORT2, 2)
-set_config(cavium.CONFIG_PHY_IF0_PORT3, 3)
--- BGX1 (QLM1)
-set_config(cavium.CONFIG_PHY_IF1_PORT0, 4)
-set_config(cavium.CONFIG_PHY_IF1_PORT1, 5)
-set_config(cavium.CONFIG_PHY_IF1_PORT2, 6)
-set_config(cavium.CONFIG_PHY_IF1_PORT3, 7)
+-- BGX0 (QLM0) is QSFP on MDIO bus 1, address 1
+set_config(cavium.CONFIG_PHY_IF0_PORT0, 0x101)
+-- BGX1 (QLM1) is SFP on MDIO bus 1, address 0
+set_config(cavium.CONFIG_PHY_IF1_PORT0, 0x100)
+set_config(cavium.CONFIG_PHY_IF1_PORT1, 0x100)
 
