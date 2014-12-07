aacc90571e8bf8ae0963f524221e46a2e442ca1a
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2014-12-07 17:24:15 +0000

    Rewrite the address test to be much faster
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@109685 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-dram/bdk-dram-test-addrbus.c
+++ libbdk-dram/bdk-dram-test-addrbus.c
@@ -25,45 +25,51 @@ int __bdk_dram_test_mem_address_bus(uint64_t area, uint64_t max_address, int bur
     bdk_zero_memory(ptr, max_address - area + 1);
     __bdk_dram_flush_to_mem_range(area, max_address);
 
-    /* Each time we write, we'll write this pattern or its inverse. Swap
-       on each write */
+    /* Each time we write, we'll write this pattern xored the address it is
+       written too */
     uint64_t pattern = 0x0fedcba987654321;
 
     /* Walk through the region incrementing our offset by a power of two. The
-       first few writes will be tothe same cache line (offset 0x8, 0x10, 0x20,
+       first few writes will be to the same cache line (offset 0x8, 0x10, 0x20,
        and 0x40. Offset 0x80 and beyond will be to different cache lines */
     uint64_t offset = 0x8;
     while (area + offset < max_address)
     {
         uint64_t address = area + offset;
-        /* Write one location */
-        WRITE64(address, pattern);
+        /* Write one location with pattern xor address */
+        uint64_t p = pattern ^ address;
+        WRITE64(address, p);
         __bdk_dram_flush_to_mem(address);
-        /* Read all of the area to make sure no other location was written */
-        uint64_t a = area;
-        while (a < max_address)
-        {
-            if (a + 256 < max_address)
-                BDK_PREFETCH(a + 256, 0);
-            for (int i=0; i<16; i++)
-            {
-                uint64_t data = READ64(a);
-                uint64_t correct = (a == address) ? pattern : 0;
-                if (data != correct)
-                {
-                    failures++;
-                    __bdk_dram_report_error(a, data, correct, 0);
-                }
-                a += 8;
-            }
-        }
-        /* Write zero back so the region is all zero again */
-        WRITE64(address, 0);
-        /* Flush everything so we can detect mistaken writes */
-        __bdk_dram_flush_to_mem_range(area, max_address);
-        /* Invert the pattern so more stuff wiggles */
-        pattern = ~pattern;
         offset <<= 1;
+    }
+
+    /* Read all of the area to make sure no other locations were written */
+    uint64_t a = area;
+    offset = 0x8;
+    uint64_t next_write = area + offset;
+    while (a < max_address)
+    {
+        if (a + 256 < max_address)
+            BDK_PREFETCH(a + 256, 0);
+        for (int i=0; i<16; i++)
+        {
+            uint64_t data = READ64(a);
+            uint64_t correct;
+            if (a == next_write)
+            {
+                correct = pattern ^ next_write;
+                offset <<= 1;
+                next_write = area + offset;
+            }
+            else
+                correct = 0;
+            if (bdk_unlikely(data != correct))
+            {
+                failures++;
+                __bdk_dram_report_error(a, data, correct, 0);
+            }
+            a += 8;
+        }
     }
 
     return failures;
