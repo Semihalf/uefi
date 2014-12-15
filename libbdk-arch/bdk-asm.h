a9e7aa20223a6989e40932d8a01085821800a4ac
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2014-12-15 15:09:33 +0000

    Fix the definition of the Dcache invalidate. For some strange reason we decided to not follow the ARM spec, so the DC instructions don't invalidate the cache. Of course this isn't documented, welcome to the new Cavium...
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@110219 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-arch/bdk-asm.h
+++ libbdk-arch/bdk-asm.h
@@ -38,13 +38,14 @@
 // a normal prefetch
 #define BDK_PREFETCH(address, offset) BDK_PREFETCH_PREFX("PLDL1KEEP", address, offset)
 #define BDK_ICACHE_INVALIDATE  { asm volatile ("ic iallu" : : ); }    // invalidate entire icache
-#define BDK_DCACHE_INVALIDATE  { asm volatile ("dc civac,xzr" : : ); } // invalidate entire dcache
 #define BDK_SYS_CVMCACHE_WBI_L2 "#0,c11,c1,#2"
 #define BDK_SYS_CVMCACHE_WB_L2 "#0,c11,c1,#3"
 #define BDK_SYS_CVMCACHE_LCK_L2 "#0,c11,c1,#4"
+#define BDK_SYS_CVMCACHE_INVALL_DC "#0,c11,c0,#2"
 #define BDK_CACHE_WBI_L2(address) { asm volatile ("sys " BDK_SYS_CVMCACHE_WBI_L2 ", %0" : : "r" (address)); } // Push to memory, invalidate, and unlock
 #define BDK_CACHE_WB_L2(address) { asm volatile ("sys " BDK_SYS_CVMCACHE_WB_L2 ", %0" : : "r" (address)); } // Push to memory, don't invalidate, don't unlock
 #define BDK_CACHE_LCK_L2(address) { asm volatile ("sys " BDK_SYS_CVMCACHE_LCK_L2 ", %0" : : "r" (address)); } // Lock into L2
+#define BDK_DCACHE_INVALIDATE { asm volatile ("sys " BDK_SYS_CVMCACHE_INVALL_DC ", xzr"); } // Invalidate the entire Dcache on local core
 
 #endif	/* __ASSEMBLER__ */
 
