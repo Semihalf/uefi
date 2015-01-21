3d865686bfde1a23fa8fdebdc2eaac0663b790c0
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2015-01-21 02:21:52 +0000

    Remove dead code line from QLM menu
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@111933 c83883e8-9f28-0410-baf1-f2025811e16a

--- lua-modules/qlm_menu.lua
+++ lua-modules/qlm_menu.lua
@@ -29,7 +29,6 @@ repeat
         ref_clock = (ref_clock + 500) / 1000
         local option
         if config_speed == 0 then
-            option = "%s %d - Disabled" % {label, qlm_num}
             option = "%s %d - Disabled, ref %3d.%03d Mhz" % {label, qlm_num, ref_clock / 1000, ref_clock % 1000}
         else
             option = "%s %d - %s @%2d.%03d GBaud, ref %3d.%03d Mhz" % {label, qlm_num, config_mode, config_speed / 1000, config_speed % 1000, ref_clock / 1000, ref_clock % 1000}
