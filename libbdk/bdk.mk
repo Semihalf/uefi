87e8ce3ba55e017e0c0d987569e358c6f8431eeb
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2015-03-16 17:26:29 +0000

    Support linking the BDK for non-zero nodes and booting off them
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@114580 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk/bdk.mk
+++ libbdk/bdk.mk
@@ -2,6 +2,9 @@ ifndef BDK_ROOT
 $(error Define BDK_ROOT in the environment)
 endif
 
+BDK_LINK_ADDRESS?=0x0 # Node 0
+#BDK_LINK_ADDRESS?=0x10000000000 # Node 1
+
 SHELL=/bin/bash
 
 #
@@ -38,6 +41,7 @@ STRIP=$(CROSS)strip
 # Setup the compile flags
 #
 CPPFLAGS = $(BDK_EXTRA_CPPFLAGS)
+CPPFLAGS += -DBDK_LINK_ADDRESS=$(BDK_LINK_ADDRESS)
 CPPFLAGS += -I $(BDK_ROOT)/libbdk -I $(BDK_ROOT)/liblua -I $(BDK_ROOT)/libc/${LIBC_DIR}/include
 CFLAGS = -Wall -Wextra -Wno-unused-parameter -Winline -mcpu=thunderx -Os -g -std=gnu99 -fno-asynchronous-unwind-tables
 CFLAGS += -ffunction-sections
@@ -47,9 +51,11 @@ ASFLAGS = $(CFLAGS)
 LDFLAGS  = -nostdlib -nostartfiles
 LDFLAGS += -L $(BDK_ROOT)/libbdk $(BDK_ROOT)/libbdk-os/bdk-start.o
 LDFLAGS += -Wl,-T -Wl,bdk.ld -Wl,-Map -Wl,$@.map -Wl,--gc-sections
+LDFLAGS += -Wl,--defsym=BDK_LINK_ADDRESS=$(BDK_LINK_ADDRESS)
 LDLIBS = -lbdk -lgcc
 
-IMAGE_END=`${CROSS}objdump -t $^ | grep " _end$$" | sed "s/^0\([0-9a-f]*\).*/print 0x\1/g" | python`
+# This leaves off the bits [63:40] as these contain the node ID, which we don't want
+IMAGE_END=`${CROSS}objdump -t $^ | grep " _end$$" | sed "s/^00000[0-9]\([0-9a-f]*\).*/print 0x\1/g" | python`
 
 #
 # This is needed to generate the depends files
