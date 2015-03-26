b1f62170649a4ae0cf459610c131825522b3b1db
jestabrook <jestabrook@c83883e8-9f28-0410-baf1-f2025811e16a>
2015-03-26 15:45:57 +0000

    Clean-up and consolidate.
    Consolidation of static SPD data tables into common files.
    
    Clean-up of CRB-2S configuration so that the bdk-boot image will build,
    and then work without throwing L2C error messages.
    
    
    
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder.ddr4_rdimm@115279 c83883e8-9f28-0410-baf1-f2025811e16a

---
+++ libdram/configs/common-spd-tables.h
@@ -0,0 +1,4 @@
+extern const uint8_t *MT18ASF1G72AZ_2G1AXESZG_SPD;
+extern const uint8_t *MT18ASF1G72AZ_2G1AYESZG_SPD;
+extern const uint8_t *WD3UN802G13LSD_SPD;
+extern const uint8_t *TS512MLK72V8N_SPD;
