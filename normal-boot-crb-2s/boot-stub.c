7901dcd953edcc30bf634129624ab956e7199ebd
fhooker <fhooker@c83883e8-9f28-0410-baf1-f2025811e16a>
2015-04-13 17:31:57 +0000

    Revert changes made in r116177 and r116177.  I intended to test them on a branch first.
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@116182 c83883e8-9f28-0410-baf1-f2025811e16a

--- normal-boot-crb-2s/boot-stub.c
+++ normal-boot-crb-2s/boot-stub.c
@@ -11,11 +11,13 @@
 /* On boards using software CCPI init, this is the speed to bringup CCPI at */
 #define CCPI_INIT_SPEED 10312
 /* Name of DRAM config for master node 0 */
-#define DRAM_NODE0 crb_2s_V3
+#define DRAM_NODE0 crb_2s_node0
+//#define DRAM_NODE0 crb_2s_V1    /* Obsolete ODT configuration */
 /* Enable verbose logging from DRAM initialization (0 or 1) */
-#define DRAM_VERBOSE 1
+#define DRAM_VERBOSE 0
 /* Name of DRAM config for slave node 1 */
-#define DRAM_NODE1 crb_2s_V3
+#define DRAM_NODE1 crb_2s_node1
+//#define DRAM_NODE1 crb_2s_V1    /* Obsolete ODT configuration */
 /* If non-zero, enable a watchdog timer to reset the chip ifwe hang during init.
    Value is in 262144 SCLK cycle intervals, max of 16 bits */
 #define WATCHDOG_TIMEOUT 8010 /* 3sec at 700Mhz */
