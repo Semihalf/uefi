14c17d5399412b602a76904995dcfd05bb82237c
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2015-04-24 16:06:08 +0000

    Fix PHY addresses for SGMII in a dual node EBB880x setup
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@116794 c83883e8-9f28-0410-baf1-f2025811e16a

--- lua-modules/board-ebb8800.lua
+++ lua-modules/board-ebb8800.lua
@@ -6,19 +6,23 @@ local set_config = cavium.c.bdk_config_set
 --------------------------------------------------------------
 -- Configuring PHY addresses for various BGX interfaces
 --------------------------------------------------------------
--- This code sets the default MDIO addresses for SGMII PHYs
--- The number is "(0x100 * bus) + phy".
+-- For MDIO:
+--     Bits[31:24]: Node ID, 0xff for node the etherent device is on
+--     Bits[23:16]: 0
+--     Bits[15:12]: 0=MDIO
+--     Bits[11:8]: MDIO bus number
+--     Bits[7:0]: MDIO address
 
 -- BGX0 (QLM0)
-set_config(cavium.CONFIG_PHY_IF0_PORT0, 0)
-set_config(cavium.CONFIG_PHY_IF0_PORT1, 1)
-set_config(cavium.CONFIG_PHY_IF0_PORT2, 2)
-set_config(cavium.CONFIG_PHY_IF0_PORT3, 3)
+set_config(cavium.CONFIG_PHY_IF0_PORT0, 0xff000000)
+set_config(cavium.CONFIG_PHY_IF0_PORT1, 0xff000001)
+set_config(cavium.CONFIG_PHY_IF0_PORT2, 0xff000002)
+set_config(cavium.CONFIG_PHY_IF0_PORT3, 0xff000003)
 -- BGX1 (QLM1)
-set_config(cavium.CONFIG_PHY_IF1_PORT0, 0x104)
-set_config(cavium.CONFIG_PHY_IF1_PORT1, 0x105)
-set_config(cavium.CONFIG_PHY_IF1_PORT2, 0x106)
-set_config(cavium.CONFIG_PHY_IF1_PORT3, 0x107)
+set_config(cavium.CONFIG_PHY_IF1_PORT0, 0xff000104)
+set_config(cavium.CONFIG_PHY_IF1_PORT1, 0xff000105)
+set_config(cavium.CONFIG_PHY_IF1_PORT2, 0xff000106)
+set_config(cavium.CONFIG_PHY_IF1_PORT3, 0xff000107)
 
 -- For RXAUI, We're using a Marvel PHY on the plugin modules. The code below
 -- programs all BGXs to use "Interleaved running disparity", which is required
