8059a40d7138850b8bafb57aa7e4c1c9bd54d99e
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2015-05-15 22:47:40 +0000

    Update CRB-2S script to have a boolean to allow watchdog reboots while booting. Default to off, which is what the rev3 board should support
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@117890 c83883e8-9f28-0410-baf1-f2025811e16a

--- tests/hardware/crb-2s-normal-boot-two-node-short-dram-test.py
+++ tests/hardware/crb-2s-normal-boot-two-node-short-dram-test.py
@@ -3,6 +3,8 @@ import connection
 import test_boot
 import test_dram
 
+USE_WATCHDOG = False
+
 def main(log):
     console, mcu1, mcu2 = boards.parseArgs()
     count = 0
@@ -10,14 +12,16 @@ def main(log):
     while True:
         count += 1
         print "Loop %d" % count
-
-        cnx.powerCycle()
-        cnx.waitfor("Verifying image", timeout=300)
-        cnx.match("Putting all cores except this one in reset")
-        cnx.match("Jumping to image at")
-        cnx.waitfor("---")
-        test_boot.wait_for_bdk_boot(cnx)
-        test_boot.wait_for_main_menu(cnx)
+        if USE_WATCHDOG:
+            cnx.powerCycle()
+            cnx.waitfor("Verifying image", timeout=300)
+            cnx.match("Putting all cores except this one in reset")
+            cnx.match("Jumping to image at")
+            cnx.waitfor("---")
+            test_boot.wait_for_bdk_boot(cnx)
+            test_boot.wait_for_main_menu(cnx)
+        else:
+            test_boot.boot_normal(cnx)
         test_dram.dram_short(cnx)
     cnx.close()
 
