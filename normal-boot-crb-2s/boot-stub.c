cd0d917116a75c1a409add1214b45e26809b95b2
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2015-06-03 21:46:01 +0000

    Add SFP+ and QSFP+ PHY programming for leds from Eugene
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@118882 c83883e8-9f28-0410-baf1-f2025811e16a

--- normal-boot-crb-2s/boot-stub.c
+++ normal-boot-crb-2s/boot-stub.c
@@ -605,6 +605,36 @@ int main(void)
         }
     }
 
+    /* Setup the status lights for the PHYs */
+    if (bdk_numa_exists(BDK_NODE_0))
+    {
+        int bus = 1;
+        int phy_id = 1;
+        int device = 0;
+        /* CPU0 SFP+ */
+        bdk_mdio_45_write(node, bus, phy_id, device, 0x010c, 0xB610);
+        bdk_mdio_45_write(node, bus, phy_id, device, 0x010d, 0x0012);
+        bdk_mdio_45_write(node, bus, phy_id, device, 0x0110, 0x0000);
+        bdk_mdio_45_write(node, bus, phy_id, device, 0x0111, 0x0000);
+        /* CPU0 QSFP+ */
+        phy_id = 0;
+        bdk_mdio_45_write(node, bus, phy_id, device, 0x0106, 0x6610);
+        bdk_mdio_45_write(node, bus, phy_id, device, 0x0107, 0x0012);
+        bdk_mdio_45_write(node, bus, phy_id, device, 0x010a, 0x0000);
+        bdk_mdio_45_write(node, bus, phy_id, device, 0x010b, 0x0000);
+    }
+    if (bdk_numa_exists(BDK_NODE_1))
+    {
+        int bus = 1;
+        int phy_id = 1;
+        int device = 0;
+        /* CPU1 SFP+ */
+        bdk_mdio_45_write(node, bus, phy_id, device, 0x0106, 0x6610);
+        bdk_mdio_45_write(node, bus, phy_id, device, 0x0107, 0x0032);
+        bdk_mdio_45_write(node, bus, phy_id, device, 0x010a, 0x0000);
+        bdk_mdio_45_write(node, bus, phy_id, device, 0x010b, 0x0000);
+    }
+
     /* Initialize BGX, ready for driver */
     for (int n = 0; n < BDK_NUMA_MAX_NODES; n++)
     {
