9e907852c907f7118ce2f468bd4348ca8dd4bc15
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2015-06-23 23:32:26 +0000

    [libfatfs] Base custom types on stdint.h.
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@120343 c83883e8-9f28-0410-baf1-f2025811e16a

--- libfatfs/integer.h
+++ libfatfs/integer.h
@@ -5,22 +5,24 @@
 #ifndef _FF_INTEGER
 #define _FF_INTEGER
 
+#include <stdint.h>
+
 /* Definitions for ThunderX CN88XX architecture (AARCH64) */
 
 /* This type MUST be 8 bit */
-typedef unsigned char	BYTE;
+typedef uint8_t			BYTE;
 
 /* These types MUST be 16 bit */
-typedef short			SHORT;
-typedef unsigned short	WORD;
-typedef unsigned short	WCHAR;
+typedef int16_t			SHORT;
+typedef uint16_t		WORD;
+typedef uint16_t		WCHAR;
 
 /* These types MUST be 16 bit or 32 bit */
-typedef int				INT;
-typedef unsigned int	UINT;
+typedef int32_t			INT;
+typedef uint32_t		UINT;
 
 /* These types MUST be 32 bit */
-typedef int				LONG;
-typedef unsigned int	DWORD;
+typedef int32_t			LONG;
+typedef uint32_t		DWORD;
 
 #endif
