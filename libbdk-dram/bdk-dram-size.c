cdfb258be41c6d65cf2b3b0da71c7b7b88ad4fce
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2015-06-27 21:42:55 +0000

    Make the BDK think the emulator has 1GB of memory
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@120772 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-dram/bdk-dram-size.c
+++ libbdk-dram/bdk-dram-size.c
@@ -119,6 +119,8 @@ int __bdk_dram_is_rdimm(bdk_node_t node, int lmc)
  */
 uint64_t bdk_dram_get_size_mbytes(int node)
 {
+    if (bdk_is_platform(BDK_PLATFORM_EMULATOR))
+        return 1 << 10; /* 1GB is just a guess */
     /* Return zero if dram isn't enabled */
     if (!__bdk_is_dram_enabled(node))
         return 0;
