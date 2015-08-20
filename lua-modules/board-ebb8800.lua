d74198e3c38b80559342823b3e736930cf82a9fa
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2015-08-20 22:09:23 +0000

    Updage SGMII PHY addresses for CN86XX
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@123815 c83883e8-9f28-0410-baf1-f2025811e16a

--- lua-modules/board-ebb8800.lua
+++ lua-modules/board-ebb8800.lua
@@ -13,16 +13,29 @@ local set_config = cavium.c.bdk_config_set
 --     Bits[11:8]: MDIO bus number
 --     Bits[7:0]: MDIO address
 
--- BGX0 (QLM0)
-set_config(cavium.CONFIG_PHY_IF0_PORT0, 0xff000000)
-set_config(cavium.CONFIG_PHY_IF0_PORT1, 0xff000001)
-set_config(cavium.CONFIG_PHY_IF0_PORT2, 0xff000002)
-set_config(cavium.CONFIG_PHY_IF0_PORT3, 0xff000003)
--- BGX1 (QLM1)
-set_config(cavium.CONFIG_PHY_IF1_PORT0, 0xff000104)
-set_config(cavium.CONFIG_PHY_IF1_PORT1, 0xff000105)
-set_config(cavium.CONFIG_PHY_IF1_PORT2, 0xff000106)
-set_config(cavium.CONFIG_PHY_IF1_PORT3, 0xff000107)
+if cavium.is_altpkg(cavium.CN88XX) then
+    -- BGX0 (QLM0)
+    set_config(cavium.CONFIG_PHY_IF0_PORT0, 0xff000108)
+    set_config(cavium.CONFIG_PHY_IF0_PORT1, 0xff000109)
+    set_config(cavium.CONFIG_PHY_IF0_PORT2, 0xff00010a)
+    set_config(cavium.CONFIG_PHY_IF0_PORT3, 0xff00010b)
+    -- BGX1 (QLM1)
+    set_config(cavium.CONFIG_PHY_IF1_PORT0, 0xff00010c)
+    set_config(cavium.CONFIG_PHY_IF1_PORT1, 0xff00010d)
+    set_config(cavium.CONFIG_PHY_IF1_PORT2, 0xff00010e)
+    set_config(cavium.CONFIG_PHY_IF1_PORT3, 0xff00010f)
+else
+    -- BGX0 (QLM0)
+    set_config(cavium.CONFIG_PHY_IF0_PORT0, 0xff000000)
+    set_config(cavium.CONFIG_PHY_IF0_PORT1, 0xff000001)
+    set_config(cavium.CONFIG_PHY_IF0_PORT2, 0xff000002)
+    set_config(cavium.CONFIG_PHY_IF0_PORT3, 0xff000003)
+    -- BGX1 (QLM1)
+    set_config(cavium.CONFIG_PHY_IF1_PORT0, 0xff000104)
+    set_config(cavium.CONFIG_PHY_IF1_PORT1, 0xff000105)
+    set_config(cavium.CONFIG_PHY_IF1_PORT2, 0xff000106)
+    set_config(cavium.CONFIG_PHY_IF1_PORT3, 0xff000107)
+end
 
 cavium.c.bdk_qlm_auto_config(cavium.MASTER_NODE)
 
