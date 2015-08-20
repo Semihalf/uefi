dd9c9da512c68b3526f4ea147911b467f99e2f8a
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2015-08-20 22:09:19 +0000

    Update QLM config menu for CN86XX
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@123813 c83883e8-9f28-0410-baf1-f2025811e16a

--- lua-modules/qlm_menu_cn88xx.lua
+++ lua-modules/qlm_menu_cn88xx.lua
@@ -8,73 +8,135 @@ require("menu")
 -- are here, but this should cover most cases. Note that QLMs that
 -- have the same list share the same lists, so take care making
 -- modifications.
+local qlm_display_order = {}
 local qlm_modes = {}
-qlm_modes[0] = {
-    {"sgmii", "SGMII   @  1 Gbps", cavium.QLM_MODE_SGMII, 1250, 0},
-    {"xaui",  "XAUI    @ 10 Gbps", cavium.QLM_MODE_XAUI_1X4, 3125, 0},
-    {"dxaui", "DXAUI   @ 20 Gbps", cavium.QLM_MODE_XAUI_1X4, 6250, 0},
-    {"rxaui", "RXAUI   @ 10 Gbps", cavium.QLM_MODE_RXAUI_2X2, 6250, 0},
-    {"xfi",   "XFI     @ 10 Gbps", cavium.QLM_MODE_XFI_4X1, 10312, 0},
-    {"xlaui", "XLAUI   @ 40 Gbps", cavium.QLM_MODE_XLAUI_1X4, 10312, 0},
-    {"10g",   "10G-KR  @ 10 Gbps", cavium.QLM_MODE_10G_KR_4X1, 10312, 0},
-    {"40g",   "40G-KR4 @ 40 Gbps", cavium.QLM_MODE_40G_KR4_1X4, 10312, 0},
-    {"dis",   "Disabled", cavium.QLM_MODE_DISABLED, 0, 0}
-}
-qlm_modes[1] = qlm_modes[0]
-qlm_modes[2] = {
-    {"rc-x4-g1", "PCIe x4 - GEN 1", cavium.QLM_MODE_PCIE_1X4, 2500, 0},
-    {"rc-x4-g2", "PCIe x4 - GEN 2", cavium.QLM_MODE_PCIE_1X4, 5000, 0},
-    {"rc-x4-g3", "PCIe x4 - GEN 3", cavium.QLM_MODE_PCIE_1X4, 8000, 0},
-    {"rc-x8-g1", "PCIe x8 - GEN 1", cavium.QLM_MODE_PCIE_1X8, 2500, 0},
-    {"rc-x8-g2", "PCIe x8 - GEN 2", cavium.QLM_MODE_PCIE_1X8, 5000, 0},
-    {"rc-x8-g3", "PCIe x8 - GEN 3", cavium.QLM_MODE_PCIE_1X8, 8000, 0},
-    {"sata1",    "SATA    @ 1.500 Gbps", cavium.QLM_MODE_SATA_4X1, 1500, 0},
-    {"sata2",    "SATA    @ 3.000 Gbps", cavium.QLM_MODE_SATA_4X1, 3000, 0},
-    {"sata3",    "SATA    @ 6.000 Gbps", cavium.QLM_MODE_SATA_4X1, 6000, 0},
-    {"dis",      "Disabled", cavium.QLM_MODE_DISABLED, 0, 0}
-}
-qlm_modes[3] = {
-    {"rc-x4-g1", "PCIe x4 - GEN 1", cavium.QLM_MODE_PCIE_1X4, 2500, 0},
-    {"rc-x4-g2", "PCIe x4 - GEN 2", cavium.QLM_MODE_PCIE_1X4, 5000, 0},
-    {"rc-x4-g3", "PCIe x4 - GEN 3", cavium.QLM_MODE_PCIE_1X4, 8000, 0},
-    {"sata1",    "SATA    @ 1.500 Gbps", cavium.QLM_MODE_SATA_4X1, 1500, 0},
-    {"sata2",    "SATA    @ 3.000 Gbps", cavium.QLM_MODE_SATA_4X1, 3000, 0},
-    {"sata3",    "SATA    @ 6.000 Gbps", cavium.QLM_MODE_SATA_4X1, 6000, 0},
-    {"dis",      "Disabled", cavium.QLM_MODE_DISABLED, 0, 0}
-}
-qlm_modes[4] = {
-    {"rc-x4-g1", "PCIe x4 - GEN 1", cavium.QLM_MODE_PCIE_1X4, 2500, 0},
-    {"rc-x4-g2", "PCIe x4 - GEN 2", cavium.QLM_MODE_PCIE_1X4, 5000, 0},
-    {"rc-x4-g3", "PCIe x4 - GEN 3", cavium.QLM_MODE_PCIE_1X4, 8000, 0},
-    {"rc-x8-g1", "PCIe x8 - GEN 1", cavium.QLM_MODE_PCIE_1X8, 2500, 0},
-    {"rc-x8-g2", "PCIe x8 - GEN 2", cavium.QLM_MODE_PCIE_1X8, 5000, 0},
-    {"rc-x8-g3", "PCIe x8 - GEN 3", cavium.QLM_MODE_PCIE_1X8, 8000, 0},
-    {"dis",      "Disabled", cavium.QLM_MODE_DISABLED, 0, 0}
-}
-qlm_modes[5] = {
-    {"rc-x4-g1", "PCIe x4 - GEN 1", cavium.QLM_MODE_PCIE_1X4, 2500, 0},
-    {"rc-x4-g2", "PCIe x4 - GEN 2", cavium.QLM_MODE_PCIE_1X4, 5000, 0},
-    {"rc-x4-g3", "PCIe x4 - GEN 3", cavium.QLM_MODE_PCIE_1X4, 8000, 0},
-    {"dis",      "Disabled", cavium.QLM_MODE_DISABLED, 0, 0}
-}
-qlm_modes[6] = qlm_modes[2]
-qlm_modes[7] = qlm_modes[3]
--- GSER 8-13 are OCI only and are normally covered by pin straps
-qlm_modes[8] = {
-    {"ccpi1",    "CCPI    @ 1.250 Gbps", cavium.QLM_MODE_OCI, 1250, 0},
-    {"ccpi2",    "CCPI    @ 2.500 Gbps", cavium.QLM_MODE_OCI, 2500, 0},
-    {"ccpi3",    "CCPI    @ 3.125 Gbps", cavium.QLM_MODE_OCI, 3125, 0},
-    {"ccpi5",    "CCPI    @ 5.000 Gbps", cavium.QLM_MODE_OCI, 5000, 0},
-    {"ccpi6",    "CCPI    @ 6.250 Gbps", cavium.QLM_MODE_OCI, 6250, 0},
-    {"ccpi8",    "CCPI    @ 8.000 Gbps", cavium.QLM_MODE_OCI, 8000, 0},
-    {"ccpi10",   "CCPI    @ 10.3125 Gbps", cavium.QLM_MODE_OCI, 10312, 0},
-    {"dis",      "Disabled", cavium.QLM_MODE_DISABLED, 0, 0}
-}
-qlm_modes[9] = qlm_modes[8]
-qlm_modes[10] = qlm_modes[8]
-qlm_modes[11] = qlm_modes[8]
-qlm_modes[12] = qlm_modes[8]
-qlm_modes[13] = qlm_modes[8]
+local qlm_external_name = {}
+if not cavium.is_altpkg(cavium.CN88XX) then
+    -- CN88XX in a standard package
+    for qlm=0,14 do
+        qlm_display_order[qlm] = qlm
+        qlm_external_name[qlm] = "QLM%d" % qlm
+    end
+    qlm_modes[0] = {
+        {"sgmii", "SGMII   @  1 Gbps", cavium.QLM_MODE_SGMII, 1250, 0},
+        {"xaui",  "XAUI    @ 10 Gbps", cavium.QLM_MODE_XAUI_1X4, 3125, 0},
+        {"dxaui", "DXAUI   @ 20 Gbps", cavium.QLM_MODE_XAUI_1X4, 6250, 0},
+        {"rxaui", "RXAUI   @ 10 Gbps", cavium.QLM_MODE_RXAUI_2X2, 6250, 0},
+        {"xfi",   "XFI     @ 10 Gbps", cavium.QLM_MODE_XFI_4X1, 10312, 0},
+        {"xlaui", "XLAUI   @ 40 Gbps", cavium.QLM_MODE_XLAUI_1X4, 10312, 0},
+        {"10g",   "10G-KR  @ 10 Gbps", cavium.QLM_MODE_10G_KR_4X1, 10312, 0},
+        {"40g",   "40G-KR4 @ 40 Gbps", cavium.QLM_MODE_40G_KR4_1X4, 10312, 0},
+        {"dis",   "Disabled", cavium.QLM_MODE_DISABLED, 0, 0}
+    }
+    qlm_modes[1] = qlm_modes[0]
+    qlm_modes[2] = {
+        {"rc-x4-g1", "PCIe x4 - GEN 1", cavium.QLM_MODE_PCIE_1X4, 2500, 0},
+        {"rc-x4-g2", "PCIe x4 - GEN 2", cavium.QLM_MODE_PCIE_1X4, 5000, 0},
+        {"rc-x4-g3", "PCIe x4 - GEN 3", cavium.QLM_MODE_PCIE_1X4, 8000, 0},
+        {"rc-x8-g1", "PCIe x8 - GEN 1", cavium.QLM_MODE_PCIE_1X8, 2500, 0},
+        {"rc-x8-g2", "PCIe x8 - GEN 2", cavium.QLM_MODE_PCIE_1X8, 5000, 0},
+        {"rc-x8-g3", "PCIe x8 - GEN 3", cavium.QLM_MODE_PCIE_1X8, 8000, 0},
+        {"sata1",    "SATA    @ 1.500 Gbps", cavium.QLM_MODE_SATA_4X1, 1500, 0},
+        {"sata2",    "SATA    @ 3.000 Gbps", cavium.QLM_MODE_SATA_4X1, 3000, 0},
+        {"sata3",    "SATA    @ 6.000 Gbps", cavium.QLM_MODE_SATA_4X1, 6000, 0},
+        {"dis",      "Disabled", cavium.QLM_MODE_DISABLED, 0, 0}
+    }
+    qlm_modes[3] = {
+        {"rc-x4-g1", "PCIe x4 - GEN 1", cavium.QLM_MODE_PCIE_1X4, 2500, 0},
+        {"rc-x4-g2", "PCIe x4 - GEN 2", cavium.QLM_MODE_PCIE_1X4, 5000, 0},
+        {"rc-x4-g3", "PCIe x4 - GEN 3", cavium.QLM_MODE_PCIE_1X4, 8000, 0},
+        {"sata1",    "SATA    @ 1.500 Gbps", cavium.QLM_MODE_SATA_4X1, 1500, 0},
+        {"sata2",    "SATA    @ 3.000 Gbps", cavium.QLM_MODE_SATA_4X1, 3000, 0},
+        {"sata3",    "SATA    @ 6.000 Gbps", cavium.QLM_MODE_SATA_4X1, 6000, 0},
+        {"dis",      "Disabled", cavium.QLM_MODE_DISABLED, 0, 0}
+    }
+    qlm_modes[4] = {
+        {"rc-x4-g1", "PCIe x4 - GEN 1", cavium.QLM_MODE_PCIE_1X4, 2500, 0},
+        {"rc-x4-g2", "PCIe x4 - GEN 2", cavium.QLM_MODE_PCIE_1X4, 5000, 0},
+        {"rc-x4-g3", "PCIe x4 - GEN 3", cavium.QLM_MODE_PCIE_1X4, 8000, 0},
+        {"rc-x8-g1", "PCIe x8 - GEN 1", cavium.QLM_MODE_PCIE_1X8, 2500, 0},
+        {"rc-x8-g2", "PCIe x8 - GEN 2", cavium.QLM_MODE_PCIE_1X8, 5000, 0},
+        {"rc-x8-g3", "PCIe x8 - GEN 3", cavium.QLM_MODE_PCIE_1X8, 8000, 0},
+        {"dis",      "Disabled", cavium.QLM_MODE_DISABLED, 0, 0}
+    }
+    qlm_modes[5] = {
+        {"rc-x4-g1", "PCIe x4 - GEN 1", cavium.QLM_MODE_PCIE_1X4, 2500, 0},
+        {"rc-x4-g2", "PCIe x4 - GEN 2", cavium.QLM_MODE_PCIE_1X4, 5000, 0},
+        {"rc-x4-g3", "PCIe x4 - GEN 3", cavium.QLM_MODE_PCIE_1X4, 8000, 0},
+        {"dis",      "Disabled", cavium.QLM_MODE_DISABLED, 0, 0}
+    }
+    qlm_modes[6] = qlm_modes[2]
+    qlm_modes[7] = qlm_modes[3]
+    -- GSER 8-13 are OCI only and are normally covered by pin straps
+    qlm_modes[8] = {
+        {"ccpi1",    "CCPI    @ 1.250 Gbps", cavium.QLM_MODE_OCI, 1250, 0},
+        {"ccpi2",    "CCPI    @ 2.500 Gbps", cavium.QLM_MODE_OCI, 2500, 0},
+        {"ccpi3",    "CCPI    @ 3.125 Gbps", cavium.QLM_MODE_OCI, 3125, 0},
+        {"ccpi5",    "CCPI    @ 5.000 Gbps", cavium.QLM_MODE_OCI, 5000, 0},
+        {"ccpi6",    "CCPI    @ 6.250 Gbps", cavium.QLM_MODE_OCI, 6250, 0},
+        {"ccpi8",    "CCPI    @ 8.000 Gbps", cavium.QLM_MODE_OCI, 8000, 0},
+        {"ccpi10",   "CCPI    @ 10.3125 Gbps", cavium.QLM_MODE_OCI, 10312, 0},
+        {"dis",      "Disabled", cavium.QLM_MODE_DISABLED, 0, 0}
+    }
+    qlm_modes[9] = qlm_modes[8]
+    qlm_modes[10] = qlm_modes[8]
+    qlm_modes[11] = qlm_modes[8]
+    qlm_modes[12] = qlm_modes[8]
+    qlm_modes[13] = qlm_modes[8]
+else
+    -- CN86XX, CN88XX in a CN83XX compatable package
+    qlm_display_order[0] = 4
+    qlm_display_order[1] = 5
+    qlm_display_order[2] = 0
+    qlm_display_order[3] = 1
+    qlm_display_order[4] = 2
+    qlm_display_order[5] = 6
+    qlm_display_order[6] = 7
+    qlm_external_name[4] = "QLM0(Internal QLM4)"
+    qlm_modes[4] = {
+        {"rc-x4-g1", "PCIe x4 - GEN 1", cavium.QLM_MODE_PCIE_1X4, 2500, 0},
+        {"rc-x4-g2", "PCIe x4 - GEN 2", cavium.QLM_MODE_PCIE_1X4, 5000, 0},
+        {"rc-x4-g3", "PCIe x4 - GEN 3", cavium.QLM_MODE_PCIE_1X4, 8000, 0},
+        {"rc-x8-g1", "PCIe x8 - GEN 1", cavium.QLM_MODE_PCIE_1X8, 2500, 0},
+        {"rc-x8-g2", "PCIe x8 - GEN 2", cavium.QLM_MODE_PCIE_1X8, 5000, 0},
+        {"rc-x8-g3", "PCIe x8 - GEN 3", cavium.QLM_MODE_PCIE_1X8, 8000, 0},
+        {"dis",      "Disabled", cavium.QLM_MODE_DISABLED, 0, 0}
+    }
+    qlm_external_name[5] = "QLM1(Internal QLM5)"
+    qlm_modes[5] = {
+        {"rc-x4-g1", "PCIe x4 - GEN 1", cavium.QLM_MODE_PCIE_1X4, 2500, 0},
+        {"rc-x4-g2", "PCIe x4 - GEN 2", cavium.QLM_MODE_PCIE_1X4, 5000, 0},
+        {"rc-x4-g3", "PCIe x4 - GEN 3", cavium.QLM_MODE_PCIE_1X4, 8000, 0},
+        {"dis",      "Disabled", cavium.QLM_MODE_DISABLED, 0, 0}
+    }
+    qlm_external_name[0] = "QLM2(Internal QLM0)"
+    qlm_modes[0] = {
+        {"sgmii", "SGMII   @  1 Gbps", cavium.QLM_MODE_SGMII, 1250, 0},
+        {"xaui",  "XAUI    @ 10 Gbps", cavium.QLM_MODE_XAUI_1X4, 3125, 0},
+        {"dxaui", "DXAUI   @ 20 Gbps", cavium.QLM_MODE_XAUI_1X4, 6250, 0},
+        {"rxaui", "RXAUI   @ 10 Gbps", cavium.QLM_MODE_RXAUI_2X2, 6250, 0},
+        {"xfi",   "XFI     @ 10 Gbps", cavium.QLM_MODE_XFI_4X1, 10312, 0},
+        {"xlaui", "XLAUI   @ 40 Gbps", cavium.QLM_MODE_XLAUI_1X4, 10312, 0},
+        {"10g",   "10G-KR  @ 10 Gbps", cavium.QLM_MODE_10G_KR_4X1, 10312, 0},
+        {"40g",   "40G-KR4 @ 40 Gbps", cavium.QLM_MODE_40G_KR4_1X4, 10312, 0},
+        {"dis",   "Disabled", cavium.QLM_MODE_DISABLED, 0, 0}
+    }
+    qlm_external_name[1] = "QLM3(Internal QLM1)"
+    qlm_modes[1] = qlm_modes[0]
+
+    qlm_external_name[2] = "DLM4(Internal QLM2)"
+    qlm_modes[2] = {
+        {"sata1",    "SATA    @ 1.500 Gbps", cavium.QLM_MODE_SATA_4X1, 1500, 0},
+        {"sata2",    "SATA    @ 3.000 Gbps", cavium.QLM_MODE_SATA_4X1, 3000, 0},
+        {"sata3",    "SATA    @ 6.000 Gbps", cavium.QLM_MODE_SATA_4X1, 6000, 0},
+        {"dis",      "Disabled", cavium.QLM_MODE_DISABLED, 0, 0}
+    }
+    qlm_external_name[6] = "DLM5(Internal QLM6)"
+    qlm_modes[6] = qlm_modes[2]
+
+    qlm_external_name[7] = "DLM6(Internal QLM7)"
+    qlm_modes[7] = qlm_modes[2]
+end
 
 --
 -- This is called when the user selects a QLM to change its config
@@ -83,7 +145,7 @@ local function do_setup(qlm)
     -- Only the first 8 QLMs can select differnet reference clocks
     -- CCPI QLMs 8-13 always use an external reference clock
     if qlm < 8 then
-        local m = menu.new("Select a reference clock for QLM%d" % qlm)
+        local m = menu.new("Select a reference clock for %s" % qlm_external_name[qlm])
         m:item("ext", "External reference (QLM%d_REF_CLK)" % qlm)
         m:item("c0", "Common clock 0 (QLMC_REF_CLK0)")
         m:item("c1", "Common clock 1 (QLMC_REF_CLK1)")
@@ -101,7 +163,7 @@ local function do_setup(qlm)
     end
 
     -- Select the mode to use
-    local m = menu.new("Select a mode for QLM%d" % qlm)
+    local m = menu.new("Select a mode for %s" % qlm_external_name[qlm])
     for i=1,#qlm_modes[qlm] do
         m:item(qlm_modes[qlm][i][1], qlm_modes[qlm][i][2])
     end
@@ -124,26 +186,31 @@ end
 --
 function qlm_setup_cn88xx()
     local qlm_max = 7
-    -- Allow changing the CCPI QLMs if they are in software mode
-    if cavium.csr.GSERX_SPD(8).SPD == 0xf then
-        qlm_max = 13
+    if not cavium.is_altpkg(cavium.CN88XX) then
+        -- Allow changing the CCPI QLMs if they are in software mode
+        if cavium.csr.GSERX_SPD(8).SPD == 0xf then
+            qlm_max = 13
+        end
+    else
+        qlm_max = 6
     end
     local m = menu.new("Select a QLM to Configure")
     repeat
-        for qlm_num = 0, qlm_max do
+        for index = 0, qlm_max do
+            local qlm_num = qlm_display_order[index]
             local mode = cavium.c.bdk_qlm_get_mode(menu.node, qlm_num)
             local config_mode = cavium.c.bdk_qlm_mode_tostring(mode)
             local config_speed = cavium.c.bdk_qlm_get_gbaud_mhz(menu.node, qlm_num)
             local num_lanes = cavium.c.bdk_qlm_get_lanes(menu.node, qlm_num)
-            local label = (num_lanes == 2) and "DLM" or "QLM"
+            local name = qlm_external_name[qlm_num]
             local ref_clock = cavium.c.bdk_qlm_measure_clock(menu.node, qlm_num)
             local option
             if config_speed == 0 then
-                option = "%s %d - Disabled" % {label, qlm_num}
+                option = "%s - Disabled" % name
             else
-                option = "%s %d - %s @%2d.%03d GBaud, ref %3d.%03d Mhz" % {label, qlm_num, config_mode, config_speed / 1000, config_speed % 1000, ref_clock / 1000000, ref_clock / 1000 % 1000}
+                option = "%s - %s @%2d.%03d GBaud, ref %3d.%03d Mhz" % {name, config_mode, config_speed / 1000, config_speed % 1000, ref_clock / 1000000, ref_clock / 1000 % 1000}
             end
-            m:item("qlm" .. qlm_num, option, do_setup, qlm_num)
+            m:item(name, option, do_setup, qlm_num)
         end
         m:item("quit", "Exit menu")
     until (m:show() == "quit")
