0fc8a58498e016b8281adf53f553e32a4357ec4f
amusthafa <amusthafa@c83883e8-9f28-0410-baf1-f2025811e16a>
2015-08-28 00:09:10 +0000

    Add USB_BIST + remove compilation warnings (copied from thunder.dram_next branch)
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@124203 c83883e8-9f28-0410-baf1-f2025811e16a

--- normal-boot/boot-stub.c
+++ normal-boot/boot-stub.c
@@ -42,6 +42,192 @@ void __bdk_require_depends(void)
 extern const char *boot_device_volstr_for_boot_method(int boot_method);
 
 /**
+ * Search the given device file (SPI or eMMC) and return a list of BDK images
+ * available. The iamges will also be displayed on the console.
+ *
+ * @param path
+ *                   Device file to search
+ * @param max_images Max number of images to return
+ * @param image_names
+ *                   List of image names
+ *
+ * @return Number of images found
+ */
+static int list_images(const char *path, int max_images, char *image_names[])
+{
+    int num_images = 0;
+    DIR dp;
+
+    printf("\nLooking for images in %s\n\n", path);
+    FRESULT res = f_opendir(&dp, path);
+    if (res)
+    {
+        bdk_warn("Target directory %s does not exist (res:%d). Aborting.\n", path, res);
+        return 0;
+    }
+
+    FILINFO info;
+#if _USE_LFN
+    char lfn[_MAX_LFN + 1] = {0};
+    info.lfname = lfn;
+    info.lfsize = sizeof(lfn);
+#endif
+    res = f_findfirst(&dp, &info, path, "*.bin");
+    while (FR_OK == res && info.fname[0] && num_images < max_images)
+    {
+        char fullpath[_MAX_LFN + 1];
+        char *dirstr = info.fattrib & AM_DIR ? "/" : "";
+        char *pd = path[strlen(path)-1] == '/' ? "" : "/";
+#if _USE_LFN
+        /* Note:
+         * We have to add the "/fatfs/" prefix as WE are working directly on
+         * the FATFS API while the calling code works on libc file operation
+         * API.
+         *
+         * Also, the strdup() below will leak mem. We don't really care...
+         */
+        if (info.lfname[0])
+            snprintf(fullpath, sizeof(fullpath), "/fatfs/%s%s%s%s", path, pd, info.lfname, dirstr);
+        else
+            snprintf(fullpath, sizeof(fullpath), "/fatfs/%s%s%s%s", path, pd, info.fname, dirstr);
+#else
+        snprintf(fullpath, sizeof(fullpath), "/fatfs/%s%s%s%s", path, pd, info.fname, dirstr);
+#endif
+
+        bdk_image_header_t header;
+        FILE *fp = fopen(fullpath, "rb");
+        if (fp)
+        {
+            fseek(fp, 0, SEEK_SET);
+            int status = bdk_image_read_header(fp, &header);
+            if (status == 0)
+            {
+                printf("  %d) %s: %s, version %s, %u bytes\n",
+                    num_images+1, fullpath, header.name, header.version, header.length);
+                image_names[num_images] = strdup(fullpath);
+                num_images++;
+            }
+            fclose(fp);
+        }
+        res = f_findnext(&dp, &info);
+    }
+
+    f_closedir(&dp);
+    return num_images;
+}
+
+
+/**
+ * Display a list of images the user can boot from a device file and let
+ * them choose oen to boot.
+ *
+ * @param path
+ *               Device file to search
+ */
+static void choose_image(const char *path)
+{
+    const int MAX_IMAGES = 20;
+    char *image_names[MAX_IMAGES];
+    int num_images = list_images(path, MAX_IMAGES, image_names);
+
+    if (num_images == 0)
+    {
+        printf("No images found\n");
+        return;
+    }
+
+    int use_image = 0;
+    if (num_images > 1)
+    {
+        const char *input = bdk_readline("Image to load: ", NULL, 0);
+        use_image = atoi(input);
+        if ((use_image < 1) || (use_image > num_images))
+        {
+            printf("Not a valid image number\n");
+            return;
+        }
+        use_image--;
+    }
+    else
+        printf("One image found, automatically loading\n");
+    boot_image(image_names[use_image], 0);
+}
+
+static void usb_bist(int node, int cnt, int clear_bist)
+{
+    BDK_CSR_INIT(uctl, node, BDK_USBHX_UCTL_CTL(cnt));
+	if (clear_bist) {
+        uctl.s.clear_bist = 1;
+	}
+	else {
+        uctl.s.clear_bist = 0;
+	}
+    BDK_CSR_WRITE(node, BDK_USBHX_UCTL_CTL(cnt), uctl.u);
+
+    uctl.u = BDK_CSR_READ(node, BDK_USBHX_UCTL_CTL(cnt));
+    uctl.s.start_bist = 1;
+    uctl.s.uphy_rst = 1;
+    uctl.s.uahc_rst = 1;
+    uctl.s.uctl_rst = 1;
+    BDK_CSR_WRITE(node, BDK_USBHX_UCTL_CTL(cnt), uctl.u);
+
+	bdk_wait_usec(10000); //Wait 10 ms
+    BDK_CSR_INIT(bist_status, node, BDK_USBHX_UCTL_BIST_STATUS(cnt));
+    if (bist_status.u)
+    {
+        printf("BIST FAILURE in USB%d clear_bist = %d\n", cnt, clear_bist);
+    }
+    else
+    {
+        printf("BIST CHECK OK in USB%d clear_bist = %d\n", cnt, clear_bist);
+    }
+    uctl.u = BDK_CSR_READ(node, BDK_USBHX_UCTL_CTL(cnt));
+	if (clear_bist) {
+        uctl.s.clear_bist = 0;
+	}
+    uctl.s.start_bist = 0;
+    uctl.s.uphy_rst = 1;
+    uctl.s.uahc_rst = 1;
+    uctl.s.uctl_rst = 1;
+    BDK_CSR_WRITE(node, BDK_USBHX_UCTL_CTL(cnt), uctl.u);
+}
+
+static void slt_boot_image(bdk_node_t node)
+{
+    printf(
+            "\n"
+            "Boot Menu\n"
+            "=========\n"
+            " 1) Load image from MMC, eMMC, or SD\n"
+            " 2) Load image from SPI\n"
+            " 3) Do USB BIST check\n");
+    const char *input;
+    input = bdk_readline("Menu choice: ", NULL, 0);
+    int option = atoi(input);
+    static char boot_device_name[48] = { 0 };
+    switch (option)
+    {
+
+        case 1:
+            sprintf(boot_device_name, "/dev/n%d.mmc0", node);
+            /* Try to load ATF image from raw flash */
+            BDK_TRACE(BOOT_STUB, "Looking for ATF image\n");
+            boot_image(boot_device_name, ATF_ADDRESS);
+            bdk_error("Unable to load image\n");
+            break;
+        case 2:
+            choose_image("SPI0:");
+            break;
+        case 3:
+            usb_bist(0,0,0);
+            usb_bist(0,0,1);
+            usb_bist(0,1,0);
+            usb_bist(0,1,1);
+            break;
+    }
+}
+
+/**
  * Main entry point
  *
  * @return exit code
@@ -167,7 +353,7 @@ int main(void)
     {
         printf("Manufacturing System Level Test Boot Menu:\n");
         slt_boot_image(node);
-        return;
+        return 0;
     }
 
     /* Send status to the BMC: Loading ATF */
@@ -202,146 +388,4 @@ int main(void)
     boot_image(boot_device_name, DIAGS_ADDRESS);
     bdk_error("Image load failed\n");
 }
-/**
- * Search the given device file (SPI or eMMC) and return a list of BDK images
- * available. The iamges will also be displayed on the console.
- *
- * @param path
- *                   Device file to search
- * @param max_images Max number of images to return
- * @param image_names
- *                   List of image names
- *
- * @return Number of images found
- */
-static int list_images(const char *path, int max_images, char *image_names[])
-{
-    int num_images = 0;
-    DIR dp;
-
-    printf("\nLooking for images in %s\n\n", path);
-    FRESULT res = f_opendir(&dp, path);
-    if (res)
-    {
-        bdk_warn("Target directory %s does not exist (res:%d). Aborting.\n", path, res);
-        return 0;
-    }
-
-    FILINFO info;
-#if _USE_LFN
-    char lfn[_MAX_LFN + 1] = {0};
-    info.lfname = lfn;
-    info.lfsize = sizeof(lfn);
-#endif
-    res = f_findfirst(&dp, &info, path, "*.bin");
-    while (FR_OK == res && info.fname[0] && num_images < max_images)
-    {
-        char fullpath[_MAX_LFN + 1];
-        char *dirstr = info.fattrib & AM_DIR ? "/" : "";
-        char *pd = path[strlen(path)-1] == '/' ? "" : "/";
-#if _USE_LFN
-        /* Note:
-         * We have to add the "/fatfs/" prefix as WE are working directly on
-         * the FATFS API while the calling code works on libc file operation
-         * API.
-         *
-         * Also, the strdup() below will leak mem. We don't really care...
-         */
-        if (info.lfname[0])
-            snprintf(fullpath, sizeof(fullpath), "/fatfs/%s%s%s%s", path, pd, info.lfname, dirstr);
-        else
-            snprintf(fullpath, sizeof(fullpath), "/fatfs/%s%s%s%s", path, pd, info.fname, dirstr);
-#else
-        snprintf(fullpath, sizeof(fullpath), "/fatfs/%s%s%s%s", path, pd, info.fname, dirstr);
-#endif
-
-        bdk_image_header_t header;
-        FILE *fp = fopen(fullpath, "rb");
-        if (fp)
-        {
-            fseek(fp, 0, SEEK_SET);
-            int status = bdk_image_read_header(fp, &header);
-            if (status == 0)
-            {
-                printf("  %d) %s: %s, version %s, %u bytes\n",
-                    num_images+1, fullpath, header.name, header.version, header.length);
-                image_names[num_images] = strdup(fullpath);
-                num_images++;
-            }
-            fclose(fp);
-        }
-        res = f_findnext(&dp, &info);
-    }
-
-    f_closedir(&dp);
-    return num_images;
-}
-
-
-/**
- * Display a list of images the user can boot from a device file and let
- * them choose oen to boot.
- *
- * @param path
- *               Device file to search
- */
-static void choose_image(const char *path)
-{
-    const int MAX_IMAGES = 20;
-    char *image_names[MAX_IMAGES];
-    int num_images = list_images(path, MAX_IMAGES, image_names);
-
-    if (num_images == 0)
-    {
-        printf("No images found\n");
-        return;
-    }
-
-    int use_image = 0;
-    if (num_images > 1)
-    {
-        const char *input = bdk_readline("Image to load: ", NULL, 0);
-        use_image = atoi(input);
-        if ((use_image < 1) || (use_image > num_images))
-        {
-            printf("Not a valid image number\n");
-            return;
-        }
-        use_image--;
-    }
-    else
-        printf("One image found, automatically loading\n");
-    boot_image(image_names[use_image], 0);
-}
-
-
-void slt_boot_image(bdk_node_t node)
-{
-    printf(
-            "\n"
-            "Boot Menu\n"
-            "=========\n"
-            " 1) Load image from MMC, eMMC, or SD\n"
-            " 2) Load image from SPI\n");
-    const char *input;
-    input = bdk_readline("Menu choice: ", NULL, 0);
-    int option = atoi(input);
-    static char boot_device_name[48] = { 0 };
-    switch (option)
-    {
-
-        case 1:
-            sprintf(boot_device_name, "/dev/n%d.mmc0", node);
-            /* Try to load ATF image from raw flash */
-            BDK_TRACE(BOOT_STUB, "Looking for ATF image\n");
-            boot_image(boot_device_name, ATF_ADDRESS);
-            bdk_error("Unable to load image\n");
-            break;
-        case 2:
-            choose_image("SPI0:");
-            break;
-    }
-}
-
-
 
