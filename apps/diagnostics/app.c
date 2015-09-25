3014f837d438e4661bd377b5f7c223d24c0c9c25
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2015-09-25 15:24:51 +0000

    Copy the diagnostics app into apps/diagnostics
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@125825 c83883e8-9f28-0410-baf1-f2025811e16a

---
+++ apps/diagnostics/app.c
@@ -0,0 +1,57 @@
+#include <bdk.h>
+
+/**
+ * This function is not defined by the BDK libraries. It must be
+ * defined by all BDK applications. It should be empty except for
+ * containing BDK_REQUIRE() lines. The bdk-init code has a strong
+ * reference to bdk_requires_depends() which then contains strong
+ * references to all needed components.
+ */
+void __bdk_require_depends(void)
+{
+    BDK_REQUIRE(CCPI);
+    BDK_REQUIRE(CSR_DB);
+    BDK_REQUIRE(DRAM_CONFIG);
+    BDK_REQUIRE(DRAM_TEST);
+    BDK_REQUIRE(ENVIRONMENT);
+    BDK_REQUIRE(ERROR_DECODE);
+    BDK_REQUIRE(FS_BOOT);
+    BDK_REQUIRE(FS_FATFS);
+    BDK_REQUIRE(FS_MEM);
+    BDK_REQUIRE(FS_MMC);
+    BDK_REQUIRE(FS_MPI);
+    BDK_REQUIRE(FS_PCIE);
+    BDK_REQUIRE(FS_RAM);
+    BDK_REQUIRE(FS_ROM);
+    BDK_REQUIRE(FS_SATA);
+    BDK_REQUIRE(FS_XMODEM);
+    BDK_REQUIRE(GPIO);
+    BDK_REQUIRE(KEY_MEMORY);
+    BDK_REQUIRE(MDIO);
+    BDK_REQUIRE(MPI);
+    BDK_REQUIRE(PCIE);
+    BDK_REQUIRE(POWER_BURN);
+    BDK_REQUIRE(QLM);
+    BDK_REQUIRE(RNM);
+    BDK_REQUIRE(SATA);
+    BDK_REQUIRE(TNS);
+    BDK_REQUIRE(TRAFFIC_GEN);
+    BDK_REQUIRE(TWSI);
+    BDK_REQUIRE(USB);
+    BDK_REQUIRE(ECAM);
+    BDK_REQUIRE(DRIVER);
+
+    BDK_REQUIRE(DRIVER_SMMU);
+}
+
+/**
+ * Main entry point
+ *
+ * @return exit code
+ */
+int main(void)
+{
+    bdk_watchdog_disable();
+    /* Start Lua. It will execute init.lua */
+    return bdk_lua_start();
+}
