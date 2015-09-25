d043ff1ea9dabb19144dab9c3f544438dc489cad
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2015-09-25 15:25:05 +0000

    Create a generic normal boot image that should boot on any board, with reduced functionality. It replaces bdk-boot
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@125829 c83883e8-9f28-0410-baf1-f2025811e16a

---
+++ normal-boot/generic/lua/board-setup.lua
@@ -0,0 +1,7 @@
+--
+-- The generic board configuration should boot on almost any ThunderX
+-- hardware platform. It doesn't configure anything board specific, so
+-- has reduced functionality compared to a customized board
+-- configuration.
+--
+
