f60309f9529191fe0b167ae37e452a42247dc64a
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2015-09-28 23:17:58 +0000

    Update serial scripts for boot process changes. Testing against inv-p3e003
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@125905 c83883e8-9f28-0410-baf1-f2025811e16a

---
+++ tests/hardware/inv-p3e003-two-node-short-dram-test.py
@@ -0,0 +1,17 @@
+import boards
+import connection
+import test_boot
+import test_dram
+
+def run_test(cnx):
+    test_boot.boot_normal(cnx)
+    test_dram.dram_short(cnx)
+
+def main(logName):
+    console, mcu1, mcu2 = boards.parseArgs()
+    log = connection.Log(logName)
+    cnx = boards.Board_CRB_2S(console=console, bmc=mcu1, logObject=log)
+    cnx.runTestLoop(run_test)
+    cnx.close()
+
+main("inv-p3e003-two-node-short-dram.log")
