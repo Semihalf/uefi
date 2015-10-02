cd81f46063c8fe408908be5918be3caef9c81654
kreese <kreese@c83883e8-9f28-0410-baf1-f2025811e16a>
2015-10-02 17:52:03 +0000

    Set L2C_OCI_CTL[lock_local_stc]=1 on CN88XX pass 2.x parts. This seems to be needed for CCPI stability. WIth this setting, multi-node DRAM tests ram overnight without any issues.
    git-svn-id: http://casvn/svn/octeon/bdk/branches/thunder@126102 c83883e8-9f28-0410-baf1-f2025811e16a

--- libbdk-os/bdk-init-ccpi.c
+++ libbdk-os/bdk-init-ccpi.c
@@ -1037,6 +1037,9 @@ int __bdk_init_ccpi_multinode(void)
         if (node == my_node)
         {
             BDK_CSR_MODIFY(l2c_oci_ctl, node, BDK_L2C_OCI_CTL,
+                /* Errata TBD - Local STC are needed for pass 2 stability */
+                if (CAVIUM_IS_MODEL(CAVIUM_CN88XX_PASS2_X))
+                    l2c_oci_ctl.s.lock_local_stc = 1;
                 l2c_oci_ctl.s.iofrcl = 0;
                 l2c_oci_ctl.s.enaoci = node_exists);
         }
@@ -1044,6 +1047,9 @@ int __bdk_init_ccpi_multinode(void)
         {
             bdk_l2c_oci_ctl_t l2c_oci_ctl;
             l2c_oci_ctl.u = ocx_pp_read(node, BDK_L2C_OCI_CTL);
+            /* Errata TBD - Local STC are needed for pass 2 stability */
+            if (CAVIUM_IS_MODEL(CAVIUM_CN88XX_PASS2_X))
+                l2c_oci_ctl.s.lock_local_stc = 1;
             l2c_oci_ctl.s.iofrcl = 0;
             l2c_oci_ctl.s.enaoci = node_exists;
             ocx_pp_write(node, BDK_L2C_OCI_CTL, l2c_oci_ctl.u);
