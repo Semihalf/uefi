962822113e5ee5146ea3dbb8feb1732e4002f250
Chad Reese <kreese@cavium.com>
2015-11-17 15:22:57 -0800

    Have setup automatically enter the board menu if the board model isn't known. This prevents users from getting confused at the main setup menu.

--- apps/setup/app.c
+++ apps/setup/app.c
@@ -57,8 +57,18 @@ void item_reboot(bdk_menu_t *menu, char key, void *arg)
 int main(void)
 {
     bdk_watchdog_disable();
-
     bdk_menu_t menu;
+
+    /* If we started without knowing the board type, for the user into the
+       board menu so they can fix it. After the board menu, restore factory
+       defaults and reset in case a bad config was saved */
+    const char *model = bdk_config_get_str(BDK_CONFIG_BOARD_MODEL);
+    if (strcmp(model, "unknown") == 0)
+    {
+        menu_board(&menu, 'B', NULL);
+        item_factory(&menu, 'F', NULL);
+    }
+
     while (1)
     {
         bdk_menu_init(&menu, "THUNDERX Setup");
