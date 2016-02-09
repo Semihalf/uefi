1ba733a7cec24a80aa607052689e4b9f07a3cc99
Chad Reese <kreese@caviumnetworks.com>
2016-02-09 09:34:21 -0800

    Per request from Bruce, only show one transient power burn option

--- lua-modules/power_burn_menu.lua
+++ lua-modules/power_burn_menu.lua
@@ -14,11 +14,14 @@ repeat
 
     m:item("off", "Disable power burn", cavium.c.bdk_power_burn, menu.node, cavium.POWER_BURN_NONE)
     m:item("full", "Full power burn", cavium.c.bdk_power_burn, menu.node, cavium.POWER_BURN_FULL)
-    m:item("10ms", "Cycle 10ms power burn, 10ms idle", cavium.c.bdk_power_burn, menu.node, cavium.POWER_BURN_CYCLE_10MS)
-    m:item("1s", "Cycle 1s power burn, 1s idle", cavium.c.bdk_power_burn, menu.node, cavium.POWER_BURN_CYCLE_1S)
-    m:item("5s", "Cycle 5s power burn, 5s idle", cavium.c.bdk_power_burn, menu.node, cavium.POWER_BURN_CYCLE_5S)
-    m:item("1m", "Cycle 1m power burn, 1m idle", cavium.c.bdk_power_burn, menu.node, cavium.POWER_BURN_CYCLE_1M)
-    m:item("5m", "Cycle 5m power burn, 5m idle", cavium.c.bdk_power_burn, menu.node, cavium.POWER_BURN_CYCLE_5M)
+    -- The generically namd transient is 1s on, 1s idle
+    m:item("tran", "Transient power burn", cavium.c.bdk_power_burn, menu.node, cavium.POWER_BURN_CYCLE_1S)
+    -- These other options are disabled by default to reduce confusion
+    --m:item("10ms", "Cycle 10ms power burn, 10ms idle", cavium.c.bdk_power_burn, menu.node, cavium.POWER_BURN_CYCLE_10MS)
+    --m:item("1s", "Cycle 1s power burn, 1s idle", cavium.c.bdk_power_burn, menu.node, cavium.POWER_BURN_CYCLE_1S)
+    --m:item("5s", "Cycle 5s power burn, 5s idle", cavium.c.bdk_power_burn, menu.node, cavium.POWER_BURN_CYCLE_5S)
+    --m:item("1m", "Cycle 1m power burn, 1m idle", cavium.c.bdk_power_burn, menu.node, cavium.POWER_BURN_CYCLE_1M)
+    --m:item("5m", "Cycle 5m power burn, 5m idle", cavium.c.bdk_power_burn, menu.node, cavium.POWER_BURN_CYCLE_5M)
     -- Disable on multi-node due to errata DAP-24000
     if (not cavium.is_model(cavium.CN88XXP1)) or (cavium.c.bdk_numa_is_only_one() == 1) then
         m:item("throt", "Set power throttle level", do_throttle)
