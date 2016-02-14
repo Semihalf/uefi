a34987a6f2f53c1e6a30800b2c85110561301ae4
Chad Reese <kreese@caviumnetworks.com>
2016-02-14 09:55:08 -0800

    Make power burn on each node independent

--- libbdk-hal/bdk-power-burn.c
+++ libbdk-hal/bdk-power-burn.c
@@ -4,18 +4,20 @@
     if BDK_REQUIRE() needs it */
 BDK_REQUIRE_DEFINE(POWER_BURN);
 
-static bdk_power_burn_type_t global_burn_type = BDK_POWER_BURN_NONE;
+static bdk_power_burn_type_t global_burn_type[BDK_NUMA_MAX_NODES] = {
+    BDK_POWER_BURN_NONE, BDK_POWER_BURN_NONE, BDK_POWER_BURN_NONE, BDK_POWER_BURN_NONE};
 
 static void power_thread(int unused1, void *unused2)
 {
     extern int __bdk_power_burn(void);
-    BDK_TRACE(INIT, "N%d.%d: Thread burning power\n", bdk_numa_local(), bdk_get_core_num());
+    bdk_node_t node = bdk_numa_local();
+    BDK_TRACE(INIT, "N%d.%d: Thread burning power\n", node, bdk_get_core_num());
     bdk_wait_usec(100000);
-    uint64_t second = bdk_clock_get_rate(bdk_numa_local(), BDK_CLOCK_TIME);
+    uint64_t second = bdk_clock_get_rate(node, BDK_CLOCK_TIME);
     while (1)
     {
         uint64_t result = 0;
-        bdk_power_burn_type_t burn_type = (volatile bdk_power_burn_type_t)global_burn_type;
+        bdk_power_burn_type_t burn_type = (volatile bdk_power_burn_type_t)global_burn_type[node];
         switch (burn_type)
         {
             case BDK_POWER_BURN_NONE:        /* Disable power burn */
@@ -65,7 +67,7 @@ static void power_thread(int unused1, void *unused2)
             }
         }
         if (result != 0)
-            bdk_fatal("N%d.CPU%d: Power burn self check failed\n", bdk_numa_local(), bdk_get_core_num());
+            bdk_fatal("N%d.CPU%d: Power burn self check failed\n", node, bdk_get_core_num());
         bdk_thread_yield();
     }
 }
@@ -82,26 +84,26 @@ int bdk_power_burn(bdk_node_t node, bdk_power_burn_type_t burn_type)
 {
     if (burn_type == BDK_POWER_BURN_NONE)
     {
-        global_burn_type = burn_type;
+        global_burn_type[node] = burn_type;
         BDK_MB;
         return 0;
     }
 
-    int need_cores = (global_burn_type == BDK_POWER_BURN_NONE);
-    global_burn_type = burn_type;
+    int need_cores = (global_burn_type[node] == BDK_POWER_BURN_NONE);
+    global_burn_type[node] = burn_type;
     BDK_MB;
     if (!need_cores)
         return 0;
 
     if (bdk_init_cores(node, 0))
     {
-        global_burn_type = BDK_POWER_BURN_NONE;
+        global_burn_type[node] = BDK_POWER_BURN_NONE;
         BDK_MB;
-        bdk_error("Not starting power burn as node's cores didn't start\n");
+        bdk_error("N%d: Not starting power burn as node's cores didn't start\n", node);
         return -1;
     }
 
-    printf("Starting power burn threads\n");
+    printf("N%d: Starting power burn threads\n", node);
 
     const int num_cores = bdk_get_num_cores(node);
     for (int core = 0; core < num_cores; core++)
