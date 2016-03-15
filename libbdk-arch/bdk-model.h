ad82bdd53c8302d04677add0e71cac762e6eadfc
Chad Reese <kreese@cavium.com>
2016-03-15 10:50:07 -0700

    Add support for detecting CN98XX and thunder 2 versus thunder 1 families

--- libbdk-arch/bdk-model.h
+++ libbdk-arch/bdk-model.h
@@ -11,13 +11,14 @@
 
 
 /* Flag bits in top byte. The top byte of MIDR_EL1 is defined
-   as ox43, the Cavium implementer code. In this number, bit 7
-   and bit 5 are defiend as zero. We use these bits to signal
+   as ox43, the Cavium implementer code. In this number, bits
+   7,5,4 are defiend as zero. We use these bits to signal
    that revision numbers should be ignored. It isn't ideal
    that these are in the middle of an already defined field,
    but this keeps the model numbers as 32 bits */
 #define __OM_IGNORE_REVISION        0x80000000
 #define __OM_IGNORE_MINOR_REVISION  0x20000000
+#define __OM_IGNORE_MODEL           0x10000000
 
 #define CAVIUM_CN88XX_PASS1_0   0x430f0a10
 #define CAVIUM_CN88XX_PASS1_1   0x430f0a11
@@ -33,6 +34,14 @@
 #define CAVIUM_CN81XX_PASS1_0   0x430f0a20
 #define CAVIUM_CN81XX           (CAVIUM_CN81XX_PASS1_0 | __OM_IGNORE_REVISION)
 #define CAVIUM_CN81XX_PASS1_X   (CAVIUM_CN81XX_PASS1_0 | __OM_IGNORE_MINOR_REVISION)
+
+#define CAVIUM_CN98XX_PASS1_0   0x430f0b10
+#define CAVIUM_CN98XX           (CAVIUM_CN98XX_PASS1_0 | __OM_IGNORE_REVISION)
+#define CAVIUM_CN98XX_PASS1_X   (CAVIUM_CN98XX_PASS1_0 | __OM_IGNORE_MINOR_REVISION)
+
+/* These match entire families of chips */
+#define CAVIUM_CN8XXX           (CAVIUM_CN88XX_PASS1_0 | __OM_IGNORE_MODEL)
+#define CAVIUM_CN9XXX           (CAVIUM_CN98XX_PASS1_0 | __OM_IGNORE_MODEL)
 
 static inline uint64_t cavium_get_model() __attribute__ ((pure, always_inline));
 static inline uint64_t cavium_get_model()
@@ -57,6 +66,7 @@ static inline uint64_t cavium_get_model()
 static inline int CAVIUM_IS_MODEL(uint32_t arg_model) __attribute__ ((pure, always_inline));
 static inline int CAVIUM_IS_MODEL(uint32_t arg_model)
 {
+    const uint32_t FAMILY = 0xff00;    /* Bits 15:8 */
     const uint32_t PARTNUM = 0xfff0;    /* Bits 15:4 */
     const uint32_t VARIANT = 0xf00000;  /* Bits 23:20 */
     const uint32_t REVISION = 0xf;      /* Bits 3:0 */
@@ -64,7 +74,9 @@ static inline int CAVIUM_IS_MODEL(uint32_t arg_model)
     uint32_t my_model = cavium_get_model();
     uint32_t mask;
 
-    if (arg_model & __OM_IGNORE_REVISION)
+    if (arg_model & __OM_IGNORE_MODEL)
+        mask = FAMILY;
+    else if (arg_model & __OM_IGNORE_REVISION)
         mask = PARTNUM;
     else if (arg_model & __OM_IGNORE_MINOR_REVISION)
         mask = PARTNUM | VARIANT;
