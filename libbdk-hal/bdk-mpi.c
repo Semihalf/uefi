Semihalf/uefi

commit 235e9ff5da289f16ace66747fb3df7f5fbb7cf17

Improve the performance of the MPI/SPI transfers

libbdk-hal/bdk-mpi.c
@@ -79,15 +79,9 @@ uint64_t bdk_mpi_transfer(bdk_node_t node, int chip_select,
 
     BDK_CSR_INIT(mpi_cfg, node, BDK_MPI_CFG);
     if (mpi_cfg.s.lsbfirst)
-    {
-        for (int i=0; i<tx_count; i++)
-            BDK_CSR_WRITE(node, BDK_MPI_DATX(i), (tx_data >> (i*8)) & 0xff);
-    }
+        BDK_CSR_WRITE(node, BDK_MPI_WIDE_DAT, bdk_cpu_to_le64(tx_data));
     else
-    {
-        for (int i=0; i<tx_count; i++)
-            BDK_CSR_WRITE(node, BDK_MPI_DATX(i), (tx_data >> ((tx_count-i-1)*8)) & 0xff);
-    }
+        BDK_CSR_WRITE(node, BDK_MPI_WIDE_DAT, bdk_cpu_to_be64(tx_data) >> ((8 - tx_count) * 8));
 
     /* Do the operation */
     BDK_CSR_DEFINE(mpi_tx, BDK_MPI_TX);
@@ -105,13 +99,9 @@ uint64_t bdk_mpi_transfer(bdk_node_t node, int chip_select,
         return -1;
     }
 
-    uint64_t result = 0;
     /* Read out the shift data */
-    for (int i=0; i<rx_count; i++)
-    {
-        result <<= 8;
-        result |= BDK_CSR_READ(node, BDK_MPI_DATX(tx_count + i));
-    }
-    return result;
+    uint64_t result = BDK_CSR_READ(node, BDK_MPI_WIDE_DAT) >> (tx_count * 8);
+    result &= bdk_build_mask(rx_count * 8);
+    return bdk_cpu_to_be64(result);
 }
 

commit 4daa75a14fcc73515d8d880f2ecba26586d95cb3

Fix MPI/SPI transfers of less than 8 RX bytes
Commit 235e9ff5da289f16ace66747fb3df7f5fbb7cf17 broke MPI
transfers where the RX bytes was less than 8 bytes.

libbdk-hal/bdk-mpi.c
@@ -100,8 +100,9 @@ uint64_t bdk_mpi_transfer(bdk_node_t node, int chip_select,
     }
 
     /* Read out the shift data */
-    uint64_t result = BDK_CSR_READ(node, BDK_MPI_WIDE_DAT) >> (tx_count * 8);
+    uint64_t result = bdk_cpu_to_be64(BDK_CSR_READ(node, BDK_MPI_WIDE_DAT));
+    result >>= (8 - tx_count - rx_count) * 8;
     result &= bdk_build_mask(rx_count * 8);
-    return bdk_cpu_to_be64(result);
+    return result;
 }
 