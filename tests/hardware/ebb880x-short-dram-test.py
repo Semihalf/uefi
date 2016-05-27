5ce16eb619367f4585ea5c008c4fd7181fa3b0b6
Chad Reese <kreese@cavium.com>
2016-05-27 10:13:16 -0700

    [serial] Update EBB880x short test to include resets
    To increase coverage, the soft dram test now does a power
    cycle and two warm resets each iteration. This should help
    catch issues related to warm reset versus power cycle.

--- tests/hardware/ebb880x-short-dram-test.py
+++ tests/hardware/ebb880x-short-dram-test.py
@@ -4,8 +4,16 @@ import test_boot
 import test_dram
 
 def run_test(cnx):
-    test_boot.boot_normal(cnx)
+    # First run from a cold power on
+    test_boot.boot_normal(cnx) # Does a power cycle
     test_dram.dram_short(cnx)
+    # Second run from a warm reset on twice. After a power cycle, the first
+    # warm reset might be a little different than the second. Do two to make
+    # sure both combinations are tested
+    for reset in [0,1]:
+        cnx.sendEcho("rbt")
+        test_boot.boot_normal(cnx, powerCycle=False) # Skip power cycle
+        test_dram.dram_short(cnx)
 
 def main(logName):
     console, mcu1, mcu2 = boards.parseArgs()
