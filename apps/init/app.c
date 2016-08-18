d63a826544fb8850904be27d99f67a3e9b63bee8
Chad Reese <kreese@cavium.com>
2016-08-18 09:10:03 -0700

    [t88] Display an fatal message if the BDK versions don't match on the nodes
    To stop issues with people upgrading one node and not the other, force boot
    to stop if the two nodes have different BDK versions.

--- apps/init/app.c
+++ apps/init/app.c
@@ -300,8 +300,11 @@ int main(int argc, const char **argv)
             bdk_config_set_str(bdk_model_get_sku(BDK_NODE_1), BDK_CONFIG_CHIP_SKU, BDK_NODE_1);
             bdk_config_set_str(bdk_boot_info_serial(BDK_NODE_1), BDK_CONFIG_CHIP_SERIAL, BDK_NODE_1);
             bdk_boot_info_strapping(BDK_NODE_1);
-            printf("BDK Version: %s\n", (const char *)bdk_phys_to_ptr(
-                bdk_numa_get_address(BDK_NODE_1, BDK_KEY_MEMX(0))));
+            const char *node1_version = (const char *)bdk_phys_to_ptr(
+                bdk_numa_get_address(BDK_NODE_1, BDK_KEY_MEMX(0)));
+            printf("BDK Version: %s\n", node1_version);
+            if (strcmp(node1_version, bdk_version_string()) != 0)
+                bdk_fatal("Mismatched BDK versions between nodes\n");
             bdk_boot_status(BDK_BOOT_STATUS_INIT_NODE1_DRAM);
             bdk_boot_dram(BDK_NODE_1, MFG_SYSTEM_LEVEL_TEST); /* Initialize DRAM on node 1 */
             // FIXME: Failure?
