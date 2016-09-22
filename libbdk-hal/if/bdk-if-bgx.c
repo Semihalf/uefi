Semihalf/uefi

commit 9171e0e4a7d06ca12aa9f9cd65b9e9a65956ecf1

[bgx] Check for bit errors during BGX link init
For a BGX link to be considered up, require that it
have block lock during the entire up sequence. Also
do a short bit error check to stop link up on false
link bounces. This is a backport of work from CN73XX
for Alibaba 73xx Liquide I/O issue. The changes are
generically applyable.

libbdk-hal/if/bdk-if-bgx.c
@@ -1115,6 +1115,12 @@ static int xaui_link(bdk_if_handle_t handle)
             }
         }
 
+        /* Set the BGXX_SPUX_BR_STATUS2[latched_lock] bit (latching low). This
+           will be checked prior to enabling packet tx and rx, ensuring block
+           lock is sustained throughout the BGX link-up procedure */
+        BDK_CSR_MODIFY(c, handle->node, BDK_BGXX_SPUX_BR_STATUS2(bgx_block, bgx_index),
+            c.s.latched_lock = 1);
+
         /* Clear rcvflt bit if it is high (it latchs high) and read it back */
         BDK_CSR_INIT(spux_status2, handle->node, BDK_BGXX_SPUX_STATUS2(bgx_block, bgx_index));
         if (spux_status2.s.rcvflt)
@@ -1169,6 +1175,32 @@ static int xaui_link(bdk_if_handle_t handle)
         if (BDK_CSR_WAIT_FOR_FIELD(handle->node, BDK_BGXX_SPUX_STATUS1(bgx_block, bgx_index), rcv_lnk, ==, 1, TIMEOUT))
         {
             BDK_TRACE(BGX, "%s: Receive link down\n", handle->name);
+            return -1;
+        }
+
+        /* Clear out the BGX error counters/bits. These errors are expected
+           as part of the BGX link up procedure */
+        /* BIP_ERR counters clear as part of this read */
+        BDK_CSR_READ(handle->node, BDK_BGXX_SPUX_BR_BIP_ERR_CNT(bgx_block, bgx_index));
+        /* BER_CNT and ERR_BLKs clear as part of this read */
+        BDK_CSR_INIT(spu_br_status2, handle->node, BDK_BGXX_SPUX_BR_STATUS2(bgx_block, bgx_index));
+        /* Checking that latched BLOCK_LOCK is still set (Block Lock never lost) */
+        if (!spu_br_status2.s.latched_lock)
+        {
+            BDK_TRACE(BGX, "%s: BASE-R PCS block lock lost, need to retry\n", handle->name);
+            return -1;
+        }
+
+        /* If set, clear the LATCHED_BER by writing it to a one.  */
+        if (spu_br_status2.s.latched_ber)
+            BDK_CSR_WRITE(handle->node, BDK_BGXX_SPUX_BR_STATUS2(bgx_block, bgx_index), spu_br_status2.u);
+        /* Complete a BER test.  If latched_ber = 1, then BER >= 10e^4 */
+        /* The BER detection time window is 125us for 10GBASE-R (1250us for 40G). */
+        bdk_wait_usec(1500);
+        spu_br_status2.u = BDK_CSR_READ(handle->node, BDK_BGXX_SPUX_BR_STATUS2(bgx_block, bgx_index));
+        if (spu_br_status2.s.latched_ber)
+        {
+            BDK_TRACE(BGX, "%s: BER test failed, BER >= 10e^4, need to retry\n", handle->name);
             return -1;
         }
     }

commit b7fe830a6b17ceccf393f896360d1b27dad521c3

[bgx] Only perform BER test on links using 10G SERDES
The BER tests use the BGXX_SPUX_BR block, which is only
active for XFI, 10G-KR, XLAUI, and 40G-KR. XAUI, DXAUI,
and RXAUI don't use this block and can't do the same BER
test. Having this code is the *XAUI links caused link
failure. *XAUI has been broken since commit
9171e0e4a7d06ca12aa9f9cd65b9e9a65956ecf1.

libbdk-hal/if/bdk-if-bgx.c
@@ -1178,30 +1178,34 @@ static int xaui_link(bdk_if_handle_t handle)
             return -1;
         }
 
-        /* Clear out the BGX error counters/bits. These errors are expected
-           as part of the BGX link up procedure */
-        /* BIP_ERR counters clear as part of this read */
-        BDK_CSR_READ(handle->node, BDK_BGXX_SPUX_BR_BIP_ERR_CNT(bgx_block, bgx_index));
-        /* BER_CNT and ERR_BLKs clear as part of this read */
-        BDK_CSR_INIT(spu_br_status2, handle->node, BDK_BGXX_SPUX_BR_STATUS2(bgx_block, bgx_index));
-        /* Checking that latched BLOCK_LOCK is still set (Block Lock never lost) */
-        if (!spu_br_status2.s.latched_lock)
+        if ((priv->mode == BGX_MODE_XFI) || (priv->mode == BGX_MODE_XLAUI) ||
+            (priv->mode == BGX_MODE_10G_KR) || (priv->mode == BGX_MODE_40G_KR))
         {
-            BDK_TRACE(BGX, "%s: BASE-R PCS block lock lost, need to retry\n", handle->name);
-            return -1;
-        }
+            /* Clear out the BGX error counters/bits. These errors are expected
+               as part of the BGX link up procedure */
+            /* BIP_ERR counters clear as part of this read */
+            BDK_CSR_READ(handle->node, BDK_BGXX_SPUX_BR_BIP_ERR_CNT(bgx_block, bgx_index));
+            /* BER_CNT and ERR_BLKs clear as part of this read */
+            BDK_CSR_INIT(spu_br_status2, handle->node, BDK_BGXX_SPUX_BR_STATUS2(bgx_block, bgx_index));
+            /* Checking that latched BLOCK_LOCK is still set (Block Lock never lost) */
+            if (!spu_br_status2.s.latched_lock)
+            {
+                BDK_TRACE(BGX, "%s: BASE-R PCS block lock lost, need to retry\n", handle->name);
+                return -1;
+            }
 
-        /* If set, clear the LATCHED_BER by writing it to a one.  */
-        if (spu_br_status2.s.latched_ber)
-            BDK_CSR_WRITE(handle->node, BDK_BGXX_SPUX_BR_STATUS2(bgx_block, bgx_index), spu_br_status2.u);
-        /* Complete a BER test.  If latched_ber = 1, then BER >= 10e^4 */
-        /* The BER detection time window is 125us for 10GBASE-R (1250us for 40G). */
-        bdk_wait_usec(1500);
-        spu_br_status2.u = BDK_CSR_READ(handle->node, BDK_BGXX_SPUX_BR_STATUS2(bgx_block, bgx_index));
-        if (spu_br_status2.s.latched_ber)
-        {
-            BDK_TRACE(BGX, "%s: BER test failed, BER >= 10e^4, need to retry\n", handle->name);
-            return -1;
+            /* If set, clear the LATCHED_BER by writing it to a one.  */
+            if (spu_br_status2.s.latched_ber)
+                BDK_CSR_WRITE(handle->node, BDK_BGXX_SPUX_BR_STATUS2(bgx_block, bgx_index), spu_br_status2.u);
+            /* Complete a BER test.  If latched_ber = 1, then BER >= 10e^4 */
+            /* The BER detection time window is 125us for 10GBASE-R (1250us for 40G). */
+            bdk_wait_usec(1500);
+            spu_br_status2.u = BDK_CSR_READ(handle->node, BDK_BGXX_SPUX_BR_STATUS2(bgx_block, bgx_index));
+            if (spu_br_status2.s.latched_ber)
+            {
+                BDK_TRACE(BGX, "%s: BER test failed, BER >= 10e^4, need to retry\n", handle->name);
+                return -1;
+            }
         }
     }
 