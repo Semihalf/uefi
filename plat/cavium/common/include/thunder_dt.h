d4088b4ee227339d1e311a93d92aae39695e87d4
Konrad Adamczyk <ka@semihalf.com>
2016-10-25 09:31:45 +0200

    common: ecam: Get ECAM devices from DTB
    Those functions are needed to get correct name and
    property from DTS.
    
    Signed-off-by: Konrad Adamczyk <ka@semihalf.com>
    Signed-off-by: Jan Dabros <jan.dabros@cavium.com>

--- plat/cavium/common/include/thunder_dt.h
+++ plat/cavium/common/include/thunder_dt.h
@@ -16,6 +16,12 @@ typedef struct board_fdt {
 int thunder_fill_board_details(int info);
 uint64_t thunder_get_ecam_config_addr(int node, int ecam);
 uint64_t thunder_get_ecam_config_size(int node, int ecam);
+int thunder_get_ecam_offset(const char *prop);
+int thunder_get_next_offset(int offset);
+const char *thunder_get_prop_name(int offset);
+const char *thunder_get_prop_value(int offset);
+int dts_get_num_from_str(const char *str);
+int dts_get_substr_index(const char *str, char *substr, char sep);
 
 extern board_fdt_t bfdt;
 #endif
